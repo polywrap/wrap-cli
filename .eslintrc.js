3b8b32e072481a60ddf2630e7ad1193217535ecf
Marin Petrunić <marin.petrunic@gmail.com>
2020-12-04 15:21:29 +0100

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,43 +13,46 @@ module.exports = {
   },
   plugins: [
     "eslint-plugin-import",
-    "@typescript-eslint"
+    "@typescript-eslint",
+    "prettier"
   ],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended"
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
   ],
   rules: {
+    "prettier/prettier": ["error"],
     "@typescript-eslint/naming-convention": ["error", 
-    {selector: "default", format: ['camelCase']},
-    {selector: ["classProperty", "objectLiteralProperty", "classMethod", "parameter"], format: ['camelCase'], leadingUnderscore: "allow"},
-    //variable must be in camel or upper case
-    {selector: "variable", format: ["camelCase", "UPPER_CASE"], leadingUnderscore: "allow"},
-    //classes and types must be in PascalCase
-    {selector: ["typeLike", "enum"], format: ['PascalCase']},
-    {selector: "enumMember", format: null},
-    //ignore rule for quoted stuff
-    {
-      selector: [
-        "classProperty",
-        "objectLiteralProperty",
-        "typeProperty",
-        "classMethod",
-        "objectLiteralMethod",
-        "typeMethod",
-        "accessor",
-        "enumMember"
-      ],
-      format: null,
-      modifiers: ["requiresQuotes"]
-    },
-    //ignore rules on destructured params
-    {
-      selector: "variable",
-      modifiers: ["destructured"],
-      format: null
-    }
+      {selector: "default", format: ['camelCase']},
+      {selector: ["classProperty", "objectLiteralProperty", "classMethod", "parameter"], format: ['camelCase'], leadingUnderscore: "allow"},
+      //variable must be in camel or upper case
+      {selector: "variable", format: ["camelCase", "UPPER_CASE"], leadingUnderscore: "allow"},
+      //classes and types must be in PascalCase
+      {selector: ["typeLike", "enum"], format: ['PascalCase']},
+      {selector: "enumMember", format: null},
+      //ignore rule for quoted stuff
+      {
+        selector: [
+          "classProperty",
+          "objectLiteralProperty",
+          "typeProperty",
+          "classMethod",
+          "objectLiteralMethod",
+          "typeMethod",
+          "accessor",
+          "enumMember"
+        ],
+        format: null,
+        modifiers: ["requiresQuotes"]
+      },
+      //ignore rules on destructured params
+      {
+        selector: "variable",
+        modifiers: ["destructured"],
+        format: null
+      }
   ],
     "@typescript-eslint/explicit-module-boundary-types": "error",
     "@typescript-eslint/member-ordering": "error",
