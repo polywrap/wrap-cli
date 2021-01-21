c5f90fe7391a82c87835f0d97e6d89e74f421895
Ben Walker <ben.walker18@gmail.com>
2021-01-20 16:43:18 -0800

    Add optional parameters to CLI test-env functions

--- packages/cli/src/lib/env/test.ts
+++ packages/cli/src/lib/env/test.ts
@@ -1,9 +1,9 @@
 import * as testEnv from "@web3api/client-test-env";
 
-export async function startupTestEnv(quiet: boolean): Promise<void> {
+export async function startupTestEnv(quiet: boolean, ganacheCli?: boolean, ipfs?: boolean): Promise<void> {
   await testEnv.up(quiet);
 }
 
-export async function shutdownTestEnv(quiet: boolean): Promise<void> {
+export async function shutdownTestEnv(quiet: boolean, ganacheCli?: boolean, ipfs?: boolean): Promise<void> {
   await testEnv.down(quiet);
 }
