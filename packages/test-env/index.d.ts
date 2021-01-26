7e3ebb76d795887e1034644558b3cba74881049b
Ben Walker <ben.walker18@gmail.com>
2021-01-26 12:51:37 -0800

    Revert CLI changes

--- packages/test-env/index.d.ts
+++ packages/test-env/index.d.ts
@@ -1,2 +1,2 @@
-export function up(quiet: boolean, ganacheCli?: boolean, ipfs?: boolean): Promise<void>;
-export function down(quiet: boolean, ganacheCli?: boolean, ipfs?: boolean): Promise<void>;
+export function up(quiet: boolean): Promise<void>;
+export function down(quiet: boolean): Promise<void>;
