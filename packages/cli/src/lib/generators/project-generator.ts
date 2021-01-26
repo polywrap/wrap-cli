514708eb79507c8475d649b9c3ce7f6f0f81e9ff
Alexander Bai <vhurryharry@gmail.com>
2021-01-26 05:39:47 -0500

    Updated create parameters

--- packages/cli/src/lib/generators/project-generator.ts
+++ packages/cli/src/lib/generators/project-generator.ts
@@ -1,43 +1,16 @@
+import { execSync } from "child_process";
 import { GluegunFilesystem } from "gluegun";
 
-export const generateProject = (projectName: string, fs: GluegunFilesystem): void => {
-  const { dir, write } = fs;
+export const generateProject = (type: string, lang: string, projectName: string, fs: GluegunFilesystem): void => {
+  const { dir, remove, move } = fs;
   dir(projectName);
-  dir(`${projectName}/src`);
-  dir(`${projectName}/src/mutation`);
-  dir(`${projectName}/src/query`);
-  dir(`${projectName}/src/subgraph`);
-  dir(`${projectName}/scripts`);
-  dir(`${projectName}/tests`);
-  write(`${projectName}/web3api.yaml`, WEB3API_YML);
-  write(`${projectName}/package.json`, PACKAGE_JSON);
+  execSync(`
+    cd ${projectName}
+    git init
+    git remote add origin https://github.com/Web3-API/prototype.git
+    git config core.sparsecheckout true
+    echo "packages/templates/${type}/${lang}" >> .git/info/sparse-checkout
+    git pull origin feature/cli-create-codegen
+    mv ./packages/templates/${type}/${lang}/* ./
+  `);
 };
-
-const WEB3API_YML = `description: TODO
-repository: TODO
-mutation:
-  schema:
-    file: ./mutation/schema.graphql
-  module:
-    language: wasm/assemblyscript
-    file: ./mutation/index.ts
-query:
-  schema:
-    file: ./query/schema.graphql
-  module:
-    language: wasm/assemblyscript
-    file: ./query/index.ts
-subgraph:
-  file: ./subgraph/subgraph.yaml
-`;
-
-const PACKAGE_JSON = `{
-  "name": "TODO",
-  "scripts": {
-    "build": "w3 build"
-  },
-  "dependencies": {
-    "@web3api/cli": "0.0.1-alpha.1"
-  }
-}
-`;
