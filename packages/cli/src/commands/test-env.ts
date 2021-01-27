9649d68e0dae0aef5b283f6669cee737e30110f2
Ben Walker <ben.walker18@gmail.com>
2021-01-26 16:39:40 -0800

    Fix additional linter errors

--- packages/cli/src/commands/test-env.ts
+++ packages/cli/src/commands/test-env.ts
@@ -1,7 +1,5 @@
 import { shutdownTestEnv, startupTestEnv } from "../lib/env/test";
-
 import { withSpinner } from "../lib/helpers/spinner";
-
 import { GluegunToolbox, print } from "gluegun";
 
 import chalk from "chalk";
--- packages/cli/src/lib/env/test.ts
+++ packages/cli/src/lib/env/test.ts
@@ -1,11 +1,13 @@
 import * as testEnv from "@web3api/client-test-env";
 
 export async function startupTestEnv(
-  quiet: boolean, directory: string): Promise<void> {
+  quiet: boolean,
+  directory: string): Promise<void> {
   await testEnv.up(quiet, directory);
 }
 
 export async function shutdownTestEnv(
-  quiet: boolean, directory: string): Promise<void> {
+  quiet: boolean,
+  directory: string): Promise<void> {
   await testEnv.down(quiet, directory);
 }
