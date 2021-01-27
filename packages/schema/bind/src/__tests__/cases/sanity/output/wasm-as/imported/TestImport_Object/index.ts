b15dea47b02e2a88c816d476936800661f6b3c58
Matija Petrunic <matija.petrunic@fer.hr>
2021-01-27 17:10:17 +0100

    Update imported object bind tests

--- packages/schema/bind/src/__tests__/cases/sanity/output/wasm-as/imported/TestImport_Object/index.ts
+++ packages/schema/bind/src/__tests__/cases/sanity/output/wasm-as/imported/TestImport_Object/index.ts
@@ -3,12 +3,16 @@ import {
   serializeTestImport_Object,
   deserializeTestImport_Object
 } from "./serialization";
+import * as Objects from "../../";
 
 export class TestImport_Object {
 
   public static uri: string = "testimport.uri.eth";
 
-  prop: string;
+  object: Objects.TestImport_AnotherObject;
+  optObject: Nullable<Objects.TestImport_AnotherObject>;
+  objectArray: Array<Objects.TestImport_AnotherObject>;
+  optObjectArray: Array<Nullable<Objects.TestImport_AnotherObject>> | null;
 
   toBuffer(): ArrayBuffer {
     return serializeTestImport_Object(this);
