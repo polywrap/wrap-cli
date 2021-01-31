4038cf164ebd237a23b0d4afd2e8b8cd3d23a299
dOrgJelli <jelli@dorg.tech>
2021-01-30 23:30:27 -0600

    generateManifest script fixes

--- packages/js/core/scripts/generateManifest.js
+++ packages/js/core/scripts/generateManifest.js
@@ -1,25 +1,47 @@
-const package = require("../package.json");
-const schemaVersion = package.dependencies["@web3api/manifest-schema"];
-const schema = require(`@web3api/manifest-schema/formats/${schemaVersion}.json`);
-const { compile } = require("json-schema-to-typescript");
-const { writeFileSync } = require("fs");
+const SchemaToTypescript = require("json-schema-to-typescript");
+const fs = require("fs");
+const path = require("path");
 
-// TODO: Generate all manifests
-const generateManifest = () => {
-  try {
-    compile(schema, "Web3API")
-      .then((file) => {
-        const manifestPath =
-          __dirname + `/../src/manifest/formats/${package.version}.ts`;
-        writeFileSync(
-          manifestPath,
-          `/* eslint-disable @typescript-eslint/naming-convention */\n${file}`
-        );
-      })
-      .catch((e) => console.error(e));
-  } catch (e) {
-    console.error("Error generating the Manifest file: ", e);
+const generateManifest = async () => {
+  // Fetch all schemas within the @web3api/manifest-schema/formats directory
+  const formatsDir = path.join(
+    path.dirname(require.resolve("@web3api/manifest-schema")),
+    "/formats"
+  );
+  const formatFiles = fs.readdirSync(formatsDir);
+
+  for (let i = 0; i < formatFiles.length; ++i) {
+    const formatFile = formatFiles[i];
+
+    try {
+      const format = JSON.parse(fs.readFileSync(
+        path.join(formatsDir, formatFile),
+        { encoding:'utf8' }
+      ));
+      const version = formatFile.replace(".json", "");
+
+      const file = await SchemaToTypescript.compile(format, "Web3API")
+
+      const manifestPath = path.join(__dirname, `/../src/manifest/formats/${version}.ts`);
+      fs.writeFileSync(
+        manifestPath,
+        `/* eslint-disable @typescript-eslint/naming-convention */\n${file}`,
+        { }
+      );
+    } catch (e) {
+      console.error(`Error generating the Manifest file ${formatFile}: `, e);
+      throw e;
+    }
   }
+
+  return Promise.resolve();
 };
 
-generateManifest();
+generateManifest()
+  .then(text => {
+    process.exit();
+  })
+  .catch(err => {
+    console.error(err);
+    process.abort();
+  });
