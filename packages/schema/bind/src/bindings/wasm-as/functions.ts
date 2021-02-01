086a7da35c854f79a1f567238c69aa6565a3c11f
dOrgJelli <jelli@dorg.tech>
2021-01-31 19:22:34 -0600

    lint fixes

--- packages/schema/bind/src/bindings/wasm-as/functions.ts
+++ packages/schema/bind/src/bindings/wasm-as/functions.ts
@@ -43,8 +43,10 @@ export const toWasmInit: MustacheFunction = () => {
       const nullable = "Nullable";
       const nullOptional = "| null";
 
-      if (nullType.substr(0, nullable.length) === nullable ||
-          nullType.substr(-nullOptional.length) === nullOptional) {
+      if (
+        nullType.substr(0, nullable.length) === nullable ||
+        nullType.substr(-nullOptional.length) === nullOptional
+      ) {
         return "null";
       }
     }
