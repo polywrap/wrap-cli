263e0fa8ba6cb5d64430ca538c09476b08391278
Alexander Bai <vhurryharry@gmail.com>
2021-02-03 12:01:37 -0500

    Added jest config, cli args update

---
+++ packages/cli/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  collectCoverage: true,
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/__tests__/?(*.)+(spec|test).[jt]s?(x)"],
+  globals: {
+    "ts-jest": {
+      diagnostics: false,
+    },
+  },
+};
--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,10 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { build, GluegunToolbox } from "gluegun";
 
 interface Args {
   [key: string]: unknown;
 }
 
-export const run = async (argv: Args): Promise<GluegunToolbox> => {
+export const run = async (
+  argv: Args | Array<unknown>
+): Promise<GluegunToolbox> => {
   const cli = build("w3")
     .src(__dirname)
     .plugins(`${process.cwd()}/node_modules`, {
