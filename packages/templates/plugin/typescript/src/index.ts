4c497b17a1d148caa376048cacd01ddf65fe3ed2
Alexander Bai <vhurryharry@gmail.com>
2021-02-09 12:46:22 -0500

    Added plugin template

---
+++ packages/templates/plugin/typescript/src/index.ts
@@ -0,0 +1,35 @@
+import { mutation, query } from "./resolvers";
+import { manifest } from "./manifest";
+
+import { Plugin, PluginManifest, PluginModules } from "@web3api/core-js";
+
+export interface SamplePluginConfig {
+  defaultValue: string;
+}
+
+export class SamplePlugin extends Plugin {
+  constructor(private _config: SamplePluginConfig) {
+    super();
+  }
+
+  public static manifest(): PluginManifest {
+    return manifest;
+  }
+
+  public getModules(): PluginModules {
+    return {
+      query: query(this),
+      mutation: mutation(this),
+    };
+  }
+
+  public async sampleQuery(query: string): Promise<string> {
+    // Todo: Add query processing part here
+    return this._config.defaultValue;
+  }
+
+  public async sampleMutation(data: Uint8Array): Promise<string> {
+    // Todo: Add mutation processing part here
+    return this._config.defaultValue;
+  }
+}
