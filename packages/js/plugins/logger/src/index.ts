224e2abc5f6498b70ad59ed81357b20cdd398e6c
Matija Petrunic <matija.petrunic@fer.hr>
2021-03-09 14:59:12 +0100

    Add color to logger plugin console calls

--- packages/js/plugins/logger/src/index.ts
+++ packages/js/plugins/logger/src/index.ts
@@ -1,7 +1,15 @@
 import { query } from "./resolvers";
 import { manifest } from "./manifest";
 
+import colors from "colors";
 import { Plugin, PluginManifest, PluginModules } from "@web3api/core-js";
+
+export enum LogLevel {
+  DEBUG,
+  INFO,
+  WARN,
+  ERROR,
+}
 
 export class LoggerPlugin extends Plugin {
   constructor() {
@@ -18,8 +26,20 @@ export class LoggerPlugin extends Plugin {
     };
   }
 
-  public async log(level: string, message: string): Promise<boolean> {
-    console.info(message);
+  public async log(level: LogLevel, message: string): Promise<boolean> {
+    switch (level) {
+      case LogLevel.DEBUG:
+        console.log(colors.blue(message));
+        break;
+      case LogLevel.WARN:
+        console.log(colors.yellow(message));
+        break;
+      case LogLevel.ERROR:
+        console.log(colors.red(message));
+        break;
+      default:
+        console.log(message);
+    }
 
     return true;
   }
