b905eea8de94528ea70ec16359634cca1f1b3c45
Matija Petrunic <matija.petrunic@fer.hr>
2021-03-09 13:24:42 +0100

    Add console api test recipe

--- packages/core-apis/console/src/query/index.ts
+++ packages/core-apis/console/src/query/index.ts
@@ -1,8 +1,10 @@
-import { Logger_Query, Logger_LogLevel } from "./w3/imported";
-import { Input_info } from "./w3";
+import { Input_info, Logger_Query, Logger_LogLevel } from "./w3";
 
 export function info(input: Input_info): boolean {
-  Logger_Query.log(Logger_LogLevel.INFO, input.message);
+  Logger_Query.log({
+    message: input.message,
+    level: Logger_LogLevel.INFO
+  });
 
   return true;
 }
