6e66bc0b015e91b04d8aa4aec5021c1bef1bc95f
Matija Petrunic <matija.petrunic@fer.hr>
2021-03-09 14:59:38 +0100

    Set logger plugin log level to be LogLevel enum

--- packages/js/plugins/logger/src/resolvers.ts
+++ packages/js/plugins/logger/src/resolvers.ts
@@ -1,9 +1,9 @@
-import { LoggerPlugin } from ".";
+import { LoggerPlugin, LogLevel } from ".";
 
 import { PluginModule } from "@web3api/core-js";
 
 export const query = (plugin: LoggerPlugin): PluginModule => ({
-  log: async (input: { level: string; message: string }) => {
+  log: async (input: { level: LogLevel; message: string }) => {
     return await plugin.log(input.level, input.message);
   },
 });
