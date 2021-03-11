ba592af00383808f3e64749695085ffc4da9edaf
Kris <kris@dorg.tech>
2021-03-11 11:46:09 -0600

    added basis for cli internationalization; not currently functional

--- packages/cli/jest.config.js
+++ packages/cli/jest.config.js
@@ -6,6 +6,17 @@ module.exports = {
   globals: {
     "ts-jest": {
       diagnostics: false,
+      astTransformers: {
+        before: [
+          {
+            path: '@formatjs/ts-transformer/ts-jest-integration',
+            options: {
+              overrideIdFn: '[sha512:contenthash:base64:6]',
+              ast: true,
+            },
+          },
+        ],
+      },
     },
   },
   setupFilesAfterEnv: ["./jest.setup.js"],
--- packages/cli/src/commands/w3.ts
+++ packages/cli/src/commands/w3.ts
@@ -1,3 +1,5 @@
+import { getIntl } from "../lib/internationalization";
+
 import { GluegunToolbox } from "gluegun";
 
 export default {
@@ -8,8 +10,17 @@ export default {
     if (parameters.first !== undefined) {
       print.error(`w3 ${parameters.first} is not a command`);
     } else {
+      const intl = await getIntl("es");
       print.success(
-        `Type ${print.colors.blue("w3 help")} to view common commands`
+        intl.formatMessage(
+          {
+            id: "typeToViewCommands",
+            defaultMessage: "Type {phrase} to view common commands",
+            description: "type phrase to view commands",
+          },
+          { phrase: `${print.colors.blue("w3 help")}` }
+        )
+        //`Type ${print.colors.blue("w3 help")} to view common commands`
       );
     }
   },
