a6e9115655e2d6a1eaf1d751a9662d9ded91b849
krisbitney <kris@dorg.tech>
2021-03-17 12:29:19 -0500

    reworked locale parsing to ensure locale used is always valid

--- packages/cli/src/lib/internationalization.ts
+++ packages/cli/src/lib/internationalization.ts
@@ -8,30 +8,33 @@ import osLocale from "os-locale";
 import { readFileSync } from "fs";
 import { MessageFormatElement } from "intl-messageformat-parser";
 
+interface LocaleData {
+  lang: string;
+  messages: Record<string, MessageFormatElement[]>;
+}
+
 const cache: IntlCache = createIntlCache();
 
 export function getIntl(locale: string = osLocale.sync()): IntlShape<string> {
-  const lang = locale.substring(0, 2);
-  const messages = getLocaleData(lang);
+  const localeData: LocaleData = getLocaleData(locale);
   return createIntl(
     {
-      locale: lang,
+      locale: localeData.lang,
       defaultLocale: "en",
-      messages: messages,
+      messages: localeData.messages,
     },
     cache
   );
 }
 
-function getLocaleData(lang: string): Record<string, MessageFormatElement[]> {
-  switch (lang) {
-    case "es":
-      return JSON.parse(
-        readFileSync(__dirname + "/../../compiled-lang/es.json", "utf-8")
-      );
-    default:
-      return JSON.parse(
-        readFileSync(__dirname + "/../../compiled-lang/en.json", "utf-8")
-      );
-  }
+function getLocaleData(locale: string): LocaleData {
+  const localeLang = locale.substring(0, 2);
+  const lang = ["en", "es"].includes(localeLang) ? localeLang : "en";
+  const messages = JSON.parse(
+    readFileSync(`${__dirname}/../../compiled-lang/${lang}.json`, "utf-8")
+  );
+  return {
+    lang: lang,
+    messages: messages,
+  };
 }
