83b49e1c6150b75e11bd85086bfdf3c62d77ef41
dOrgJelli <jelli@dorg.tech>
2021-03-18 22:15:35 -0500

    console api fix

--- packages/apis/console/src/query/index.ts
+++ packages/apis/console/src/query/index.ts
@@ -1,37 +1,29 @@
 import { Input_info, Logger_Query, Logger_LogLevel } from "./w3";
 
 export function debug(input: Input_info): bool {
-  Logger_Query.log({
+  return Logger_Query.log({
     message: input.message,
     level: Logger_LogLevel.DEBUG
   });
-
-  return true;
 }
 
 export function info(input: Input_info): bool {
-  Logger_Query.log({
+  return Logger_Query.log({
     message: input.message,
     level: Logger_LogLevel.INFO
   });
-
-  return true;
 }
 
 export function warn(input: Input_info): bool {
-  Logger_Query.log({
+  return Logger_Query.log({
     message: input.message,
     level: Logger_LogLevel.WARN
   });
-
-  return true;
 }
 
 export function error(input: Input_info): bool {
-  Logger_Query.log({
+  return Logger_Query.log({
     message: input.message,
     level: Logger_LogLevel.ERROR
   });
-
-  return true;
 }
