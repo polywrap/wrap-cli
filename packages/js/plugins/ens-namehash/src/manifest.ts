5875c17de7f39c740b943616519eb9947130e862
namesty <nestor09amesty@gmail.com>
2021-03-23 23:52:00 +0100

    [WIP] removed ens namehash plugin

--- packages/js/plugins/ens-namehash/src/manifest.ts
+++
@@ -1,9 +0,0 @@
-import { PluginManifest } from "@web3api/core-js";
-
-export const manifest: PluginManifest = {
-  // TODO: use the schema.graphql
-  // https://github.com/Web3-API/prototype/issues/101
-  schema: "type Query { dummy: String }",
-  implemented: [],
-  imported: [],
-};
