2bc8a332bb5ccf5c63e5025ad2aea4e8ab7df3fb
dOrgJelli <jelli@dorg.tech>
2021-04-07 15:53:16 -0500

    use plugin manifests for schema resolution

--- packages/js/plugins/ipfs/src/manifest.ts
+++ packages/js/plugins/ipfs/src/manifest.ts
@@ -3,7 +3,40 @@ import { PluginManifest, Uri } from "@web3api/core-js";
 export const manifest: PluginManifest = {
   // TODO: use the schema.graphql
   // https://github.com/web3-api/monorepo/issues/101
-  schema: "type Query { dummy: String }",
+  schema: `
+# TODO: should import and "implements" the api-resolver core-api schema
+# https://github.com/Web3-API/monorepo/issues/75
+
+type Query {
+  catFile(cid: String!): Bytes!
+
+  tryResolveUri(
+    authority: String!
+    path: String!
+  ): ApiResolver_MaybeUriOrManifest
+
+  getFile(
+    path: String!
+  ): Bytes
+}
+
+type Mutation {
+  # TODO: Allow for custom type CID
+  # https://github.com/web3-api/monorepo/issues/103
+  addFile(data: Bytes!): AddResult!
+}
+
+type AddResult {
+  path: String!
+  cid: String!
+}
+
+# TODO: should get replaced with an import
+# https://github.com/Web3-API/monorepo/issues/75
+type ApiResolver_MaybeUriOrManifest {
+  uri: String
+  manifest: String
+}`,
   implemented: [new Uri("w3/api-resolver")],
   imported: [],
 };
