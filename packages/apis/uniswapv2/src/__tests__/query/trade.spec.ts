eb3a0b905bb453cd1fbc6546a47d2c3942e288d3
Matija Petrunic <matija.petrunic@fer.hr>
2021-04-09 14:59:24 +0200

    Add trade minimumAmountOut and maximumAmountIn

---
+++ packages/apis/uniswapv2/src/__tests__/query/trade.spec.ts
@@ -0,0 +1,227 @@
+import { tradeMaximumAmountIn, tradeMinimumAmountOut } from "../../query/trade"
+import { ChainId, Pair, Token, Trade, TradeType } from "../../query/w3";
+
+const token0: Token = {
+  chainId: ChainId.MAINNET,
+  address: '0x0000000000000000000000000000000000000001',
+  decimals: 18,
+  symbol: 't0',
+  name: null,
+}
+const token1: Token = {
+  chainId: ChainId.MAINNET,
+  address: '0x0000000000000000000000000000000000000002',
+  decimals: 18,
+  symbol: 't1',
+  name: null,
+}
+const token2: Token = {
+  chainId: ChainId.MAINNET,
+  address: '0x0000000000000000000000000000000000000003',
+  decimals: 18,
+  symbol: 't2',
+  name: null,
+}
+const token3: Token = {
+  chainId: ChainId.MAINNET,
+  address: '0x0000000000000000000000000000000000000004',
+  decimals: 18,
+  symbol: 't3',
+  name: null,
+}
+
+const pair_0_1: Pair = {
+  tokenAmount0: {
+    amount: "1000",
+    token: token0
+  },
+  tokenAmount1: {
+    amount: "1000",
+    token: token1
+  }
+}
+const pair_1_2: Pair = {
+  tokenAmount0: {
+    amount: "1200",
+    token: token1
+  },
+  tokenAmount1: {
+    amount: "1000",
+    token: token2
+  }
+}
+const exactIn: Trade = {
+  route: {
+    pairs: [pair_0_1, pair_1_2],
+    input: token0
+  },
+  amount: {
+    token: token0,
+    amount: "100"
+  },
+  tradeType: TradeType.EXACT_INPUT
+}
+const exactOut: Trade = {
+  route: {
+    pairs: [pair_0_1, pair_1_2],
+    input: token0
+  },
+  amount: {
+    token: token2,
+    amount: "100"
+  },
+  tradeType: TradeType.EXACT_OUTPUT
+}
+
+describe('Trade', () => {
+
+  describe("tradeMinimumAmountOut", () => {
+
+    describe("tradeType = EXACT_INPUT", () => {
+      test("throws if less than 0", () => {
+        expect(() => {
+          tradeMinimumAmountOut({
+          trade: exactIn,
+          slippageTolerance: "-1"
+        })}).toThrow()
+      })
+
+      test("returns  exact if nonzero", () => {
+        expect(tradeMinimumAmountOut({
+          trade: exactIn,
+          slippageTolerance: "2"
+        })).toStrictEqual({
+          token: token2,
+          amount: "23"
+        })
+
+        expect(tradeMinimumAmountOut({
+          trade: exactIn,
+          slippageTolerance: "0.05"
+        })).toStrictEqual({
+          token: token2,
+          amount: "65"
+        })
+
+        expect(tradeMinimumAmountOut({
+          trade: exactIn,
+          slippageTolerance: "0"
+        })).toStrictEqual({
+          token: token2,
+          amount: "69"
+        })
+      })
+    });
+
+    describe("tradeType = EXACT_OUTPUT", () => {
+      test("throws if less than 0", () => {
+        expect(() => {
+          tradeMinimumAmountOut({
+          trade: exactOut,
+          slippageTolerance: "-1"
+        })}).toThrow()
+      })
+
+      test("returns  exact if nonzero", () => {
+        expect(tradeMinimumAmountOut({
+          trade: exactOut,
+          slippageTolerance: "2"
+        })).toStrictEqual({
+          token: token2,
+          amount: "100"
+        })
+
+        expect(tradeMinimumAmountOut({
+          trade: exactOut,
+          slippageTolerance: "0.05"
+        })).toStrictEqual({
+          token: token2,
+          amount: "100"
+        })
+
+        expect(tradeMinimumAmountOut({
+          trade: exactOut,
+          slippageTolerance: "0"
+        })).toStrictEqual({
+          token: token2,
+          amount: "100"
+        })
+      })
+    });
+  });
+
+  describe("tradeMaximumAmountIn", () => {
+
+    describe("tradeType = EXACT_INPUT", () => {
+      test("throws if less than 0", () => {
+        expect(() => {
+          tradeMaximumAmountIn({
+          trade: exactIn,
+          slippageTolerance: "-1"
+        })}).toThrow()
+      })
+
+      test("returns  exact if nonzero", () => {
+        expect(tradeMaximumAmountIn({
+          trade: exactIn,
+          slippageTolerance: "2"
+        })).toStrictEqual({
+          token: token0,
+          amount: "100"
+        })
+
+        expect(tradeMaximumAmountIn({
+          trade: exactIn,
+          slippageTolerance: "0.05"
+        })).toStrictEqual({
+          token: token0,
+          amount: "100"
+        })
+
+        expect(tradeMaximumAmountIn({
+          trade: exactIn,
+          slippageTolerance: "0"
+        })).toStrictEqual({
+          token: token0,
+          amount: "100"
+        })
+      })
+    });
+
+    describe("tradeType = EXACT_OUTPUT", () => {
+      test("throws if less than 0", () => {
+        expect(() => {
+          tradeMaximumAmountIn({
+          trade: exactOut,
+          slippageTolerance: "-1"
+        })}).toThrow()
+      })
+
+      test("returns  exact if nonzero", () => {
+        expect(tradeMaximumAmountIn({
+          trade: exactOut,
+          slippageTolerance: "2"
+        })).toStrictEqual({
+          token: token0,
+          amount: "468"
+        })
+
+        expect(tradeMaximumAmountIn({
+          trade: exactOut,
+          slippageTolerance: "0.05"
+        })).toStrictEqual({
+          token: token0,
+          amount: "163"
+        })
+
+        expect(tradeMaximumAmountIn({
+          trade: exactOut,
+          slippageTolerance: "0"
+        })).toStrictEqual({
+          token: token0,
+          amount: "156"
+        })
+      })
+    });
+  });
+})
