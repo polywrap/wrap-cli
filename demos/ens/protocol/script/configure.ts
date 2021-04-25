e8f0e32310a8e1583d6f568b03995c396bae99a8
dOrgJelli <jelli@dorg.tech>
2021-04-24 23:52:46 -0500

    added open domain script, need to merge prealpha-dev to get it to work

--- demos/ens/protocol/script/configure.ts
+++ demos/ens/protocol/script/configure.ts
@@ -1,6 +1,147 @@
 import { Web3ApiClient } from "@web3api/client-js";
+import { ipfsPlugin } from "@web3api/ipfs-plugin-js";
+import { ethereumPlugin, EthereumProvider } from "@web3api/ethereum-plugin-js";
+import { ensPlugin } from "@web3api/ens-plugin-js";
+import { uts46Plugin } from "@web3api/uts46-plugin-js";
+import { sha3Plugin } from "@web3api/sha3-plugin-js";
+
+import { ethers, Wallet } from "ethers";
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
 
 // TODO:
 // - deploy web3api to testnet at testnet/ens.eth
 // - create Web3ApiClient pointed to rinkeby w/ signer($ETH_PRIV_KEY)
 // - send queries
+
+async function main() {
+  const uri = "ipfs/QmZKcHjxXHrWapE9MXDseVUEJqhFtrP6QGX1TUfcoQJfAx";
+  const ensAddress = "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e";
+  // const resolverAddress = "0xf6305c19e814d2a75429Fd637d01F7ee0E77d615";
+  const network = "rinkeby";
+  const domain = "open.someopendomain.eth";
+  const privKey = process.env.ETH_PRIV_KEY as string;
+
+  if (!privKey) {
+    throw Error("ETH_PRIV_KEY env variable is missing");
+  }
+
+  // Get the test ens address
+  const { data } = await axios.get("http://localhost:4040/ens");
+  const testnetEnsAddress = data.ensAddress as string;
+
+  console.log(testnetEnsAddress)
+
+  const client = new Web3ApiClient({
+    redirects: [
+      {
+        from: "w3://ens/uts46.web3api.eth",
+        to: uts46Plugin(),
+      },
+      {
+        from: "w3://ens/sha3.web3api.eth",
+        to: sha3Plugin(),
+      },
+      {
+        from: "w3://ens/ipfs.web3api.eth",
+        to: ipfsPlugin({
+          provider: "http://localhost:5001",
+          fallbackProviders: ["https://ipfs.io"]
+        }),
+      },
+      {
+        from: "w3://ens/ens.web3api.eth",
+        to: ensPlugin({
+          addresses: {
+            testnet: testnetEnsAddress
+          }
+        }),
+      },
+      {
+        from: "w3://ens/ethereum.web3api.eth",
+        to: ethereumPlugin({
+          networks: {
+            rinkeby: {
+              provider: ethers.getDefaultProvider("rinkeby") as EthereumProvider,
+              signer: new Wallet(privKey)
+            },
+            testnet: {
+              provider: "http://localhost:8545"
+            }
+          }
+        }),
+      }
+    ]
+  })
+
+  // Deploy a new instance of the FIFS registrar
+  const deployFifs = await client.query<{
+    deployFIFSRegistrar: string
+  }>({
+    uri,
+    query: `mutation {
+      deployFIFSRegistrar(
+        registryAddress: "${ensAddress}"
+        tld: "${domain}"
+        connection: {
+          networkNameOrChainId: "${network}"
+        }
+      )
+    }`
+  });
+
+  if (!deployFifs.errors) {
+    console.log("Deployed FIFSRegistrar!")
+    console.log(deployFifs.data?.deployFIFSRegistrar);
+  } else {
+    throw Error(`Failed to deploy FIFSRegistrar: ${deployFifs.errors}`);
+  }
+
+  const fifsAddress = deployFifs.data?.deployFIFSRegistrar;
+
+  // Set the subdomain's owner to the FIFSRegistrar
+  const setOwner = await client.query<{
+    setSubdomainOwner: string
+  }>({
+    uri,
+    query: `mutation {
+      setSubdomainOwner(
+        subdomain: "${domain}"
+        owner: "${fifsAddress}"
+        registryAddress: "${ensAddress}"
+        connection: {
+          networkNameOrChainId: "${network}"
+        }
+      )
+    }`
+  })
+
+  if (!setOwner.errors) {
+    console.log("Set Owner Succeeded!")
+    console.log(setOwner.data?.setSubdomainOwner);
+  } else {
+    throw Error(`Failed to Set Owner: ${setOwner.errors}`);
+  }
+}
+
+main()
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(() =>
+    process.exit(0)
+  );
+
+/*
+//PSEUDO-CODE Recipe to deploy and configure public subdomain
+x const ensAddress = "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e"
+x const publicResolverAddress = "0x..."
+x const label = "mysubdomain"
+const owner = "0x..."
+x const domain = "public.domain.eth"
+
+x const fifsRegistrarAddress = await deployFIFSRegistrar(ensAddress, domain)
+x await setSubdomainOwner(domain, fifsRegistrarAddress, ensAddress)
+*/
---
+++ demos/ens/protocol/script/create-domain.ts
@@ -0,0 +1,6 @@
+/*
+//PSEUDO-CODE Recipe to register subdomain under public subdomain
+
+await registerSubnodeOwnerWithFIFSRegistrar(label, owner, fifsRegistrarAddress)
+await setResolver(`${label}.${domain}`, publicResolverAddress, ensAddress)
+*/
