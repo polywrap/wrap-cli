95d0d1dfb89e6ab1adde90cd2eac582610119888
Alexander Bai <vhurryharry@gmail.com>
2021-05-04 21:52:48 -0400

    Updated server

--- packages/js/tracing/src/server.ts
+++ packages/js/tracing/src/server.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from "express";
 
 const app = express();
