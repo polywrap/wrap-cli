fdf5cf0ca73fe47fd766aee4f2394022108f89f0
Alexander Bai <vhurryharry@gmail.com>
2021-05-04 20:25:23 -0400

    Added logging server

---
+++ packages/js/tracing/server/src/server.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import express from "express";
+
+const app = express();
+const PORT = 4040;
+
+app.use(express.json());
+
+let level = "debug";
+
+app.get("/level", (req: express.Request, res: express.Response) => {
+  res.send(level);
+});
+
+app.post("/level", (req: express.Request, res: express.Response) => {
+  level = req.body.level;
+  res.send(level);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
