bfa5581d3630f5472ef6d43012f986fcd242f57a
krisbitney <kris@dorg.tech>
2021-05-17 12:50:57 -0500

    changed WETH symbol from WETH9 to WETH

--- packages/apis/uniswapv2/src/utils/utils.ts
+++ packages/apis/uniswapv2/src/utils/utils.ts
@@ -36,7 +36,7 @@ export function getWETH9(chainId: ChainId): Token {
         address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
         currency: {
           decimals: 18,
-          symbol: "WETH9",
+          symbol: "WETH",
           name: "Wrapped Ether",
         },
       };
@@ -46,7 +46,7 @@ export function getWETH9(chainId: ChainId): Token {
         address: "0xc778417E063141139Fce010982780140Aa0cD5Ab",
         currency: {
           decimals: 18,
-          symbol: "WETH9",
+          symbol: "WETH",
           name: "Wrapped Ether",
         },
       };
@@ -56,7 +56,7 @@ export function getWETH9(chainId: ChainId): Token {
         address: "0xc778417E063141139Fce010982780140Aa0cD5Ab",
         currency: {
           decimals: 18,
-          symbol: "WETH9",
+          symbol: "WETH",
           name: "Wrapped Ether",
         },
       };
@@ -66,7 +66,7 @@ export function getWETH9(chainId: ChainId): Token {
         address: "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6",
         currency: {
           decimals: 18,
-          symbol: "WETH9",
+          symbol: "WETH",
           name: "Wrapped Ether",
         },
       };
@@ -76,7 +76,7 @@ export function getWETH9(chainId: ChainId): Token {
         address: "0xd0A1E359811322d97991E03f863a0C30C2cF029C",
         currency: {
           decimals: 18,
-          symbol: "WETH9",
+          symbol: "WETH",
           name: "Wrapped Ether",
         },
       };
