fffdde50ae104c9ade16d732ba598a5f78b49438
krisbitney <kris@dorg.tech>
2021-05-29 12:23:10 -0700

    Updated Schema binding test case Query/serialization.ts for MsgPack Context

--- packages/test-cases/cases/bind/sanity/output/wasm-as/query/Query/serialization.ts
+++ packages/test-cases/cases/bind/sanity/output/wasm-as/query/Query/serialization.ts
@@ -5,7 +5,7 @@ import {
   WriteEncoder,
   Write,
   Nullable,
-  BigInt
+  BigInt, Context
 } from "@web3api/wasm-as";
 import * as Types from "..";
 
@@ -19,6 +19,7 @@ export class Input_queryMethod {
 }
 
 export function deserializequeryMethodArgs(argsBuf: ArrayBuffer): Input_queryMethod {
+  const context: Context =  new Context("Deserializing query-type: queryMethod");
   const reader = new ReadDecoder(argsBuf);
   var numFields = reader.readMapLength();
 
@@ -36,14 +37,20 @@ export function deserializequeryMethodArgs(argsBuf: ArrayBuffer): Input_queryMet
     numFields--;
     const field = reader.readString();
 
+    reader.context().push(field, "unknown", "searching for property type");
     if (field == "str") {
+      reader.context().push(field, "string", "type found, reading property");
       _str = reader.readString();
       _strSet = true;
+      reader.context().pop();
     }
     else if (field == "optStr") {
+      reader.context().push(field, "string | null", "type found, reading property");
       _optStr = reader.readNullableString();
+      reader.context().pop();
     }
     else if (field == "en") {
+      reader.context().push(field, "Types.CustomEnum", "type found, reading property");
       let value: Types.CustomEnum;
       if (reader.isNextString()) {
         value = Types.getCustomEnumValue(reader.readString());
@@ -53,8 +60,10 @@ export function deserializequeryMethodArgs(argsBuf: ArrayBuffer): Input_queryMet
       }
       _en = value;
       _enSet = true;
+      reader.context().pop();
     }
     else if (field == "optEnum") {
+      reader.context().push(field, "Nullable<Types.CustomEnum>", "type found, reading property");
       let value: Nullable<Types.CustomEnum>;
       if (!reader.isNextNil()) {
         if (reader.isNextString()) {
@@ -71,8 +80,10 @@ export function deserializequeryMethodArgs(argsBuf: ArrayBuffer): Input_queryMet
         value = Nullable.fromNull<Types.CustomEnum>();
       }
       _optEnum = value;
+      reader.context().pop();
     }
     else if (field == "enumArray") {
+      reader.context().push(field, "Array<Types.CustomEnum>", "type found, reading property");
       _enumArray = reader.readArray((reader: Read): Types.CustomEnum => {
         let value: Types.CustomEnum;
         if (reader.isNextString()) {
@@ -84,8 +95,10 @@ export function deserializequeryMethodArgs(argsBuf: ArrayBuffer): Input_queryMet
         return value;
       });
       _enumArraySet = true;
+      reader.context().pop();
     }
     else if (field == "optEnumArray") {
+      reader.context().push(field, "Array<Nullable<Types.CustomEnum>> | null", "type found, reading property");
       _optEnumArray = reader.readNullableArray((reader: Read): Nullable<Types.CustomEnum> => {
         let value: Nullable<Types.CustomEnum>;
         if (!reader.isNextNil()) {
@@ -104,6 +117,7 @@ export function deserializequeryMethodArgs(argsBuf: ArrayBuffer): Input_queryMet
         }
         return value;
       });
+      reader.context().pop()
     }
   }
 
@@ -128,16 +142,20 @@ export function deserializequeryMethodArgs(argsBuf: ArrayBuffer): Input_queryMet
 }
 
 export function serializequeryMethodResult(result: i32): ArrayBuffer {
-  const sizer = new WriteSizer();
+  const sizerContext: Context = new Context("Serializing (sizing) query-type: queryMethod");
+  const sizer = new WriteSizer(sizerContext);
   writequeryMethodResult(sizer, result);
   const buffer = new ArrayBuffer(sizer.length);
-  const encoder = new WriteEncoder(buffer);
+  const encoderContext: Context = new Context("Serializing (encoding) query-type: queryMethod");
+  const encoder = new WriteEncoder(buffer, encoderContext);
   writequeryMethodResult(encoder, result);
   return buffer;
 }
 
 export function writequeryMethodResult(writer: Write, result: i32): void {
+  writer.context().push("queryMethod", "i32", "writing property");
   writer.writeInt32(result);
+  writer.context().pop();
 }
 
 export class Input_objectMethod {
@@ -148,7 +166,8 @@ export class Input_objectMethod {
 }
 
 export function deserializeobjectMethodArgs(argsBuf: ArrayBuffer): Input_objectMethod {
-  const reader = new ReadDecoder(argsBuf);
+  const context: Context =  new Context("Deserializing query-type: objectMethod");
+  const reader = new ReadDecoder(argsBuf, context);
   var numFields = reader.readMapLength();
 
   var _object: Types.AnotherType | null = null;
@@ -162,26 +181,34 @@ export function deserializeobjectMethodArgs(argsBuf: ArrayBuffer): Input_objectM
     numFields--;
     const field = reader.readString();
 
+    reader.context().push(field, "unknown", "searching for property type");
     if (field == "object") {
+      reader.context().push(field, "Types.AnotherType", "type found, reading property");
       const object = Types.AnotherType.read(reader);
       _object = object;
       _objectSet = true;
+      reader.context().pop();
     }
     else if (field == "optObject") {
+      reader.context().push(field, "Types.AnotherType | null", "type found, reading property");
       var object: Types.AnotherType | null = null;
       if (!reader.isNextNil()) {
         object = Types.AnotherType.read(reader);
       }
       _optObject = object;
+      reader.context().pop();
     }
     else if (field == "objectArray") {
+      reader.context().push(field, "Array<Types.AnotherType>", "type found, reading property");
       _objectArray = reader.readArray((reader: Read): Types.AnotherType => {
         const object = Types.AnotherType.read(reader);
         return object;
       });
       _objectArraySet = true;
+      reader.context().pop();
     }
     else if (field == "optObjectArray") {
+      reader.context().push(field, "Array<Types.AnotherType | null> | null", "type found, reading property");
       _optObjectArray = reader.readNullableArray((reader: Read): Types.AnotherType | null => {
         var object: Types.AnotherType | null = null;
         if (!reader.isNextNil()) {
@@ -189,7 +216,9 @@ export function deserializeobjectMethodArgs(argsBuf: ArrayBuffer): Input_objectM
         }
         return object;
       });
+      reader.context().pop();
     }
+    reader.context().pop();
   }
 
   if (!_object || !_objectSet) {
@@ -208,18 +237,22 @@ export function deserializeobjectMethodArgs(argsBuf: ArrayBuffer): Input_objectM
 }
 
 export function serializeobjectMethodResult(result: Types.AnotherType | null): ArrayBuffer {
-  const sizer = new WriteSizer();
+  const sizerContext: Context = new Context("Serializing (sizing) query-type: objectMethod");
+  const sizer = new WriteSizer(sizerContext);
   writeobjectMethodResult(sizer, result);
   const buffer = new ArrayBuffer(sizer.length);
-  const encoder = new WriteEncoder(buffer);
+  const encoderContext: Context = new Context("Serializing (encoding) query-type: objectMethod");
+  const encoder = new WriteEncoder(buffer, encoderContext);
   writeobjectMethodResult(encoder, result);
   return buffer;
 }
 
 export function writeobjectMethodResult(writer: Write, result: Types.AnotherType | null): void {
+  writer.context().push("objectMethod", "Types.AnotherType | null", "writing property");
   if (result) {
     Types.AnotherType.write(writer, result as Types.AnotherType);
   } else {
     writer.writeNil();
   }
+  writer.context().pop();
 }
