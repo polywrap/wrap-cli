4e89d115ed55bc400f64c471583fa12277febe14
krisbitney <kris@dorg.tech>
2021-06-02 07:57:02 -0700

    fixed erroneously commented out mapping function in ethereum plugin

--- packages/js/plugins/ethereum/src/mapping.ts
+++ packages/js/plugins/ethereum/src/mapping.ts
@@ -21,21 +21,41 @@ export const mapTxReceipt = (
   cumulativeGasUsed: receipt.cumulativeGasUsed.toString(),
 });
 
-// export const mapTxResponse = (
-//   response: ethers.providers.TransactionResponse
-// ): TxResponse => ({
-//   hash: response.hash,
-//   blockNumber: response.blockNumber,
-//   blockHash: response.blockHash,
-//   timestamp: response.timestamp,
-//   confirmations: response.confirmations,
-//   from: response.from,
-//   raw: response.raw,
-//   nonce: response.nonce.toString(),
-//   gasLimit: response.gasLimit.toString(),
-//   gasPrice: response.gasPrice.toString(),
-//   data: response.data.toString(),
+// export const mapTxReceipt = (
+//   receipt: ethers.providers.TransactionReceipt
+// ): TxReceipt => ({
+//   to: receipt.to,
+//   from: receipt.from,
+//   contractAddress: receipt.contractAddress,
+//   transactionIndex: receipt.transactionIndex,
+//   root: receipt.root,
+//   gasUsed: receipt.gasUsed.toString(),
+//   logsBloom: receipt.logsBloom,
+//   blockHash: receipt.blockHash,
+//   transactionHash: receipt.transactionHash,
+//   logs: receipt.logs.map(mapLog),
+//   blockNumber: receipt.blockNumber.toString(),
+//   confirmations: receipt.confirmations,
+//   cumulativeGasUsed: receipt.cumulativeGasUsed.toString(),
+//   byzantium: receipt.byzantium,
+//   status: receipt.status ?? 0,
 // });
+
+export const mapTxResponse = (
+  response: ethers.providers.TransactionResponse
+): TxResponse => ({
+  hash: response.hash,
+  blockNumber: response.blockNumber,
+  blockHash: response.blockHash,
+  timestamp: response.timestamp,
+  confirmations: response.confirmations,
+  from: response.from,
+  raw: response.raw,
+  nonce: response.nonce.toString(),
+  gasLimit: response.gasLimit.toString(),
+  gasPrice: response.gasPrice.toString(),
+  data: response.data.toString(),
+});
 
 export const mapTxRequest = (
   request: ethers.providers.TransactionRequest
