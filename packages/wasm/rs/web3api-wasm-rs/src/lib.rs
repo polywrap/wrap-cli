69726bdffe05d170e2e1801ed59c0e4d55302bc5
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-06-14 16:17:40 +0000

    Bring `abort` `invoke` and `subinvoke` into lib scope

--- packages/wasm/rs/web3api-wasm-rs/src/lib.rs
+++ packages/wasm/rs/web3api-wasm-rs/src/lib.rs
@@ -1,19 +1,11 @@
-mod utils;
+//mod utils;
+pub mod abort;
+pub mod invoke;
+pub mod msgpack;
+pub mod subinvoke;
 
-use wasm_bindgen::prelude::*;
-
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, web3api-wasm-rs!");
-}
+pub use msgpack::{
+    context::Context, read::Read, read_decoder::ReadDecoder, write::Write,
+    write_encoder::WriteEncoder, write_sizer::WriteSizer,
+};
+pub type Result = std::result::Result<(), failure::Error>;
