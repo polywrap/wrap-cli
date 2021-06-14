6540fd08c4b0790116e04ea9a599e9d4ec70972d
dOrgJelli <jelli@dorg.tech>
2021-06-14 00:32:03 -0500

    increase timeout

--- packages/js/plugins/ethereum/src/__tests__/e2e.spec.ts
+++ packages/js/plugins/ethereum/src/__tests__/e2e.spec.ts
@@ -16,7 +16,7 @@ import axios from "axios"
 
 const { hash: namehash } = require("eth-ens-namehash");
 
-jest.setTimeout(60000)
+jest.setTimeout(120000)
 
 describe("Ethereum Plugin", () => {
   let client: Web3ApiClient;
