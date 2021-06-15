4d78e4247f70c76386712474945b09755406c4f7
dOrgJelli <jelli@dorg.tech>
2021-06-15 00:19:11 -0500

    remove import_redirects + fix idna + sha3 plugins

--- packages/js/plugins/sha3/src/manifest.ts
+++ packages/js/plugins/sha3/src/manifest.ts
@@ -1,9 +1,26 @@
-import { PluginManifest, Uri } from "@web3api/core-js";
+import { PluginManifest } from "@web3api/core-js";
 
 export const manifest: PluginManifest = {
   // TODO: use the schema.graphql
   // https://github.com/Web3-API/prototype/issues/101
-  schema: "type Query { dummy: String }",
-  implemented: [new Uri("w3/api-resolver")],
+  schema:
+`type Query {
+  sha3_512(message: String!): String!
+  sha3_384(message: String!): String!
+  sha3_256(message: String!): String!
+  sha3_224(message: String!): String!
+
+  keccak_512(message: String!): String!
+  keccak_384(message: String!): String!
+  keccak_256(message: String!): String!
+  keccak_224(message: String!): String!
+
+  buffer_keccak_256(message: String!): String!
+  uint8array_keccak_256(message: String!): String!
+
+  shake_128(message: String!, outputBits: Int!): String!
+  shake_256(message: String!, outputBits: Int!): String!
+}`,
+  implemented: [],
   imported: [],
 };
