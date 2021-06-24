6582b21d192d6dfb67bb9a220c02499db9d39db1
krisbitney <kris@dorg.tech>
2021-06-24 15:49:40 -0500

    removed possible undefined instantiation of type in schema-compose test

--- packages/schema/compose/src/__tests__/index.ts
+++ packages/schema/compose/src/__tests__/index.ts
@@ -139,7 +139,7 @@ export function fetchTestCases(): TestCases {
         combined: schemaSchema && schemaTypeInfo ? {
           schema: schemaSchema,
           typeInfo: schemaTypeInfo
-        } : undefined
+        } : {}
       },
     };
   };
