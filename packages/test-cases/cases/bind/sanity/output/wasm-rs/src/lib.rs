b88b9a69d473dd990cb786b37d0808be0fc5dd2f
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-06-25 08:53:18 +0000

    feat(test-cases): Init

--- packages/test-cases/cases/bind/sanity/output/wasm-rs/src/lib.rs
+++ packages/test-cases/cases/bind/sanity/output/wasm-rs/src/lib.rs
@@ -1,8 +1,8 @@
-pub use web3api_wasm_rs::{abort, invoke, subinvoke};
-pub use web3api_wasm_rs::{Context, Read, ReadDecoder, Write, WriteEncoder, WriteSizer};
+pub use polywrap_wasm_rs::{abort, invoke, subinvoke};
+pub use polywrap_wasm_rs::{Context, Read, ReadDecoder, Write, WriteEncoder, WriteSizer};
 
-mod mutation;
-mod query;
+//mod mutation;
+//mod query;
 
-pub use query::{AnotherType, CustomEnum, CustomType};
-pub use query::{TestImportAnotherObject, TestImportEnum, TestImportObject, TestImportQuery};
+//pub use query::{AnotherType, CustomEnum, CustomType};
+//pub use query::{TestImportAnotherObject, TestImportEnum, TestImportObject, TestImportQuery};
--- packages/test-cases/cases/bind/sanity/output/wasm-rs/src/query/imported/mod.rs
+++
@@ -1,9 +0,0 @@
-mod test_import_another_object;
-mod test_import_enum;
-mod test_import_object;
-mod test_import_query;
-
-pub use test_import_another_object::TestImportAnotherObject;
-pub use test_import_enum::TestImportEnum;
-pub use test_import_object::TestImportObject;
-pub use test_import_query::TestImportQuery;
