4b7f7f5556834fb730245e42b5aaf92b2133ab81
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-06-25 08:03:32 +0000

    feat(polywrap-wasm-rs): Add initial setup for subinvoke-invoke-abort API

--- packages/wasm/rs/src/lib.rs
+++ packages/wasm/rs/src/lib.rs
@@ -35,10 +35,10 @@
 )]
 */
 
-//pub mod abort;
-//pub mod invoke;
+pub mod abort;
+pub mod invoke;
 pub mod msgpack;
-//pub mod subinvoke;
+pub mod subinvoke;
 
 pub use msgpack::{
     context::Context, read::Read, read_decoder::ReadDecoder, write::Write,
