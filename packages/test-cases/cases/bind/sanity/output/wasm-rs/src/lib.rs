a6365ced96f8975f5df806cda30722ff1d18cce5
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-06-25 08:58:05 +0000

    feat(test-cases): Add CustomEnum for `Query`

--- packages/test-cases/cases/bind/sanity/output/wasm-rs/src/lib.rs
+++ packages/test-cases/cases/bind/sanity/output/wasm-rs/src/lib.rs
@@ -2,7 +2,7 @@ pub use polywrap_wasm_rs::{abort, invoke, subinvoke};
 pub use polywrap_wasm_rs::{Context, Read, ReadDecoder, Write, WriteEncoder, WriteSizer};
 
 //mod mutation;
-//mod query;
+mod query;
 
 //pub use query::{AnotherType, CustomEnum, CustomType};
 //pub use query::{TestImportAnotherObject, TestImportEnum, TestImportObject, TestImportQuery};
