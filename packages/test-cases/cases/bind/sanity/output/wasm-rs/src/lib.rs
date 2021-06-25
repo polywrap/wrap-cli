b1023e505f734c4e5953e4a3e379bbc5de407aee
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-06-25 09:18:32 +0000

    feat(test-cases): Implement `Mutation`

--- packages/test-cases/cases/bind/sanity/output/wasm-rs/src/lib.rs
+++ packages/test-cases/cases/bind/sanity/output/wasm-rs/src/lib.rs
@@ -1,8 +1,8 @@
 pub use polywrap_wasm_rs::{abort, invoke, subinvoke};
 pub use polywrap_wasm_rs::{Context, Read, ReadDecoder, Write, WriteEncoder, WriteSizer};
 
-//mod mutation;
+mod mutation;
 mod query;
 
-//pub use query::{AnotherType, CustomEnum, CustomType};
-//pub use query::{TestImportAnotherObject, TestImportEnum, TestImportObject, TestImportQuery};
+pub use query::{AnotherType, CustomEnum, CustomType};
+pub use query::{TestImportAnotherObject, TestImportEnum, TestImportObject, TestImportQuery};
