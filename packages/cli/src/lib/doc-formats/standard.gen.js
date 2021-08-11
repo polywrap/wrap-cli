98008ac4bc219515fff8aa3441bfaeee8524b5f0
krisbitney <kris@dorg.tech>
2021-08-11 15:07:45 -0600

    fixed directory location for docgen output when using html/css template

--- packages/cli/src/lib/doc-formats/standard.gen.js
+++ packages/cli/src/lib/doc-formats/standard.gen.js
@@ -1,18 +1,12 @@
 exports.run = (output, config) => {
   output.entries.push({
-    type: "Directory",
-    name: "./docs",
-    data: [
-      {
-        type: "File",
-        name: "./index.html",
-        data: config.generate("standard.mustache", config.typeInfo),
-      },
-      {
-        type: "File",
-        name: "./style.css",
-        data: config.generate("standard-style.mustache", config.typeInfo),
-      },
-    ],
+    type: "File",
+    name: "./index.html",
+    data: config.generate("standard.mustache", config.typeInfo),
+  });
+  output.entries.push({
+    type: "File",
+    name: "./style.css",
+    data: config.generate("standard-style.mustache", config.typeInfo),
   });
 };
