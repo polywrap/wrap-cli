c57b17df689e217311bb9f9ed68e0ee5aad44a92
Mak Muftic <mak@nodefactory.io>
2021-08-18 15:42:01 +0200

    Restructure execution of ipfs order

--- packages/apis/ipfs/src/query/index.ts
+++ packages/apis/ipfs/src/query/index.ts
@@ -8,6 +8,87 @@ import { IpfsError } from "../error";
 import { Nullable } from "@web3api/wasm-as";
 import { IpfsOptions } from "./w3/IpfsOptions";
 
+export function catFileToString(input: Input_catFileToString): String {
+  const result = executeOperation(
+    input.ipfs,
+    createRequest(input.catFileOptions, input.cid, Http_ResponseType.TEXT),
+    "catFileToString",
+    "/api/v0/cat"
+  );
+  return result.data;
+}
+
+export function catFile(input: Input_catFile): ArrayBuffer {
+  const result = executeOperation(
+    input.ipfs,
+    createRequest(input.catFileOptions, input.cid, Http_ResponseType.BINARY),
+    "catFile",
+    "/api/v0/cat"
+  );
+  return decode(result.data).buffer;
+}
+
+class ExecutionResult {
+  data: string;
+  provider: string;
+}
+
+function executeOperation(
+  ipfs: IpfsOptions,
+  request: Http_Request,
+  operation: string,
+  operationUrl: string
+): ExecutionResult {
+  const provider = ipfs.provider;
+  const fallbackProviders = ipfs.fallbackProviders != null ? ipfs.fallbackProviders as string[] : [];
+
+  let complete = false;
+  let fallbackIdx = -1;
+  let result: string;
+
+  while (!complete) {
+    const url = provider + operationUrl;
+    const response = Http_Query.get({
+      url: url,
+      request: request
+    });
+
+    // no response - shouldn't happen
+    if (response == null) {
+      throw new IpfsError(operation, response.status.value, response.statusText);
+    }
+
+    // error happend in http plugin
+    if (response.error != null) {
+      const err = response.error as Http_ResponseError;
+      if (err.timeoutExcided) {
+        fallbackIdx += 1;
+        if (fallbackIdx >= fallbackProviders.length) {
+          complete = true;
+        }
+        continue;
+      } else {
+        throw new IpfsError(operation, null, null, err.errorMessage);
+      }
+    }
+
+    // not 200 response error
+    if (response.status.value != 200) {
+      throw new IpfsError(operation, response.status.value, response.statusText);
+    }
+
+    // succesfull request
+    result = response.body as string;
+    complete = true;
+  }
+
+  if (!result) {
+    throw new IpfsError("Timeout has been exceeded, and all providers have been exhausted."); // TODO
+  }
+
+  return { data: result, provider }
+}
+
 function createRequest(catFileOptions: CatFileOptions | null, cid: string, responseType: Http_ResponseType): Http_Request {
   let headers: Http_Header[];
   let urlParams: Http_UrlParam[];
@@ -46,75 +127,4 @@ function createRequest(catFileOptions: CatFileOptions | null, cid: string, respo
     body: null,
     timeout: timeout
   }
-}
-
-export function catFileToString(input: Input_catFileToString): String {
-  const result = executeCatOperation(
-    input.ipfs,
-    input.cid,
-    input.catFileOptions,
-    Http_ResponseType.TEXT
-  );
-  return result;
-}
-
-export function catFile(input: Input_catFile): ArrayBuffer {
-  const result = executeCatOperation(
-    input.ipfs,
-    input.cid,
-    input.catFileOptions,
-    Http_ResponseType.BINARY
-  );
-  return decode(result).buffer;
-}
-
-export function executeCatOperation(ipfs: IpfsOptions, cid: string, catFileOptions: CatFileOptions | null, responseType: Http_ResponseType): string {
-  // make request
-  const provider = ipfs.provider;
-  const fallbackProviders = ipfs.fallbackProviders != null ? ipfs.fallbackProviders as string[] : [];
-  let complete = false;
-  let fallbackIdx = -1;
-  let result: string;
-  
-  while (!complete) {
-    const url = provider + "/api/v0/cat";
-    const response = Http_Query.get({
-      url: url,
-      request: createRequest(catFileOptions, cid, responseType)
-    });
-
-    // no response - shouldn't happen
-    if (response == null) {
-      throw new IpfsError("catFile", response.status.value, response.statusText);
-    }
-
-    // error happend in http plugin
-    if (response.error != null) {
-      const err = response.error as Http_ResponseError;
-      if (err.timeoutExcided) {
-        fallbackIdx += 1;
-        if(fallbackIdx >= fallbackProviders.length) {
-          complete = true;
-        }
-        continue;
-      } else {
-        throw new IpfsError("catFile", null, null, err.errorMessage);
-      }
-    }
-
-    // not 200 response error
-    if (response.status.value != 200) {
-      throw new IpfsError("catFile", response.status.value, response.statusText);
-    }
-
-    // succesfull request
-    result = response.body as string;
-    complete = true;
-  }
-
-  if(!result) {
-    throw new IpfsError("Timeout has been exceeded, and all providers have been exhausted."); // TODO
-  }
-
-  return result;
 }
