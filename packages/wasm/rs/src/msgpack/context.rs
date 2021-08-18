6bce985346996d88eb340b45f01f5edaad6c1037
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-08-18 05:00:57 +0000

    Remove error handling from Context `pop()` function

--- packages/wasm/rs/src/msgpack/context.rs
+++ packages/wasm/rs/src/msgpack/context.rs
@@ -35,15 +35,22 @@ impl Context {
         self.nodes.push(node);
     }
 
-    pub fn pop(&mut self) -> Result<String, String> {
+    // pub fn _pop(&mut self) -> Result<String, String> {
+    //     if self.is_empty() {
+    //         return Err("Error: tried to pop an item from an empty Context stack".to_string());
+    //     }
+    //     let node = self.nodes.pop().unwrap_or_default();
+    //     Ok(format!(
+    //         "{} : {} >> {}",
+    //         node.node_item, node.node_type, node.node_info
+    //     ))
+    // }
+
+    pub fn pop(&mut self) {
         if self.is_empty() {
-            return Err("Error: tried to pop an item from an empty Context stack".to_string());
+            panic!("Error: tried to pop an item from an empty Context stack");
         }
-        let node = self.nodes.pop().unwrap_or_default();
-        Ok(format!(
-            "{} : {} >> {}",
-            node.node_item, node.node_type, node.node_info
-        ))
+        let _ = self.nodes.pop();
     }
 
     pub fn context_to_string(&self) -> String {
