8a8858828dac51030f0bc9bc5b02266deed2c271
dOrgJelli <jelli@dorg.tech>
2021-08-30 18:53:10 -0500

    fully built inside docker, testing next

--- packages/test-cases/cases/apis/rust-sanity/manual-build.sh
+++
@@ -1,4 +0,0 @@
-# Build the module at {{dir}}
-cd query && cargo build --target wasm32-unknown-unknown
-
-wasm-opt -O2 --asyncify
--- packages/test-cases/cases/apis/rust-sanity/mutation/src/lib.rs
+++ packages/test-cases/cases/apis/rust-sanity/mutation/src/lib.rs
@@ -1,1 +1,7 @@
+pub mod w3;
+use w3::*;
+pub use w3::entry;
 
+fn method(input: InputMethod) -> String {
+  format!("{}", input.arg)
+}
