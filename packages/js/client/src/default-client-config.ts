8b43954ced804f8e53d810a3c1e6d5650c01246c
dOrgJelli <jelli@dorg.tech>
2021-09-01 21:00:20 -0500

    Add polywrap-dev pinata gateway as fallback IPFS provider

--- packages/js/client/src/default-client-config.ts
+++ packages/js/client/src/default-client-config.ts
@@ -19,7 +19,12 @@ export const getDefaultClientConfig = Tracer.traceFunc(
         // IPFS is required for downloading Web3API packages
         {
           uri: new Uri("w3://ens/ipfs.web3api.eth"),
-          plugin: ipfsPlugin({ provider: "https://ipfs.io" }),
+          plugin: ipfsPlugin({
+            provider: "https://ipfs.io",
+            fallbackProviders: [
+              "https://polywrap-dev.mypinata.cloud/"
+            ]
+          }),
         },
         // ENS is required for resolving domain to IPFS hashes
         {
