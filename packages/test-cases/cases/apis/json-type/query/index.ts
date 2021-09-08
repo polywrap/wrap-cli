a3f68fe0e2bfec281e2aa531983782423c0c3d6f
dOrgJelli <jelli@dorg.tech>
2021-09-08 16:04:40 -0500

    update json-type test-case

--- packages/test-cases/cases/apis/json-type/query/index.ts
+++ packages/test-cases/cases/apis/json-type/query/index.ts
@@ -1,24 +1,23 @@
-import { Input_parse, Input_methodString, Input_methodJSON } from "./w3";
-import { JSON as ASJSON } from "@web3api/wasm-as";
+import {
+  Input_parse,
+  Input_methodString,
+  Input_methodJSON
+} from "./w3";
+import { JSON } from "@web3api/wasm-as";
 
-export function parse(input: Input_parse): ASJSON.Obj {
-  return <ASJSON.Obj>ASJSON.parse(input.value);
+export function parse(input: Input_parse): JSON.Value {
+  return JSON.parse(input.value);
 }
 
-export function methodString(input: Input_methodString): string {
-  const result = ASJSON.Value.Object();
-  result.set("valueA", ASJSON.from(input.valueA));
-  result.set("valueB", ASJSON.from(input.valueB));
-  result.set("valueC", ASJSON.from(input.valueC));
-
-  return result.stringify();
+export function stringify(input: Input_methodString): string {
+  return input.value.stringify();
 }
 
-export function methodJSON(input: Input_methodJSON): ASJSON.Obj {
-  const result = ASJSON.Value.Object();
-  result.set("valueA", ASJSON.from(input.valueA));
-  result.set("valueB", ASJSON.from(input.valueB));
-  result.set("valueC", ASJSON.from(input.valueC));
+export function methodJSON(input: Input_methodJSON): JSON.Value {
+  const result = JSON.Value.Object();
+  result.set("valueA", JSON.from(input.valueA));
+  result.set("valueB", JSON.from(input.valueB));
+  result.set("valueC", JSON.from(input.valueC));
 
   return result;
 }
