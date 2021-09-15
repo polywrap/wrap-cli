c5988594ed287bd2a5f3821c9ef66d9be98a0ff0
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-09-15 17:45:16 +0000

    Use the `no-std` version of the wasm runtime in `rust-sanity`

--- packages/test-cases/cases/apis/rust-sanity/mutation/src/lib.rs
+++ packages/test-cases/cases/apis/rust-sanity/mutation/src/lib.rs
@@ -1,7 +1,30 @@
+#![no_std]
+
 pub mod w3;
-use w3::*;
 pub use w3::entry;
+use w3::*;
+
+extern crate alloc;
+extern crate std;
+use std::alloc::{GlobalAlloc, Layout, System};
+
+struct PolywrapAlloc;
+
+unsafe impl GlobalAlloc for PolywrapAlloc {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        System.alloc(layout)
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        System.dealloc(ptr, layout)
+    }
+}
+
+#[global_allocator]
+static GLOBAL: PolywrapAlloc = PolywrapAlloc;
+
+use alloc::{format, string::String};
 
 fn method(input: InputMethod) -> String {
-  format!("{}", input.arg)
+    format!("{}", input.arg)
 }
