19322fcea61e198804c6ba88d2eb10e3f242f7c4
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-09-24 11:08:54 +0000

    Temporarily introduce Cargo

---
+++ packages/test-cases/cases/bind/sanity/output/wasm-rs-no-std/query/src/another_type/mod.rs
@@ -0,0 +1,41 @@
+pub mod serialization;
+use polywrap_wasm_rs::{
+    Read,
+    Write,
+};
+use serde::{
+    Deserialize,
+    Serialize,
+};
+pub use serialization::{
+    deserialize_another_type,
+    read_another_type,
+    serialize_another_type,
+    write_another_type,
+};
+
+use crate::CustomType;
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct AnotherType {
+    pub prop: Option<String>,
+    pub circular: Option<Box<CustomType>>,
+}
+
+impl AnotherType {
+    pub fn to_buffer(input: &AnotherType) -> Vec<u8> {
+        serialize_another_type(input)
+    }
+
+    pub fn from_buffer(input: &[u8]) -> AnotherType {
+        deserialize_another_type(input)
+    }
+
+    pub fn write<W: Write>(input: &AnotherType, writer: &mut W) {
+        write_another_type(input, writer);
+    }
+
+    pub fn read<R: Read>(reader: &mut R) -> AnotherType {
+        read_another_type(reader).expect("Failed to read AnotherType")
+    }
+}
