0e286061e3cf1709db7fb30c5c3e749b4f9ce557
krisbitney <kristoferbitney@hotmail.com>
2021-10-07 11:50:07 -0500

    added tests for dapp.ts and ensured CLI tests are passing for all CLI commands

--- packages/cli/src/commands/dapp.ts
+++ packages/cli/src/commands/dapp.ts
@@ -6,14 +6,24 @@ import {
   resolveManifestPath,
   validateCodegenParams,
 } from "./codegen";
+import { fixParameters } from "../lib/helpers";
 
 import chalk from "chalk";
 import { GluegunToolbox } from "gluegun";
 
-const typescriptGenerationFile =
-  __dirname + "/../lib/codegen-templates/types-ts.gen.js";
-const defaultGenerationFile = typescriptGenerationFile;
-const defaultManifest = ["web3api.yaml", "web3api.yml"];
+interface DappLang {
+  lang: string;
+  typeGenFile: string;
+}
+
+const langSupport: DappLang[] = [
+  {
+    lang: "typescript",
+    typeGenFile: __dirname + "/../lib/codegen-templates/types-ts.gen.js",
+  },
+];
+
+export const defaultManifest = ["web3api.yaml", "web3api.yml"];
 
 const cmdStr = intlMsg.commands_plugin_options_command();
 const optionsStr = intlMsg.commands_options_options();
@@ -24,9 +34,8 @@ const outputDirStr = `${intlMsg.commands_plugins_options_types({
   default: defaultOutputTypesStr,
 })}`;
 const codegenLang = intlMsg.commands_plugin_options_lang();
-const defaultLang = "typescript";
 const outputLangStr = `${intlMsg.commands_dapp_options_l({
-  default: defaultLang,
+  default: langSupport[0].lang,
 })}`;
 const nodeStr = intlMsg.commands_codegen_options_i_node();
 const pathStr = intlMsg.commands_codegen_options_o_path();
@@ -39,7 +48,7 @@ Commands:
   ${chalk.bold(
     "codegen"
   )}   ${codegenStr} (${intlMsg.commands_dapp_supported()}: typescript)
-  
+
 ${optionsStr[0].toUpperCase() + optionsStr.slice(1)}:
   -h, --help                              ${intlMsg.commands_codegen_options_h()}
   -m, --manifest-path <${pathStr}>              ${intlMsg.commands_codegen_options_m()}: ${defaultManifestStr})
@@ -65,7 +74,37 @@ export default {
     ipfs = ipfs || i;
     ens = ens || e;
 
-    if (help || !validateCodegenParams(print, outputDir, ens, false)) {
+    let command = "";
+    try {
+      const params = parameters;
+      [command] = fixParameters(
+        {
+          options: params.options,
+          array: params.array,
+        },
+        {
+          h,
+          help,
+        }
+      );
+    } catch (e) {
+      print.error(e.message);
+      process.exitCode = 1;
+      return;
+    }
+
+    if (help) {
+      print.info(HELP);
+      return;
+    }
+
+    if (!command) {
+      print.error(intlMsg.commands_plugin_error_noCommand());
+      print.info(HELP);
+      return;
+    }
+
+    if (!validateCodegenParams(print, outputDir, ens, false)) {
       print.info(HELP);
       return;
     }
@@ -73,11 +112,24 @@ export default {
     const { ipfsProvider, ethProvider } = await getCodegenProviders(ipfs);
     const ensAddress: string | undefined = ens;
 
-    // Resolve generation file & output directories
-    const customScript =
-      lang && lang.toLowerCase() === "typescript"
-        ? filesystem.resolve(typescriptGenerationFile)
-        : filesystem.resolve(defaultGenerationFile);
+    // Resolve generation file
+    let selectedLang: DappLang | undefined;
+    if (typeof lang === "string") {
+      const requestedLang = lang.trim().toLowerCase();
+      selectedLang = langSupport.find(
+        (supported: DappLang) => requestedLang === supported.lang
+      );
+    } else if (!lang) {
+      selectedLang = langSupport[0];
+    }
+    if (!selectedLang) {
+      print.error(intlMsg.commands_dapp_error_noLang());
+      print.info(HELP);
+      return;
+    }
+    const customScript = filesystem.resolve(selectedLang.typeGenFile);
+
+    // Resolve manifest and output directories
     manifestPath = await resolveManifestPath(filesystem, manifestPath);
     outputDir =
       (outputDir && filesystem.resolve(outputDir)) || filesystem.path("types");
