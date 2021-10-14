1cfd9ac8605ef962679a1eebd156f7e77046b523
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-10-14 10:56:00 +0000

    Temporarily make the `rust-sanity` project a `std` crate

--- packages/test-cases/cases/apis/rust-sanity/mutation/src/lib.rs
+++ packages/test-cases/cases/apis/rust-sanity/mutation/src/lib.rs
@@ -1,29 +1,29 @@
-// Set up for no-std.
-#![no_std]
-// Currently, no_std requires the nightly compiler due to the crates that it uses.
-#![feature(alloc_error_handler, core_intrinsics, lang_items)]
+// // Set up for no-std.
+// #![no_std]
+// // Currently, no_std requires the nightly compiler due to the crates that it uses.
+// #![feature(alloc_error_handler, core_intrinsics, lang_items)]
 
-// Set up the global allocator.
+// // Set up the global allocator.
 extern crate alloc;
-extern crate wee_alloc;
+// extern crate wee_alloc;
 
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+// #[global_allocator]
+// static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-// Set up panic and error handlers
-#[alloc_error_handler]
-fn err_handler(_: core::alloc::Layout) -> ! {
-    core::intrinsics::abort();
-}
+// // Set up panic and error handlers
+// #[alloc_error_handler]
+// fn err_handler(_: core::alloc::Layout) -> ! {
+//     core::intrinsics::abort();
+// }
 
-#[panic_handler]
-#[lang = "panic_impl"]
-extern "C" fn rust_begin_panic(_: &core::panic::PanicInfo) -> ! {
-    core::intrinsics::abort();
-}
+// #[panic_handler]
+// #[lang = "panic_impl"]
+// extern "C" fn rust_begin_panic(_: &core::panic::PanicInfo) -> ! {
+//     core::intrinsics::abort();
+// }
 
-#[lang = "eh_personality"]
-extern "C" fn eh_personality() {}
+// #[lang = "eh_personality"]
+// extern "C" fn eh_personality() {}
 
 use alloc::{format, string::String};
 pub mod w3;
