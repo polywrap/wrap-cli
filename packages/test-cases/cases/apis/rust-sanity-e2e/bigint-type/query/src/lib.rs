da60e91a5a6715f5fefcb26136dc941cb268504d
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-10-18 11:00:48 +0000

    Complete the `bigint-type` e2e

--- packages/test-cases/cases/apis/rust-sanity-e2e/bigint-type/query/src/lib.rs
+++ packages/test-cases/cases/apis/rust-sanity-e2e/bigint-type/query/src/lib.rs
@@ -1,10 +1,20 @@
 extern crate alloc;
 
-use alloc::{format, string::String};
+use std::ops::Mul;
+
 use polywrap_wasm_rs::BigInt;
 pub mod w3;
 pub use w3::*;
 
 pub fn big_int_method(input: InputBigIntMethod) -> BigInt {
-    todo!()
+    let mut result = input.arg1.mul(input.obj.prop1);
+
+    if input.arg2.is_some() {
+        result = result.mul(input.arg2.unwrap());
+    }
+    if input.obj.prop2.is_some() {
+        result = result.mul(input.obj.prop2.unwrap());
+    }
+
+    result
 }
