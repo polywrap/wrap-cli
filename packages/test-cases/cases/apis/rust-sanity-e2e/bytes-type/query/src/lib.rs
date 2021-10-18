3ab63b8848ca48647aa1ccaa9b4160e79203c1df
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-10-18 11:16:16 +0000

    Complete the `bytes-type` e2e

--- packages/test-cases/cases/apis/rust-sanity-e2e/bytes-type/query/src/lib.rs
+++ packages/test-cases/cases/apis/rust-sanity-e2e/bytes-type/query/src/lib.rs
@@ -1,9 +1,11 @@
 extern crate alloc;
 
-use alloc::{format, string::String};
+use alloc::string::String;
 pub mod w3;
 pub use w3::*;
 
 pub fn bytes_method(input: InputBytesMethod) -> Vec<u8> {
-    todo!()
+    let arg_str = String::from_utf8(input.arg.prop).unwrap();
+    let sanity_str = [&arg_str, " Sanity!"].concat();
+    String::into_bytes(sanity_str)
 }
