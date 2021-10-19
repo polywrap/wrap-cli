05d9f5965d176a242be5e3d8126909a70a68ac2b
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-10-19 15:48:37 +0000

    Add `implementations` api to wasm-rs apis

---
+++ packages/test-cases/cases/apis/wasm-rs/implementations/test-api/mutation/src/lib.rs
@@ -0,0 +1,10 @@
+pub mod w3;
+pub use w3::*;
+
+pub fn mutation_method(input: InputMutationMethod) -> u8 {
+    todo!()
+}
+
+pub fn abstract_mutation_method(input: InputAbstractMutationMethod) -> u8 {
+    todo!()
+}
---
+++ packages/test-cases/cases/apis/wasm-rs/implementations/test-api/query/src/lib.rs
@@ -0,0 +1,10 @@
+pub mod w3;
+pub use w3::*;
+
+pub fn query_method(input: InputQueryMethod) -> ImplementationType {
+    todo!()
+}
+
+pub fn abstract_query_method(input: InputAbstractQueryMethod) -> String {
+    todo!()
+}
