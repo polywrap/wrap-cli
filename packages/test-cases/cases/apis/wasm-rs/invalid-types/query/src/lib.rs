04e57971e69930ea65793ff417a9d334455d7d9d
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-10-19 16:12:42 +0000

    Add `invalid-types` api to wasm-rs apis

---
+++ packages/test-cases/cases/apis/wasm-rs/invalid-types/query/src/lib.rs
@@ -0,0 +1,22 @@
+pub mod w3;
+pub use w3::*;
+
+pub fn bool_method(input: InputBoolMethod) -> bool {
+    todo!()
+}
+
+pub fn int_method(input: InputIntMethod) -> i32 {
+    todo!()
+}
+
+pub fn uint_method(input: InputUintMethod) -> u32 {
+    todo!()
+}
+
+pub fn bytes_method(input: InputBytesMethod) -> Vec<u8> {
+    todo!()
+}
+
+pub fn array_method(input: InputArrayMethod) -> Vec<String> {
+    todo!()
+}
