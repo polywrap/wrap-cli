25776b80d869b4cc85a8859561f06f72d8143137
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-10-19 16:37:53 +0000

    Add `json-type` api to wasm-rs apis

---
+++ packages/test-cases/cases/apis/wasm-rs/json-type/query/src/lib.rs
@@ -0,0 +1,19 @@
+pub mod w3;
+use polywrap_wasm_rs::JSON;
+pub use w3::*;
+
+pub fn parse(input: InputParse) -> JSON::Value {
+    todo!()
+}
+
+pub fn stringify(input: InputStringify) -> String {
+    todo!()
+}
+
+pub fn stringify_object(input: InputStringifyObject) -> String {
+    todo!()
+}
+
+pub fn method_json(input: InputMethodJson) -> JSON::Value {
+    todo!()
+}
