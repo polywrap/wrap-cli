4b09470d8f45bb53256238b71378bffbd551335c
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-10-19 17:11:07 +0000

    Add `number-types` api to wasm-rs apis

---
+++ packages/test-cases/cases/apis/wasm-rs/number-types/query/src/lib.rs
@@ -0,0 +1,26 @@
+pub mod w3;
+pub use w3::*;
+
+pub fn i8_method(input: InputI8Method) -> i8 {
+    todo!()
+}
+
+pub fn u8_method(input: InputU8Method) -> u8 {
+    todo!()
+}
+
+pub fn i16_method(input: InputI16Method) -> i16 {
+    todo!()
+}
+
+pub fn u16_method(input: InputU16Method) -> u16 {
+    todo!()
+}
+
+pub fn i32_method(input: InputI32Method) -> i32 {
+    todo!()
+}
+
+pub fn u32_method(input: InputU32Method) -> u32 {
+    todo!()
+}
