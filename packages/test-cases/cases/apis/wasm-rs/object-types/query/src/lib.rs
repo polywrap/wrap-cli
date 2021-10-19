34f99ed21e02974b8065cccd57606826701966d9
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-10-19 17:26:17 +0000

    Add `object-types` api to wasm-rs apis

---
+++ packages/test-cases/cases/apis/wasm-rs/object-types/query/src/lib.rs
@@ -0,0 +1,18 @@
+pub mod w3;
+pub use w3::*;
+
+pub fn method1(input: InputMethod1) -> Vec<Output> {
+    todo!()
+}
+
+pub fn method2(input: InputMethod2) -> Option<Output> {
+    todo!()
+}
+
+pub fn method3(input: InputMethod3) -> Vec<Option<Output>> {
+    todo!()
+}
+
+pub fn method5(input: InputMethod5) -> Output {
+    todo!()
+}
