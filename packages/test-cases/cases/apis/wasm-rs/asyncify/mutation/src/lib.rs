be854a1b509b14175d70848677656f7c9080cb48
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-10-20 04:13:12 +0000

    Add `asyncify` api to wasm-rs apis

---
+++ packages/test-cases/cases/apis/wasm-rs/asyncify/mutation/src/lib.rs
@@ -0,0 +1,30 @@
+pub mod w3;
+pub use w3::*;
+
+pub fn set_data_with_large_args(input: InputSetDataWithLargeArgs) -> String {
+    todo!()
+}
+
+pub fn set_data_with_many_args(input: InputSetDataWithManyArgs) -> String {
+    todo!()
+}
+
+pub fn set_data_with_many_structured_args(input: InputSetDataWithManyStructuredArgs) -> bool {
+    todo!()
+}
+
+pub fn deploy_contract(input: InputDeployContract) -> String {
+    todo!()
+}
+
+pub fn local_var_method(input: InputLocalVarMethod) -> bool {
+    todo!()
+}
+
+pub fn global_var_method(input: InputGlobalVarMethod) -> bool {
+    todo!()
+}
+
+pub fn subsequent_invokes(input: InputSubsequentInvokes) -> Vec<String> {
+    todo!()
+}
---
+++ packages/test-cases/cases/apis/wasm-rs/asyncify/query/src/lib.rs
@@ -0,0 +1,10 @@
+pub mod w3;
+pub use w3::*;
+
+pub fn get_data(input: InputGetData) -> u32 {
+    todo!()
+}
+
+pub fn return_true() -> bool {
+    true
+}
