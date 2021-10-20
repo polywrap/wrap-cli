d737dd0b90913893d800d14388ec0e1662f1c9d7
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-10-20 03:57:07 +0000

    Add `simple-storage` api to wasm-rs apis

---
+++ packages/test-cases/cases/apis/wasm-rs/simple-storage/mutation/src/lib.rs
@@ -0,0 +1,10 @@
+pub mod w3;
+pub use w3::*;
+
+pub fn set_data(input: InputSetData) -> String {
+    todo!()
+}
+
+pub fn deploy_contract(input: InputDeployContract) -> String {
+    todo!()
+}
---
+++ packages/test-cases/cases/apis/wasm-rs/simple-storage/query/src/lib.rs
@@ -0,0 +1,6 @@
+pub mod w3;
+pub use w3::*;
+
+pub fn get_data(input: InputGetData) -> u32 {
+    todo!()
+}
