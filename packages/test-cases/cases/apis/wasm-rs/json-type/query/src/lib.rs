b73f7529be899a49cd7351a5d1f1fd707d5d3822
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-10-25 15:23:31 +0000

    Complete the `json-type` api

--- packages/test-cases/cases/apis/wasm-rs/json-type/query/src/lib.rs
+++ packages/test-cases/cases/apis/wasm-rs/json-type/query/src/lib.rs
@@ -3,17 +3,29 @@ use polywrap_wasm_rs::JSON;
 pub use w3::*;
 
 pub fn parse(input: InputParse) -> JSON::Value {
-    todo!()
+    JSON::to_value(input.value).unwrap()
 }
 
 pub fn stringify(input: InputStringify) -> String {
-    todo!()
+    let mut temp_str = String::new();
+    for i in 0..input.values.len() {
+        let value = &input.values[i];
+        temp_str.push_str(value.as_str().unwrap());
+    }
+    temp_str
 }
 
 pub fn stringify_object(input: InputStringifyObject) -> String {
-    todo!()
+    let mut temp_str = String::new();
+    temp_str.push_str(input.object.json_a.as_str().unwrap());
+    temp_str.push_str(input.object.json_b.as_str().unwrap());
+    temp_str
 }
 
 pub fn method_json(input: InputMethodJson) -> JSON::Value {
-    todo!()
+    let mut map = JSON::Map::new();
+    let _ = map.insert("valueA".to_string(), JSON::to_value(input.value_a).unwrap());
+    let _ = map.insert("valueB".to_string(), JSON::to_value(input.value_b).unwrap());
+    let _ = map.insert("valueC".to_string(), JSON::to_value(input.value_c).unwrap());
+    JSON::Value::Object(map)
 }
