761ea175e08bfcf0ebe25af3c1b9dceab706960b
Jure Bogunovic <bogunovij@gmail.com>
2021-10-25 17:39:20 +0200

    added external import test case for inherited empty types

---
+++ packages/test-cases/cases/compose/external-import-inherited-empty-types/output/mutation.ts
@@ -0,0 +1,100 @@
+import {
+  createMethodDefinition,
+  createQueryDefinition,
+  createObjectDefinition,
+  TypeInfo,
+  createObjectPropertyDefinition,
+  createInterfaceImplementedDefinition,
+  createImportedObjectDefinition,
+} from "@web3api/schema-parse";
+
+export const typeInfo: TypeInfo = {
+  objectTypes: [
+    {
+      ...createObjectDefinition({
+        type: "CustomType",
+      }),
+      interfaces: [
+        createInterfaceImplementedDefinition({
+          type: "Base_ImportedBaseType"
+        })
+      ],
+      properties: [
+      ],
+    },
+  ],
+  queryTypes: [
+    {
+      ...createQueryDefinition({ type: "Mutation" }),
+      imports: [
+        { type: "Base_ImportedBaseType" },
+        { type: "Derived_ImportedDerivedType" },
+        { type: "Derived_ImportedBaseType" },
+      ],
+      interfaces: [],
+      methods: [
+        {
+          ...createMethodDefinition({
+            type: "mutation",
+            name: "method1",
+            return: createObjectPropertyDefinition({
+              name: "method1",
+              type: "Derived_ImportedDerivedType",
+            }),
+          }),
+          arguments: [
+          ],
+        },
+        {
+          ...createMethodDefinition({
+            type: "mutation",
+            name: "method2",
+            return: createObjectPropertyDefinition({
+              name: "method2",
+              type: "CustomType",
+            }),
+          }),
+          arguments: [
+          ],
+        },
+      ],
+    },
+  ],
+  enumTypes: [],
+  importedObjectTypes: [
+    {
+      ...createImportedObjectDefinition({
+        uri: "base.eth",
+        namespace: "Base",
+        nativeType: "ImportedBaseType",
+        type: "Base_ImportedBaseType"
+      }),
+      properties: [],
+    },
+    {
+      ...createImportedObjectDefinition({
+        uri: "derived.eth",
+        namespace: "Derived",
+        nativeType: "ImportedDerivedType",
+        type: "Derived_ImportedDerivedType"
+      }),
+      interfaces: [
+        createInterfaceImplementedDefinition({
+          type: "Derived_ImportedBaseType"
+        })
+      ],
+      properties: [],
+    },
+    {
+      ...createImportedObjectDefinition({
+        uri: "derived.eth",
+        namespace: "Derived",
+        nativeType: "ImportedBaseType",
+        type: "Derived_ImportedBaseType"
+      }),
+      properties: [],
+    },
+  ],
+  importedQueryTypes: [],
+  importedEnumTypes: [],
+};
