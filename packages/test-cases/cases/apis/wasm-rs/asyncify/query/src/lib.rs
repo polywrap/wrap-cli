268392cc2348b850e149532a60e66a2459d813e7
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-10-25 09:28:05 +0000

    Complete the `query` module of `asyncify` api

--- packages/test-cases/cases/apis/wasm-rs/asyncify/query/src/lib.rs
+++ packages/test-cases/cases/apis/wasm-rs/asyncify/query/src/lib.rs
@@ -1,8 +1,16 @@
 pub mod w3;
+use w3::imported::InputCallContractView;
 pub use w3::*;
 
-pub fn get_data(input: InputGetData) -> u32 {
-    todo!()
+pub fn get_data(input: InputGetData) -> i32 {
+    EthereumQuery::call_contract_view(&InputCallContractView {
+        address: input.address,
+        method: "function get() view returns (u64)".to_string(),
+        args: None,
+        connection: input.connection,
+    })
+    .parse::<i32>()
+    .unwrap()
 }
 
 pub fn return_true() -> bool {
