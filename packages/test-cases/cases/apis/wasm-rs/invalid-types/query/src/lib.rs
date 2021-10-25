dc0363edb609759a1500b0596f5b55139ea54595
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-10-25 14:29:02 +0000

    Complete the `invalid-types` api

--- packages/test-cases/cases/apis/wasm-rs/invalid-types/query/src/lib.rs
+++ packages/test-cases/cases/apis/wasm-rs/invalid-types/query/src/lib.rs
@@ -2,21 +2,21 @@ pub mod w3;
 pub use w3::*;
 
 pub fn bool_method(input: InputBoolMethod) -> bool {
-    todo!()
+    input.arg
 }
 
 pub fn int_method(input: InputIntMethod) -> i32 {
-    todo!()
+    input.arg
 }
 
 pub fn uint_method(input: InputUintMethod) -> u32 {
-    todo!()
+    input.arg
 }
 
 pub fn bytes_method(input: InputBytesMethod) -> Vec<u8> {
-    todo!()
+    input.arg
 }
 
-pub fn array_method(input: InputArrayMethod) -> Vec<String> {
-    todo!()
+pub fn array_method(input: InputArrayMethod) -> Option<Vec<String>> {
+    Some(input.arg)
 }
