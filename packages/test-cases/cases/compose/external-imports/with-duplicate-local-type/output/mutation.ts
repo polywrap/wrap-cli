2cffdc9e735308cb4648c2e04e932689b6517884
Jure Bogunovic <bogunovij@gmail.com>
2021-10-26 02:20:50 +0200

    updated yarn lock, added duplicate type test case

---
+++ packages/test-cases/cases/compose/external-imports/with-duplicate-local-type/output/mutation.ts
@@ -0,0 +1,55 @@
+import {
+  createMethodDefinition,
+  createQueryDefinition,
+  TypeInfo,
+  createObjectPropertyDefinition,
+  createImportedObjectDefinition,
+  createScalarPropertyDefinition,
+} from "@web3api/schema-parse";
+
+export const typeInfo: TypeInfo = {
+  objectTypes: [
+  ],
+  queryTypes: [
+    {
+      ...createQueryDefinition({ type: "Mutation" }),
+      imports: [
+        { type: "Namespace_ExternalType" }
+      ],
+      interfaces: [],
+      methods: [
+        {
+          ...createMethodDefinition({
+            type: "mutation",
+            name: "method",
+            return: createObjectPropertyDefinition({
+              name: "method",
+              type: "Namespace_ExternalType",
+            }),
+          }),
+          arguments: [
+          ],
+        },
+      ],
+    },
+  ],
+  enumTypes: [],
+  importedObjectTypes: [
+    {
+      ...createImportedObjectDefinition({
+        uri: "external.eth",
+        namespace: "Namespace",
+        nativeType: "ExternalType",
+        type: "Namespace_ExternalType"
+      }),
+      properties: [
+        createScalarPropertyDefinition({
+          name: "str",
+          type: "String"
+        })
+      ],
+    },
+  ],
+  importedQueryTypes: [],
+  importedEnumTypes: [],
+};
