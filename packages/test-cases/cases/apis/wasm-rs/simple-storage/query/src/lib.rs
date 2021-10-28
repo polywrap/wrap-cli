fcad865546038036ed892d64b839dff92067a474
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-10-28 00:35:03 +0000

    Complete the `simple-storage` api

--- packages/test-cases/cases/apis/wasm-rs/simple-storage/query/src/lib.rs
+++ packages/test-cases/cases/apis/wasm-rs/simple-storage/query/src/lib.rs
@@ -1,6 +1,14 @@
 pub mod w3;
+use w3::imported::InputCallContractView;
 pub use w3::*;
 
-pub fn get_data(input: InputGetData) -> u32 {
-    todo!()
+pub fn get_data(input: InputGetData) -> i32 {
+    EthereumQuery::call_contract_view(&InputCallContractView {
+        address: input.address,
+        method: "function get() view returns (u64)".to_string(),
+        args: None,
+        connection: input.connection,
+    })
+    .parse::<i32>()
+    .unwrap()
 }
