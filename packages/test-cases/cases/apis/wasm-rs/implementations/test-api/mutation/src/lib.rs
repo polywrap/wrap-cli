81186ef3c517e74129ff2e8470b06b4a3d456ab7
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-10-28 15:11:36 +0000

    Complete the `implementations` api

--- packages/test-cases/cases/apis/wasm-rs/implementations/test-api/mutation/src/lib.rs
+++ packages/test-cases/cases/apis/wasm-rs/implementations/test-api/mutation/src/lib.rs
@@ -2,9 +2,9 @@ pub mod w3;
 pub use w3::*;
 
 pub fn mutation_method(input: InputMutationMethod) -> u8 {
-    todo!()
+    input.arg
 }
 
 pub fn abstract_mutation_method(input: InputAbstractMutationMethod) -> u8 {
-    todo!()
+    input.arg
 }
--- packages/test-cases/cases/apis/wasm-rs/implementations/test-api/query/src/lib.rs
+++ packages/test-cases/cases/apis/wasm-rs/implementations/test-api/query/src/lib.rs
@@ -2,9 +2,9 @@ pub mod w3;
 pub use w3::*;
 
 pub fn query_method(input: InputQueryMethod) -> ImplementationType {
-    todo!()
+    input.arg
 }
 
 pub fn abstract_query_method(input: InputAbstractQueryMethod) -> String {
-    todo!()
+    input.arg.str
 }
