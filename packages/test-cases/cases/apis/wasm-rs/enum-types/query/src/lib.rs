e22a7bb3ce0e1bb1542c3bb93166cbd941c4c84b
dOrgJelli <jelli@dorg.tech>
2021-11-19 23:56:06 -0600

    fix enum-types

--- packages/test-cases/cases/apis/wasm-rs/enum-types/query/src/lib.rs
+++ packages/test-cases/cases/apis/wasm-rs/enum-types/query/src/lib.rs
@@ -1,10 +1,10 @@
 pub mod w3;
 pub use w3::*;
 
-pub fn sanity_enum_method1(input: InputSanityEnumMethod1) -> SanityEnum {
+pub fn method1(input: InputSanityEnumMethod1) -> SanityEnum {
     input.en
 }
 
-pub fn sanity_enum_method2(input: InputSanityEnumMethod2) -> Vec<SanityEnum> {
+pub fn method2(input: InputSanityEnumMethod2) -> Vec<SanityEnum> {
     input.enum_array
 }
