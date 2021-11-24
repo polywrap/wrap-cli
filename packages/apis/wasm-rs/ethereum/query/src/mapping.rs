7e0746ae42555e53256dbd6555afc967a36a59e0
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-11-24 14:20:52 +0000

    Ethereum wrapper: map `query::TxReceipt` to `ethers::TransactionReceipt`

--- packages/apis/wasm-rs/ethereum/query/src/mapping.rs
+++ packages/apis/wasm-rs/ethereum/query/src/mapping.rs
@@ -1,5 +1,8 @@
 use crate::{Log, TxReceipt};
+use ethers_core::abi::ethereum_types::{Address, H256, U256, U64};
 use ethers_core::types::transaction::response::TransactionReceipt;
+use ethers_core::types::{Bloom, Bytes};
+use num_traits::ToPrimitive;
 use polywrap_wasm_rs::BigInt;
 
 pub fn to_tx_receipt(receipt: TransactionReceipt) -> TxReceipt {
@@ -12,7 +15,7 @@ pub fn to_tx_receipt(receipt: TransactionReceipt) -> TxReceipt {
         gas_used: BigInt::try_from(receipt.gas_used.unwrap().as_u64()).unwrap(),
         logs_bloom: receipt.logs_bloom.to_string(),
         transaction_hash: receipt.transaction_hash.to_string(),
-        logs: get_log(receipt.clone()),
+        logs: to_polywrapper_logs(receipt.clone()),
         block_number: BigInt::try_from(receipt.block_number.unwrap().as_u64()).unwrap(),
         block_hash: receipt.block_hash.unwrap().to_string(),
         confirmations: 0, // TODO: remove this field?
@@ -25,7 +28,27 @@ pub fn to_tx_receipt(receipt: TransactionReceipt) -> TxReceipt {
     }
 }
 
-fn get_log(receipt: TransactionReceipt) -> Vec<Log> {
+pub fn from_tx_receipt(receipt: TxReceipt) -> TransactionReceipt {
+    TransactionReceipt {
+        transaction_hash: H256::from_slice(receipt.transaction_hash.as_bytes()),
+        transaction_index: U64::try_from(receipt.transaction_index as u64).unwrap(),
+        block_hash: Some(H256::from_slice(receipt.block_hash.as_bytes())),
+        block_number: Some(U64::try_from(receipt.block_number.to_u64().unwrap()).unwrap()),
+        cumulative_gas_used: U256::try_from(receipt.cumulative_gas_used.to_u64().unwrap()).unwrap(),
+        gas_used: Some(U256::try_from(receipt.gas_used.to_u64().unwrap()).unwrap()),
+        contract_address: Some(Address::from_slice(receipt.contract_address.as_bytes())),
+        logs: to_ethers_core_logs(receipt.clone()),
+        status: Some(U64::try_from(receipt.status.unwrap() as u64).unwrap()),
+        root: Some(H256::from_slice(receipt.root.unwrap().as_bytes())),
+        logs_bloom: Bloom::from_slice(receipt.logs_bloom.as_bytes()),
+        transaction_type: Some(U64::try_from(receipt.m_type as u64).unwrap()),
+        effective_gas_price: Some(
+            U256::try_from(receipt.effective_gas_price.to_u64().unwrap()).unwrap(),
+        ),
+    }
+}
+
+fn to_polywrapper_logs(receipt: TransactionReceipt) -> Vec<Log> {
     let mut logs: Vec<Log> = vec![];
     for log in receipt.logs {
         logs.push(Log {
@@ -42,6 +65,30 @@ fn get_log(receipt: TransactionReceipt) -> Vec<Log> {
                 .collect(),
             transaction_hash: String::from_utf8(log.transaction_hash.unwrap().0.to_vec()).unwrap(),
             log_index: log.log_index.unwrap().as_u32(),
+        });
+    }
+    logs
+}
+
+fn to_ethers_core_logs(receipt: TxReceipt) -> Vec<ethers_core::types::Log> {
+    let mut logs: Vec<ethers_core::types::Log> = vec![];
+    for log in receipt.logs {
+        logs.push(ethers_core::types::Log {
+            address: Address::from_slice(log.address.as_bytes()),
+            topics: log
+                .topics
+                .into_iter()
+                .map(|inner| H256::from_slice(inner.as_bytes()))
+                .collect(),
+            data: Bytes::try_from(log.data.as_bytes().to_vec()).unwrap(),
+            block_hash: Some(H256::from_slice(log.block_hash.as_bytes())),
+            block_number: Some(U64::try_from(BigInt::to_u64(&log.block_number).unwrap()).unwrap()),
+            transaction_hash: Some(H256::from_slice(log.transaction_hash.as_bytes())),
+            transaction_index: Some(U64::try_from(log.transaction_index).unwrap()),
+            log_index: Some(U256::try_from(log.log_index).unwrap()),
+            transaction_log_index: None,
+            log_type: None,
+            removed: Some(log.removed),
         });
     }
     logs
