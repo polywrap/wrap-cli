751939de374ce7a728a074254ee5514694601c26
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-11-30 18:41:25 +0000

    Implement more resolvers for `Mutation` module

--- packages/apis/wasm-rs/ethereum/mutation/src/resolvers.rs
+++ packages/apis/wasm-rs/ethereum/mutation/src/resolvers.rs
@@ -9,7 +9,7 @@ use ethers_core::{
 };
 use ethers_middleware::SignerMiddleware;
 use ethers_providers::{Http, Middleware, Provider};
-use ethers_signers::LocalWallet;
+use ethers_signers::{LocalWallet, Signer};
 use polywrap_wasm_rs::JSON;
 use query;
 use std::convert::TryFrom;
@@ -20,18 +20,13 @@ pub async fn resolve_call_contract_method(input: InputCallContractMethod) -> Tra
     let client = SignerMiddleware::new(provider, signer);
 
     let address = Address::from_slice(input.address.as_bytes());
-    let abi: Abi = JSON::from_str(stringify!(input.clone())).unwrap();
+    let abi: Abi = JSON::from_str(stringify!(input.clone())).unwrap(); // TODO: Refactor
     let contract = Contract::new(address, abi, client);
 
     let call = contract
         .method::<_, H256>(&input.method, input.args.unwrap())
         .unwrap();
-    let future_tx = call.send().await.unwrap();
-    let pending_tx = tokio::runtime::Builder::new_current_thread()
-        .build()
-        .unwrap()
-        .block_on(future_tx);
-    pending_tx.unwrap().unwrap()
+    call.send().await.unwrap().await.unwrap().unwrap()
 }
 
 pub async fn resolve_call_contract_method_and_wait(
@@ -42,18 +37,13 @@ pub async fn resolve_call_contract_method_and_wait(
     let client = SignerMiddleware::new(provider, signer);
 
     let address = Address::from_slice(input.address.as_bytes());
-    let abi: Abi = JSON::from_str(stringify!(input.clone())).unwrap();
+    let abi: Abi = JSON::from_str(stringify!(input.clone())).unwrap(); // TODO: Refactor
     let contract = Contract::new(address, abi, client);
 
     let call = contract
         .method::<_, H256>(&input.method, input.args.unwrap())
         .unwrap();
-    let future_tx = call.send().await.unwrap();
-    let pending_tx = tokio::runtime::Builder::new_current_thread()
-        .build()
-        .unwrap()
-        .block_on(future_tx);
-    pending_tx.unwrap().unwrap()
+    call.send().await.unwrap().await.unwrap().unwrap()
 }
 
 pub async fn resolve_send_transaction(input: InputSendTransaction) -> TransactionReceipt {
@@ -62,27 +52,75 @@ pub async fn resolve_send_transaction(input: InputSendTransaction) -> Transactio
     let client = SignerMiddleware::new(provider, signer);
 
     let tx_request = query::mapping::from_tx_request(input.tx);
-    let future_tx = client.send_transaction(tx_request, None).await.unwrap();
-
-    let pending_tx = tokio::runtime::Builder::new_current_thread()
-        .build()
+    client
+        .send_transaction(tx_request, None)
+        .await
         .unwrap()
-        .block_on(future_tx);
-    pending_tx.unwrap().unwrap()
+        .await
+        .unwrap()
+        .unwrap()
 }
 
-pub async fn resolve_send_transaction_and_wait(input: InputSendTransactionAndWait) -> TxReceipt {
-    todo!()
+pub async fn resolve_send_transaction_and_wait(
+    input: InputSendTransactionAndWait,
+) -> TransactionReceipt {
+    let provider = Provider::<Http>::try_from(input.connection.clone().provider.as_str()).unwrap();
+    let signer: LocalWallet = input.connection.signer.as_str().parse().unwrap();
+    let client = SignerMiddleware::new(provider, signer);
+
+    let tx_request = query::mapping::from_tx_request(input.tx);
+    client
+        .send_transaction(tx_request, None)
+        .await
+        .unwrap()
+        .await
+        .unwrap()
+        .unwrap()
 }
 
 pub async fn resolve_deploy_contract(input: InputDeployContract) -> String {
-    todo!()
+    let provider = Provider::<Http>::try_from(input.connection.clone().provider.as_str()).unwrap();
+    let signer: LocalWallet = input.connection.signer.as_str().parse().unwrap();
+    let client = SignerMiddleware::new(provider, signer);
+
+    let abi: Abi = JSON::from_str(&input.abi).unwrap();
+    let address = Address::from_slice(input.bytecode.as_bytes()); // TODO: Refactor
+    let contract = Contract::new(address, abi, client);
+
+    contract
+        .method::<_, String>(&input.bytecode, input.args.unwrap())
+        .unwrap()
+        .call()
+        .await
+        .unwrap()
 }
 
 pub async fn resolve_sign_message(input: InputSignMessage) -> String {
-    todo!()
+    let provider = Provider::<Http>::try_from(input.connection.clone().provider.as_str()).unwrap();
+    let signer: LocalWallet = input.connection.signer.clone().as_str().parse().unwrap();
+    let client = SignerMiddleware::new(provider, signer.clone());
+    client
+        .sign(input.message.as_bytes().to_vec(), &signer.address())
+        .await
+        .unwrap()
+        .to_string()
 }
 
 pub async fn resolve_send_rpc(input: InputSendRpc) -> Option<String> {
+    // let provider = Provider::<Http>::try_from(input.connection.clone().provider.as_str()).unwrap();
+    // let signer: LocalWallet = input.connection.signer.clone().as_str().parse().unwrap();
+    // let client = SignerMiddleware::new(provider, signer.clone());
+
     todo!()
 }
+
+// fn example(input: InputSendTransaction) -> TransactionReceipt {
+//     tokio::runtime::Builder::new_current_thread()
+//         .build()
+//         .unwrap()
+//         .block_on(resolve_send_transaction(input))
+// }
+
+// fn use_example(input: InputSendTransaction) -> TransactionReceipt {
+//     example(input)
+// }
