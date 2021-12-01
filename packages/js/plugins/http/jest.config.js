721ecee3b40047ce1a960b0e60014eaf3efa8dc3
dOrgJelli <jelli@dorg.tech>
2021-12-01 16:21:45 -0600

    fixing tests

--- packages/js/plugins/http/jest.config.js
+++ packages/js/plugins/http/jest.config.js
@@ -10,13 +10,13 @@ module.exports = {
     "^.+\\.(ts|tsx)$": "ts-jest"
   },
   modulePathIgnorePatterns: [
-    "<rootDir>/src/__tests__/e2e/integration"
+    "<rootDir>/src/__tests__/e2e/integration/"
   ],
   testPathIgnorePatterns: [
-    "<rootDir>/src/__tests__/e2e/integration"
+    "<rootDir>/src/__tests__/e2e/integration/"
   ],
   transformIgnorePatterns: [
-    "<rootDir>/src/__tests__/e2e/integration"
+    "<rootDir>/src/__tests__/e2e/integration/"
   ],
   testEnvironment: 'node'
 }
