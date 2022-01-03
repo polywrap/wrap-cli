7dee8c4f402b60f7ff91d3c6ba8f0b119761c889
krisbitney <kristoferbitney@hotmail.com>
2022-01-02 21:44:58 -0300

    dapp-client-extensions: added comments to Extension.ts in js/core

--- packages/js/core/src/types/Extension.ts
+++ packages/js/core/src/types/Extension.ts
@@ -2,6 +2,13 @@ import { Client } from "./Client";
 import { Uri } from "./Uri";
 import { InvokeApiOptions, InvokeApiResult } from "./Invoke";
 
+/**
+ * Executable Wrapper/Plugin Invocation
+ *
+ * @param config returns Input arguments for the Invocation, which can be used
+ * as input to the client.invoke(...) method
+ * @param execute Executes client.invoke(...) and returns the result
+ */
 export interface ExtensionInvocation<
   TData = unknown,
   TUri extends Uri | string = string
@@ -10,16 +17,35 @@ export interface ExtensionInvocation<
   execute: () => Promise<InvokeApiResult<TData>>;
 }
 
+/**
+ * Base config for Extension, to be extended by a custom config interface.
+ *
+ * @param uri The uri used for queries within the Extension
+ */
 export interface ExtensionConfig {
   uri?: Uri | string;
 }
 
+/**
+ * Extension interface
+ *
+ * @param client A reference to the Client that is used for queries within the Extension
+ * @param uri The uri used for queries within the Extension
+ * @param config Configuration values for the extension
+ */
 export interface Extension {
   readonly client: Client;
   readonly uri: Uri;
   config: ExtensionConfig;
 }
 
+/**
+ * An ExtensionPackage holds an Extension factory and properties used to extend
+ * the Client
+ *
+ * @param factory Produces Extension
+ * @param namespace Namespace used to access Extension instance from client instance
+ */
 export type ExtensionPackage = {
   factory: (client: Client) => Extension;
   namespace: string;
