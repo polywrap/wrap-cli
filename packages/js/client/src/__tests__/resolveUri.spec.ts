4ab94985afb8a72b0e6bfb52e269cc0adcac9022
Jure Bogunovic <bogunovij@gmail.com>
2022-01-05 01:01:49 +0100

    more resolve uri tests

--- packages/js/client/src/__tests__/resolveUri.spec.ts
+++ packages/js/client/src/__tests__/resolveUri.spec.ts
@@ -116,7 +116,7 @@ describe("Web3ApiClient - resolveUri", () => {
     const result = await client.resolveUri(pluginUri);
 
     expect(result.api).toBeTruthy();
-    expect(result.uri).toBe(pluginUri);
+    expect(result.uri).toEqual(pluginUri);
     expect(result.error).toBeFalsy();
 
     expect(result.uriHistory.stack).toEqual([
@@ -137,22 +137,225 @@ describe("Web3ApiClient - resolveUri", () => {
       ipfsProvider,
       ensAddress
     );
-    const apiUri = new Uri(`w3://ens/testnet/${deployResult.ensDomain}`);
+    const ensUri = new Uri(`ens/testnet/${deployResult.ensDomain}`);
+    const ipfsUri = new Uri(`ipfs/${deployResult.ipfsCid}`);
 
-    const result = await client.resolveUri(apiUri);
+    const result = await client.resolveUri(ensUri);
 
     expect(result.api).toBeTruthy();
-    expect(result.uri).toBeTruthy();
+    expect(result.uri).toEqual(ipfsUri);
     expect(result.error).toBeFalsy();
 
     expect(result.uriHistory.stack).toEqual([
       {
-        sourceUri: apiUri.uri,
+        sourceUri: ensUri.uri,
         resolver: "ApiResolver: w3://ens/ens.web3api.eth"
       },
       {
-        sourceUri: "w3://ipfs/QmZsGybrGU97miDxRfNPmrfrEoHxm6EXLqYcf9QyYzUTAc",
+        sourceUri: ipfsUri.uri,
         resolver: "ApiResolver: w3://ens/ipfs.web3api.eth"
+      }
+    ]);
+  });
+
+  it("can resolve cache", async () => {
+    await runCLI({ args: ["build"], cwd: `${GetPathToTestApis()}/interface-invoke/test-interface`});
+
+    const client = await getClient();
+
+    const deployResult = await buildAndDeployApi(
+      `${GetPathToTestApis()}/interface-invoke/test-api`,
+      ipfsProvider,
+      ensAddress
+    );
+    const ensUri = new Uri(`ens/testnet/${deployResult.ensDomain}`);
+    const ipfsUri = new Uri(`ipfs/${deployResult.ipfsCid}`);
+
+    const result = await client.resolveUri(ipfsUri);
+
+    expect(result.api).toBeTruthy();
+    expect(result.uri).toEqual(ipfsUri);
+    expect(result.error).toBeFalsy();
+
+    expect(result.uriHistory.stack).toEqual([
+      {
+        sourceUri: ipfsUri.uri,
+        resolver: "ApiResolver: w3://ens/ipfs.web3api.eth"
+      }
+    ]);
+
+    const result2 = await client.resolveUri(ensUri);
+
+    expect(result2.api).toBeTruthy();
+    expect(result2.uri).toEqual(ipfsUri);
+    expect(result2.error).toBeFalsy();
+
+    expect(result2.uriHistory.stack).toEqual([
+      {
+        sourceUri: ensUri.uri,
+        resolver: "ApiResolver: w3://ens/ens.web3api.eth"
+      },
+      {
+        sourceUri: ipfsUri.uri,
+        resolver: "CacheResolver"
+      }
+    ]);
+  });
+
+  it("can resolve cache - noCacheRead", async () => {
+    await runCLI({ args: ["build"], cwd: `${GetPathToTestApis()}/interface-invoke/test-interface`});
+
+    const client = await getClient();
+
+    const deployResult = await buildAndDeployApi(
+      `${GetPathToTestApis()}/interface-invoke/test-api`,
+      ipfsProvider,
+      ensAddress
+    );
+    const ensUri = new Uri(`ens/testnet/${deployResult.ensDomain}`);
+    const ipfsUri = new Uri(`ipfs/${deployResult.ipfsCid}`);
+
+    const result = await client.resolveUri(ipfsUri);
+
+    expect(result.api).toBeTruthy();
+    expect(result.uri).toEqual(ipfsUri);
+    expect(result.error).toBeFalsy();
+
+    expect(result.uriHistory.stack).toEqual([
+      {
+        sourceUri: ipfsUri.uri,
+        resolver: "ApiResolver: w3://ens/ipfs.web3api.eth"
+      }
+    ]);
+
+    const result2 = await client.resolveUri(ensUri, { noCacheRead: true });
+
+    expect(result2.api).toBeTruthy();
+    expect(result2.uri).toEqual(ipfsUri);
+    expect(result2.error).toBeFalsy();
+
+    expect(result2.uriHistory.stack).toEqual([
+      {
+        sourceUri: ensUri.uri,
+        resolver: "ApiResolver: w3://ens/ens.web3api.eth"
+      },
+      {
+        sourceUri: ipfsUri.uri,
+        resolver: "ApiResolver: w3://ens/ipfs.web3api.eth"
+      }
+    ]);
+  });
+
+  it("can resolve cache - noCacheWrite", async () => {
+    await runCLI({ args: ["build"], cwd: `${GetPathToTestApis()}/interface-invoke/test-interface`});
+
+    const client = await getClient();
+
+    const deployResult = await buildAndDeployApi(
+      `${GetPathToTestApis()}/interface-invoke/test-api`,
+      ipfsProvider,
+      ensAddress
+    );
+    const ensUri = new Uri(`ens/testnet/${deployResult.ensDomain}`);
+    const ipfsUri = new Uri(`ipfs/${deployResult.ipfsCid}`);
+
+    const result = await client.resolveUri(ipfsUri, { noCacheWrite: true });
+
+    expect(result.api).toBeTruthy();
+    expect(result.uri).toEqual(ipfsUri);
+    expect(result.error).toBeFalsy();
+
+    expect(result.uriHistory.stack).toEqual([
+      {
+        sourceUri: ipfsUri.uri,
+        resolver: "ApiResolver: w3://ens/ipfs.web3api.eth"
+      }
+    ]);
+
+    const result2 = await client.resolveUri(ensUri);
+
+    expect(result2.api).toBeTruthy();
+    expect(result2.uri).toEqual(ipfsUri);
+    expect(result2.error).toBeFalsy();
+
+    expect(result2.uriHistory.stack).toEqual([
+      {
+        sourceUri: ensUri.uri,
+        resolver: "ApiResolver: w3://ens/ens.web3api.eth"
+      },
+      {
+        sourceUri: ipfsUri.uri,
+        resolver: "ApiResolver: w3://ens/ipfs.web3api.eth"
+      }
+    ]);
+  });
+
+  it("can resolve api with redirects", async () => {
+    await runCLI({ args: ["build"], cwd: `${GetPathToTestApis()}/interface-invoke/test-interface`});
+
+    const deployResult = await buildAndDeployApi(
+      `${GetPathToTestApis()}/interface-invoke/test-api`,
+      ipfsProvider,
+      ensAddress
+    );
+
+    const ensUri = new Uri(`ens/testnet/${deployResult.ensDomain}`);
+    const ipfsUri = new Uri(`ipfs/${deployResult.ipfsCid}`);
+    const redirectUri = new Uri(`ens/redirect.eth`);
+
+    const client = await getClient({
+      redirects: [
+        {
+          from: ipfsUri.uri,
+          to: redirectUri.uri
+        }
+      ]
+    });
+
+    const result = await client.resolveUri(ensUri);
+
+    expect(result.api).toBeFalsy();
+    expect(result.uri).toEqual(redirectUri);
+    expect(result.error).toBeFalsy();
+
+    expect(result.uriHistory.stack).toEqual([
+      {
+        sourceUri: ensUri.uri,
+        resolver: "ApiResolver: w3://ens/ens.web3api.eth"
+      },
+      {
+        sourceUri: ipfsUri.uri,
+        resolver: "RedirectsResolver"
+      }
+    ]);
+  });
+
+  it("can resolve uri with custom resolver", async () => {
+    const client = await getClient();
+
+    const sourceUri = new Uri("ens/source.eth");
+    const apiUri = new Uri("ens/api.eth");
+    const customResolverName = "TestResolver";
+
+    const result = await client.resolveUri(sourceUri, undefined, [
+      {
+        name: customResolverName,
+        resolveUri: async (uri: Uri) => {
+          return {
+            uri: apiUri,
+          };
+        }
+      }
+    ]);
+
+    expect(result.uri).toEqual(apiUri);
+    expect(result.api).toBeFalsy();
+    expect(result.error).toBeFalsy();
+
+    expect(result.uriHistory.stack).toEqual([
+      {
+        sourceUri: sourceUri.uri,
+        resolver: customResolverName
       }
     ]);
   });
