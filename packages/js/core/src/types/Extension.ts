6cb781644ef1c880b106d60845faa30d28decbef
krisbitney <kristoferbitney@hotmail.com>
2022-01-19 16:50:06 -0300

    dapp-client-extensions: removed commented out code

--- packages/js/core/src/types/Extension.ts
+++ packages/js/core/src/types/Extension.ts
@@ -2,21 +2,6 @@ import { Client } from "./Client";
 import { Uri } from "./Uri";
 
 /**
- * Executable Wrapper/Plugin Invocation
- *
- * @param config returns Input arguments for the Invocation, which can be used
- * as input to the client.invoke(...) method
- * @param execute Executes client.invoke(...) and returns the result
- */
-// export interface ExtensionInvocation<
-//   TData = unknown,
-//   TUri extends Uri | string = string
-// > {
-//   config: () => InvokeApiOptions<TUri>;
-//   execute: () => Promise<InvokeApiResult<TData>>;
-// }
-
-/**
  * Base config for Extension
  *
  * @param uri The uri used for queries within the Extension
