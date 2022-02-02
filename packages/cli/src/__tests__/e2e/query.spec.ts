61436d294c5cb3f71e97e01b1b489136fa9b12a2
arcbjorn <arky.happ@gmail.com>
2022-02-03 01:32:05 +0300

    test (cli query): mixed recipes for js-yaml parser

--- packages/cli/src/__tests__/e2e/query.spec.ts
+++ packages/cli/src/__tests__/e2e/query.spec.ts
@@ -1,4 +1,7 @@
+import fs from "fs";
 import path from "path";
+import yaml from "js-yaml";
+
 import { clearStyle, w3Cli } from "./utils";
 
 import { runCLI } from "@web3api/test-env-js";
@@ -43,9 +46,9 @@ ${HELP}`);
       .toEqual(`--client-config option missing <config-path> argument
 ${HELP}`);
   });
-})
+});
 
-describe("e2e tests for query command", () => {
+describe.only("e2e tests for query command", () => {
   beforeAll(async () => {
     const { exitCode: testenvCode, stderr: testEnvUpErr } = await runCLI({
       args: ["test-env", "up"],
@@ -54,7 +57,7 @@ describe("e2e tests for query command", () => {
     });
     expect(testEnvUpErr).toBe("");
     expect(testenvCode).toEqual(0);
-  })
+  });
 
   afterAll(async () => {
     await runCLI({
@@ -62,27 +65,33 @@ describe("e2e tests for query command", () => {
       cwd: projectRoot,
       cli: w3Cli,
     });
-  })
+  });
 
   test("Should use custom config for client if specified", async () => {
-
     const configs = ["./client-config.ts", "./client-config.js"];
 
     for (const config of configs) {
       const { exitCode, stdout, stderr } = await runCLI({
-        args: ["query", "./recipes/e2e.json", "--test-ens", "--client-config", config ],
+        args: [
+          "query",
+          "./recipes/e2e.json",
+          "--test-ens",
+          "--client-config",
+          config,
+        ],
         cwd: projectRoot,
         cli: w3Cli,
       });
-  
+
       expect(stderr).toBeFalsy();
       expect(stdout).toBeTruthy();
-  
+
       expect(exitCode).toEqual(0);
       expect(stderr).toBe("");
-  
+
       const constants = require(`${projectRoot}/recipes/constants.json`);
-      expect(clearStyle(normalizeLineEndings(stdout, "\n"))).toContain(`-----------------------------------
+      expect(clearStyle(normalizeLineEndings(stdout, "\n")))
+        .toContain(`-----------------------------------
 mutation {
   setData(
     options: {
@@ -111,16 +120,15 @@ mutation {
     "txReceipt": "0xdone"
   }
 }
------------------------------------`
-);
+-----------------------------------`);
     }
   }, 48000);
 
-  test("Should successfully return response", async () => {
+  test("Should successfully return response: using json recipes", async () => {
     const { stderr: deployErr } = await runCLI({
       args: ["./deploy-contracts.js"],
       cwd: projectRoot,
-      cli: " "
+      cli: " ",
     });
 
     expect(deployErr).toBe("");
@@ -150,7 +158,8 @@ mutation {
     expect(queryErr).toBe("");
 
     const constants = require(`${projectRoot}/recipes/constants.json`);
-    expect(clearStyle(normalizeLineEndings(output, "\n"))).toContain(`-----------------------------------
+    expect(clearStyle(normalizeLineEndings(output, "\n")))
+      .toContain(`-----------------------------------
 mutation {
   setData(
     options: {
@@ -176,12 +185,161 @@ mutation {
 {
   "setData": {
     "txReceipt": "0x`);
-      expect(clearStyle(output)).toContain(`",
+    expect(clearStyle(output)).toContain(`",
     "value": 569
   }
 }
 -----------------------------------
 `);
+  }, 480000);
 
+  test("Should successfully return response: using yaml recipes", async () => {
+    const { stderr: deployErr } = await runCLI({
+      args: ["./deploy-contracts.js"],
+      cwd: projectRoot,
+      cli: " ",
+    });
+
+    expect(deployErr).toBe("");
+
+    const { exitCode: buildCode, stderr: buildErr } = await runCLI({
+      args: [
+        "build",
+        "--ipfs",
+        "http://localhost:5001",
+        "--test-ens",
+        "simplestorage.eth",
+      ],
+      cwd: projectRoot,
+      cli: w3Cli,
+    });
+
+    expect(buildErr).toBe("");
+    expect(buildCode).toEqual(0);
+
+    const { exitCode: code, stdout: output, stderr: queryErr } = await runCLI({
+      args: ["query", "./recipes/e2e.yaml", "--test-ens"],
+      cwd: projectRoot,
+      cli: w3Cli,
+    });
+
+    expect(code).toEqual(0);
+    expect(queryErr).toBe("");
+
+    const constants = yaml.load(
+      await fs.promises.readFile(
+        `${projectRoot}/recipes/constants.yaml`,
+        "utf8"
+      )
+    ) as { SimpleStorageAddr: string };
+
+    expect(clearStyle(normalizeLineEndings(output, "\n")))
+      .toContain(`-----------------------------------
+mutation {
+  setData(
+    options: {
+      address: $address
+      value: $value
+    }
+    connection: {
+      networkNameOrChainId: $network
+    }
+  ) {
+    value
+    txReceipt
+  }
+}
+
+{
+  "address": "${constants.SimpleStorageAddr}",
+  "value": 569,
+  "network": "testnet"
+}
+-----------------------------------
+-----------------------------------
+{
+  "setData": {
+    "txReceipt": "0x`);
+    expect(clearStyle(output)).toContain(`",
+    "value": 569
+  }
+}
+-----------------------------------
+`);
+  }, 480000);
+
+  test("Should successfully return response: using mix of yaml & json recipes", async () => {
+    const { stderr: deployErr } = await runCLI({
+      args: ["./deploy-contracts.js"],
+      cwd: projectRoot,
+      cli: " ",
+    });
+
+    expect(deployErr).toBe("");
+
+    const { exitCode: buildCode, stderr: buildErr } = await runCLI({
+      args: [
+        "build",
+        "--ipfs",
+        "http://localhost:5001",
+        "--test-ens",
+        "simplestorage.eth",
+      ],
+      cwd: projectRoot,
+      cli: w3Cli,
+    });
+
+    expect(buildErr).toBe("");
+    expect(buildCode).toEqual(0);
+
+    const { exitCode: code, stdout: output, stderr: queryErr } = await runCLI({
+      args: ["query", "./recipes/e2e.json", "--test-ens"],
+      cwd: projectRoot,
+      cli: w3Cli,
+    });
+
+    expect(code).toEqual(0);
+    expect(queryErr).toBe("");
+
+    const constants = yaml.load(
+      await fs.promises.readFile(
+        `${projectRoot}/recipes/constants.yaml`,
+        "utf8"
+      )
+    ) as { SimpleStorageAddr: string };
+
+    expect(clearStyle(normalizeLineEndings(output, "\n")))
+      .toContain(`-----------------------------------
+mutation {
+  setData(
+    options: {
+      address: $address
+      value: $value
+    }
+    connection: {
+      networkNameOrChainId: $network
+    }
+  ) {
+    value
+    txReceipt
+  }
+}
+
+{
+  "address": "${constants.SimpleStorageAddr}",
+  "value": 569,
+  "network": "testnet"
+}
+-----------------------------------
+-----------------------------------
+{
+  "setData": {
+    "txReceipt": "0x`);
+    expect(clearStyle(output)).toContain(`",
+    "value": 569
+  }
+}
+-----------------------------------
+`);
   }, 480000);
 });
