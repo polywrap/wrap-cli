87fdfd04fbf645ce042f0a4c3e582530b0b8b875
dOrgJelli <jelli@dorg.tech>
2022-02-07 20:33:52 -0600

    fix toTypescript imports

--- packages/cli/src/lib/codegen-templates/dapp/polywrap-dapp/package-ts.gen.js
+++ packages/cli/src/lib/codegen-templates/dapp/polywrap-dapp/package-ts.gen.js
@@ -1,11 +1,11 @@
 const { toPrefixedGraphQLType, transformTypeInfo } = require("@web3api/schema-parse");
-const { toTypescript } = require("@web3api/schema-bind");
+const { PluginTs } = require("@web3api/schema-bind");
 
 exports.run = (output, config) => {
 
   // Transform the TypeInfo to our liking
   config.typeInfo = transformTypeInfo(config.typeInfo, toPrefixedGraphQLType);
-  config.typeInfo.toTypescript = toTypescript;
+  config.typeInfo.toTypescript = PluginTs.Functions.toTypescript;
   config.typeInfo.title = () => {
     return (value, render) => {
       let rendered = render(value);
--- packages/cli/src/lib/codegen-templates/dapp/types/package-ts.gen.js
+++ packages/cli/src/lib/codegen-templates/dapp/types/package-ts.gen.js
@@ -1,12 +1,12 @@
 /* eslint-env es6 */
 const { toPrefixedGraphQLType, transformTypeInfo } = require("@web3api/schema-parse");
-const { toTypescript } = require("@web3api/schema-bind");
+const { PluginTs } = require("@web3api/schema-bind");
 
 exports.run = (output, config) => {
 
   // Transform the TypeInfo to our liking
   config.typeInfo = transformTypeInfo(config.typeInfo, toPrefixedGraphQLType);
-  config.typeInfo.toTypescript = toTypescript;
+  config.typeInfo.toTypescript = PluginTs.Functions.toTypescript;
 
   output.entries.push({
     type: "File",
