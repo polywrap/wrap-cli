b9d563f164ffcfdb0978d20539b9ecdccb7f166d
dOrgJelli <jelli@dorg.tech>
2022-02-16 19:29:22 -0700

    init test-cases + fixup test-case runners

--- packages/test-cases/index.ts
+++ packages/test-cases/index.ts
@@ -1,4 +1,65 @@
+import path from "path";
+import { readFileSync, existsSync } from "fs";
+
+import { normalizeLineEndings } from "@web3api/os-js";
+
 export const GetPathToBindTestFiles = () => `${__dirname}/cases/bind`
 export const GetPathToComposeTestFiles = () => `${__dirname}/cases/compose`
 export const GetPathToParseTestFiles = () => `${__dirname}/cases/parse`
 export const GetPathToTestApis = () => `${__dirname}/cases/apis`
+
+export function readFileIfExists(
+  file: string,
+  directory: string,
+  absolute = false
+): string | undefined {
+  const filePath = getFilePath( 
+    file,
+    directory,
+    absolute
+  );
+
+  if (existsSync(filePath)) {
+    return normalizeLineEndings(
+      readFileSync(filePath, { encoding: "utf-8" }),
+      "\n"
+    );
+  } else {
+    return undefined;
+  }
+};
+
+export async function readNamedExportIfExists<TExport>(
+  namedExport: string,
+  file: string,
+  directory: string,
+  absolute = false
+): Promise<TExport | undefined> {
+  const filePath = getFilePath(file, directory, absolute);
+
+  if (existsSync(filePath)) {
+    const module = await import(filePath);
+
+    if (!module[namedExport]) {
+      throw Error(
+        `Required named export "${namedExport}" is missing in ${filePath}`
+      );
+    }
+
+    return module[namedExport] as TExport;
+  } else {
+    return undefined;
+  }
+}
+
+function getFilePath(
+  file: string,
+  directory: string,
+  absolute = false
+): string {
+  if (absolute) {
+    return file;
+  } else {
+    return path.join(directory, file);
+  }
+}
