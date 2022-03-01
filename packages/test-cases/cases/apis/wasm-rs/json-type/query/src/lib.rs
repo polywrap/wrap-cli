ba2e7f16070a32431a64b21096491752dae8f37b
namesty <nestor09amesty@gmail.com>
2022-03-01 03:02:08 +0100

    (fix): corrected JSON test case

--- packages/test-cases/cases/apis/wasm-rs/json-type/query/src/lib.rs
+++ packages/test-cases/cases/apis/wasm-rs/json-type/query/src/lib.rs
@@ -1,20 +1,34 @@
 pub mod w3;
 use polywrap_wasm_rs::JSON;
 pub use w3::*;
+use std::string::String;
 
 pub fn parse(input: InputParse) -> JSON::Value {
-    JSON::to_value(&input).unwrap()
+    JSON::from_str(&input.value).unwrap()
 }
 
 pub fn stringify(input: InputStringify) -> String {
-    JSON::to_string(&input).unwrap()
+    let result_vec: Vec<String> = input.values.iter()
+    .map(|value| JSON::to_string(&value).unwrap())
+    .collect();
+
+    polywrap_wasm_rs::w3_debug_log(&format!("{:?}", result_vec));
+
+    result_vec.join("")
 }
 
 pub fn stringify_object(input: InputStringifyObject) -> String {
-    JSON::to_string(&input).unwrap()
+    let mut result = String::new();
+    let json_a_str = JSON::to_string(&input.object.json_a).unwrap();
+    let json_b_str = JSON::to_string(&input.object.json_b).unwrap();
+    
+    result.push_str(&json_a_str);
+    result.push_str(&json_b_str);
+
+    result
 }
 
-pub fn method_json(input: InputMethodJson) -> JSON::Value {
+pub fn method_j_s_o_n(input: InputMethodJSON) -> JSON::Value {
     let mut map = JSON::Map::new();
     let _ = map.insert("valueA".to_string(), JSON::to_value(input.value_a).unwrap());
     let _ = map.insert("valueB".to_string(), JSON::to_value(input.value_b).unwrap());
