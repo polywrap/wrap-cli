e35da73fd62ed33251d6e9694bfadfbdf1aed04d
drewhoang <drewhoang@CefaliaDevelopment.localdomain>
2022-03-15 13:14:55 -0700

    add the rest of the options commands for commander.js for refactor

--- packages/cli/src/commands/app.ts
+++ packages/cli/src/commands/app.ts
@@ -1,7 +1,16 @@
 import { Command, Program } from "./types";
+import fs from 'fs';
 import {
+    //AppProject,
+    //CodeGenerator,
+    //SchemaComposer,
     intlMsg,
-} from "../lib";
+    //fixParameters,
+    //resolvePathIfExists,
+    defaultAppManifest,
+    // getSimpleClient,
+    // getTestEnvProviders,
+  } from "../lib";
 
 
 export const app: Command = {
@@ -27,8 +36,42 @@ export const app: Command = {
                   default: defaultOutputTypesDir,
                 }
               )}`)
+            .option(`-i, --ipfs [<${intlMsg.commands_codegen_options_i_node()}>] `,  `${intlMsg.commands_codegen_options_i()}`)
+            .option(`-e, --ens [<${intlMsg.commands_codegen_options_e_address()}>]`,   `${intlMsg.commands_codegen_options_e()}`)
             .action(async (options) => {
-                console.log(options);
+                await run(options);
             });
     }
+}
+
+async function run(options:any) {
+
+        console.log(options)
+        // Resolve manifest
+        const manifestPaths = options.manifestFile ? [options.manifestFile] : defaultAppManifest;
+
+        function resolvePathIfExists(
+            searchPaths: string[]
+          ): string | undefined {
+            for (let i = 0; i < manifestPaths.length; i++) {
+              if (fs.existsSync(searchPaths[i])) {
+                return searchPaths[i];
+              }
+            }
+            return undefined;
+          }
+          
+        let manifestFile = resolvePathIfExists(manifestPaths)
+        
+        if (!manifestFile) {
+          console.error(
+            intlMsg.commands_app_error_manifestNotFound({
+              paths: manifestPaths.join(", "),
+            })
+          );
+          return;
+        }
+
+        // TODO 
+        // add the rest of the logic 
 }
