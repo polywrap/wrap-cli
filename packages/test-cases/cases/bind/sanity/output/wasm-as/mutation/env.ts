8554d19341b15e9e6e9263569aa08f7c72b3136d
Niraj Kamdar <nirajkamdar27@pm.me>
2022-04-04 12:35:06 +0530

    chore: add ensureEnv function

--- packages/test-cases/cases/bind/sanity/output/wasm-as/mutation/env.ts
+++ packages/test-cases/cases/bind/sanity/output/wasm-as/mutation/env.ts
@@ -3,3 +3,8 @@ import {
 } from "./MutationEnv";
 
 export let env: MutationEnv | null = null;
+
+export function ensureEnv(): MutationEnv {
+  if (env == null) throw new Error("Undefined mutation env");
+  return env as MutationEnv;
+}
--- packages/test-cases/cases/bind/sanity/output/wasm-as/query/env.ts
+++ packages/test-cases/cases/bind/sanity/output/wasm-as/query/env.ts
@@ -3,3 +3,8 @@ import {
 } from "./QueryEnv";
 
 export let env: QueryEnv | null = null;
+
+export function ensureEnv(): QueryEnv {
+  if (env == null) throw new Error("Undefined query env");
+  return env as QueryEnv;
+}
