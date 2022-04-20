e2c318b52d4896bc430d7843c8ce35628ddf679e
namesty <nestor09amesty@gmail.com>
2022-04-20 14:59:23 +0200

    (chore): changed IPFS deployer impl to use http client package

--- packages/cli/src/lib/deployers/ipfs/index.ts
+++ packages/cli/src/lib/deployers/ipfs/index.ts
@@ -1,22 +1,12 @@
 import { Deployer } from "../../deploy/deployer";
-import { convertDirectoryBlobToFormData } from "./formData";
-import { parseAddDirectoryResponse } from "./utils";
-import { convertDirectoryToEntry, DirectoryBlob } from "./file";
 
-import FormData from "form-data";
-import axios from "axios";
 import { Uri } from "@web3api/core-js";
 
+// eslint-disable-next-line @typescript-eslint/no-require-imports,@typescript-eslint/no-var-requires,@typescript-eslint/naming-convention
+const IPFSClient = require("ipfs-http-client");
+const { globSource } = IPFSClient;
+
 const isValidUri = (uri: Uri) => uri.authority === "fs";
-
-const resolveBuildDir = (buildDirPath: string): DirectoryBlob => {
-  const dirEntry = convertDirectoryToEntry(buildDirPath);
-
-  return {
-    files: [],
-    directories: [dirEntry],
-  };
-};
 
 class IPFSDeployer implements Deployer {
   async execute(uri: Uri, config?: { gatewayUri: string }): Promise<Uri> {
@@ -27,54 +17,30 @@ class IPFSDeployer implements Deployer {
     }
 
     const path = uri.path;
-    const buildDirBlob = resolveBuildDir(path);
 
     const ipfsUrl = config?.gatewayUri ?? "http://localhost:5001";
 
-    const formDataEntries = convertDirectoryBlobToFormData(buildDirBlob);
+    const client = new IPFSClient({ url: ipfsUrl });
+    const globOptions = {
+      recursive: true,
+    };
 
-    const fd = new FormData();
+    const addOptions = {
+      wrapWithDirectory: false,
+    };
 
-    formDataEntries.forEach((formDataEntry) => {
-      const options: FormData.AppendOptions = {};
-      if (formDataEntry.opts) {
-        if (
-          formDataEntry.opts.contentType &&
-          formDataEntry.opts.contentType != ""
-        ) {
-          options.contentType = formDataEntry.opts.contentType;
-        }
-        if (formDataEntry.opts.fileName && formDataEntry.opts.fileName != "") {
-          options.filename = formDataEntry.opts.fileName;
-        }
-        if (formDataEntry.opts.filePath && formDataEntry.opts.filePath != "") {
-          options.filepath = formDataEntry.opts.filePath;
-        }
+    let rootCID = "";
+
+    for await (const file of client.addAll(
+      globSource(path, globOptions),
+      addOptions
+    )) {
+      if (file.path.indexOf("/") === -1) {
+        rootCID = file.cid.toString();
       }
-      const elementData =
-        formDataEntry.data == null ? Buffer.alloc(0) : formDataEntry.data;
-      fd.append(formDataEntry.key, elementData, options);
-    });
-
-    const resp = await axios.post(`${ipfsUrl}/api/v0/add`, fd, {
-      headers: {
-        ...fd.getHeaders(),
-      },
-    });
-
-    if (resp.status === 200) {
-      const directoryCID = parseAddDirectoryResponse(resp.data).find(
-        (a) => !a.name.includes("/")
-      )?.hash;
-
-      if (!directoryCID) {
-        throw new Error("Could not find root folder's CID");
-      }
-
-      return new Uri(`w3://ipfs/${directoryCID}`);
-    } else {
-      throw new Error("Unexpected error: " + resp.status);
     }
+
+    return new Uri(`w3://ipfs/${rootCID}`);
   }
 }
 
