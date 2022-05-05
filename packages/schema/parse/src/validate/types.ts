461b3e1b3c7ebd93ebf344a2539fba9d73a7c16a
Niraj Kamdar <nirajkamdar27@pm.me>
2022-05-05 23:22:41 +0530

    fix(schema-parse): type validator

--- packages/schema/parse/src/validate/types.ts
+++ packages/schema/parse/src/validate/types.ts
@@ -64,11 +64,7 @@ export const getTypeDefinitionsValidator = (): SchemaValidator => {
         // TODO: Remove JSON after next release
         // NOTE: BREAKING CHANGE: no plugin or wrapper should have json
         ScalarTypeDefinition: (node: ScalarTypeDefinitionNode) => {
-          if (
-            node.name.value !== "JSON" &&
-            node.name.value !== "Map" &&
-            !isScalarType(node.name.value)
-          ) {
+          if (node.name.value !== "Map" && !isScalarType(node.name.value)) {
             throw Error(
               `Custom scalar types are not supported. Found: "${node.name.value}". Supported scalars: ${scalarTypeNames}`
             );
