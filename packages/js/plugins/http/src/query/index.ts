31231b0f111a102753db02a7a9ddb82bde5ade5d
namesty <nestor09amesty@gmail.com>
2022-06-04 21:31:39 +0200

    (feat): implemented UriResolver for Http Plugin

--- packages/js/plugins/http/src/query/index.ts
+++ packages/js/plugins/http/src/query/index.ts
@@ -1,4 +1,14 @@
-import { Client, Module, Input_get, Input_post, Response } from "./w3";
+import {
+  Client,
+  Module,
+  Input_get,
+  Input_post,
+  Response,
+  Input_tryResolveUri,
+  Input_getFile,
+  Bytes,
+  UriResolver_MaybeUriOrManifest,
+} from "./w3";
 import { fromAxiosResponse, toAxiosRequestConfig } from "./util";
 
 import axios from "axios";
@@ -6,6 +16,70 @@ import axios from "axios";
 export type QueryConfig = Record<string, unknown>;
 
 export class Query extends Module<QueryConfig> {
+  async tryResolveUri(
+    input: Input_tryResolveUri,
+    _client: Client
+  ): Promise<UriResolver_MaybeUriOrManifest | null> {
+    if (input.authority !== "http") {
+      return null;
+    }
+
+    const manifestSearchPatterns = [
+      "web3api.json",
+      "web3api.yaml",
+      "web3api.yml",
+    ];
+
+    let manifest: string | undefined;
+
+    for (const manifestSearchPattern of manifestSearchPatterns) {
+      try {
+        const response = await this.get(
+          {
+            url: `${input.path}/${manifestSearchPattern}`,
+          },
+          _client
+        );
+
+        if (response?.status === 200 && response.body) {
+          manifest = response.body;
+        }
+      } catch (e) {
+        // TODO: logging
+        // https://github.com/web3-api/monorepo/issues/33
+      }
+    }
+
+    if (manifest) {
+      return { uri: null, manifest };
+    } else {
+      // Noting found
+      return { uri: null, manifest: null };
+    }
+  }
+
+  public async getFile(
+    input: Input_getFile,
+    _client: Client
+  ): Promise<Bytes | null> {
+    try {
+      const response = await this.get(
+        {
+          url: input.path,
+        },
+        _client
+      );
+
+      if (response?.status !== 200 || !response.body) {
+        return null;
+      }
+
+      return Buffer.from(response.body, "utf-8");
+    } catch (e) {
+      return null;
+    }
+  }
+
   public async get(
     input: Input_get,
     _client: Client
