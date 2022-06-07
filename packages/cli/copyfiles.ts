0cfa3992542dd5c647fa9a707692c4d8f2f1a049
namesty <nestor09amesty@gmail.com>
2022-06-07 07:44:38 +0200

    (fix): fixed copyfiles script and CLI workspace

--- packages/cli/copyfiles.ts
+++ packages/cli/copyfiles.ts
@@ -6,23 +6,16 @@ const dest = process.argv[3];
 const ignore = [
   ".env",
   "node_modules",
-  "build",
   ".idea",
   ".log",
   ".vscode",
-  "w3",
   ".w3",
-  ".DS_Store",
-  "report.",
-  "target",
   ".rs.bk",
   ".lock",
-  "bin",
-  "pkg",
 ];
 
 fse.copySync(source, dest, {
   filter: function (path) {
-    return !ignore.some((ignorePath) => path.includes(ignorePath));
+    return !ignore.some((ignorePath) => path.endsWith(ignorePath));
   },
 });
