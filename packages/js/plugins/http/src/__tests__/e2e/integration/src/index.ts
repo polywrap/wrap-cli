146c990353dd6fba7de20c9cc14f903d59e684b3
dOrgJelli <jelli@dorg.tech>
2022-06-13 17:36:54 -0700

    fix http tests

--- packages/js/plugins/http/src/__tests__/e2e/integration/src/index.ts
+++ packages/js/plugins/http/src/__tests__/e2e/integration/src/index.ts
@@ -1,19 +1,19 @@
 import {
-  HTTP_Query,
+  HTTP_Module,
   HTTP_Response,
   Input_get,
   Input_post,
 } from "./w3";
 
 export function get(input: Input_get): HTTP_Response | null {
-  return HTTP_Query.get({
+  return HTTP_Module.get({
     url: input.url,
     request: input.request
   }).unwrap();
 }
 
 export function post(input: Input_post): HTTP_Response | null {
-  return HTTP_Query.post({
+  return HTTP_Module.post({
     url: input.url,
     request: input.request
   }).unwrap();
