cb6fa82faa12225347710f88f8bfea1cb65264fd
dOrgJelli <jelli@dorg.tech>
2022-06-20 09:31:42 -0700

    update wasm-rs test-case

--- packages/test-cases/cases/wrappers/wasm-as/env-types/src/index.ts
+++ packages/test-cases/cases/wrappers/wasm-as/env-types/src/index.ts
@@ -1,9 +1,39 @@
 import {
-  env,
   Env,
-  Input_moduleEnv,
+  Input_methodNoEnv,
+  Input_methodRequireEnv,
+  Input_methodOptionalEnv,
 } from "./wrap";
 
-export function moduleEnv(input: Input_moduleEnv): Env {
-  return env as Env;
+function createEnv(env: Env): Env {
+  return {
+    str: env.str,
+    optStr: env.optStr,
+    optFilledStr: env.optFilledStr,
+    number: env.number,
+    optNumber: env.optNumber,
+    bool: env.bool,
+    optBool: env.optBool,
+    en: env.en,
+    optEnum: env.optEnum,
+    object: env.object,
+    optObject: env.optObject,
+    array: env.array,
+  };
+}
+
+export function methodNoEnv(input: Input_methodNoEnv): string {
+  return input.arg;
+}
+
+export function methodRequireEnv(input: Input_methodRequireEnv): Env {
+  return createEnv(input.env);
+}
+
+export function methodOptionalEnv(input: Input_methodOptionalEnv): Env | null {
+  if (input.env) {
+    return createEnv(input.env);
+  } else {
+    return null;
+  }
 }
