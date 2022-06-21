fd936b3ae240400f384ac16ec79871eda017f56d
“Pileks” <jure@amber-it.co>
2022-06-21 11:25:30 +0200

    increase timeout for ipfs-resolver tests

--- packages/js/plugins/uri-resolvers/ipfs-resolver/src/__tests__/e2e.spec.ts
+++ packages/js/plugins/uri-resolvers/ipfs-resolver/src/__tests__/e2e.spec.ts
@@ -14,7 +14,7 @@ import { IpfsClient } from "../../../../ipfs/src/utils/IpfsClient";
 
 const createIpfsClient = require("@dorgjelli-test/ipfs-http-client-lite");
 
-jest.setTimeout(30000);
+jest.setTimeout(300000);
 
 describe("IPFS Plugin", () => {
   let client: PolywrapClient;
