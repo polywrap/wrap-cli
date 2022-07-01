f817578985dabcf7303a374301683d64da304469
dOrgJelli <jelli@dorg.tech>
2022-06-30 23:54:53 -0700

    fix compose tests

--- packages/test-cases/cases/compose/002-external-imports/00-sanity/output/module.ts
+++ packages/test-cases/cases/compose/002-external-imports/00-sanity/output/module.ts
@@ -4,6 +4,7 @@ import {
   createObjectPropertyDefinition,
   createImportedObjectDefinition,
   createScalarPropertyDefinition,
+  Abi,
   createAbi,
   createImportedModuleDefinition,
   createImportedEnvDefinition,
