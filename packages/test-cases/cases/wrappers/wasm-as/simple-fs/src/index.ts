eaf62cdff8c317857ad4674f82275b32dd830b91
Pileks <jure@amber-it.co>
2022-07-12 23:47:05 +0200

    remove simple-fs test wrapper (not used anywhere anymore)

--- packages/test-cases/cases/wrappers/wasm-as/simple-fs/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { Args_readFile, FileSystem_Module } from "./wrap";
-
-export function readFile(args: Args_readFile): ArrayBuffer {
-  const result = FileSystem_Module.readFile({ path: args.path });
-
-  return result.unwrap();
-}
