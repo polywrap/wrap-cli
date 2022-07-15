5068252e58fcf7d68a5af54422f4ef5e6c55f225
namesty <nestor09amesty@gmail.com>
2022-07-15 15:57:09 +0200

    (chore): bigger wrapper with exports. Added codegen step

--- packages/cli/src/__tests__/e2e/benchmarks.spec.ts
+++ packages/cli/src/__tests__/e2e/benchmarks.spec.ts
@@ -1,15 +1,18 @@
-import { polywrapCli } from "./utils";
-
-import { runCLI } from "@polywrap/test-env-js";
 import { GetPathToCliTestFiles } from "@polywrap/test-cases";
 import fse from "fs-extra";
 import path from "path";
+import { execSync } from "child_process";
+import { runCLI } from "@polywrap/test-env-js";
+import { polywrapCli } from "./utils";
 
-const { performance } = require('perf_hooks');
+const { performance } = require("perf_hooks");
 jest.setTimeout(500000);
 
 describe("benchmarking", () => {
-  const testCaseRoot = path.join(GetPathToCliTestFiles(), "wasm/build-benchmarks");
+  const testCaseRoot = path.join(
+    GetPathToCliTestFiles(),
+    "wasm/build-benchmarks"
+  );
   const testCases = fse
     .readdirSync(testCaseRoot, { withFileTypes: true })
     .filter((dirent) => dirent.isDirectory())
@@ -17,61 +20,96 @@ describe("benchmarking", () => {
   const getTestCaseDir = (index: number) =>
     path.join(testCaseRoot, testCases[index]);
 
+  let cacheFiles = new Map<string, string>();
 
-    it("Rust images", async () => {
-      fse.removeSync(`${getTestCaseDir(0)}/post/build`);
-      fse.removeSync(`${getTestCaseDir(0)}/post/.polywrap`);
-      const startTime1 = performance.now()
-      
-      const { stderr, stdout } = await runCLI({
-        args: ["build", "-v"],
-        cwd: `${getTestCaseDir(0)}/post`,
-        cli: polywrapCli,
-      });
-      const endTime1 = performance.now()
-      const msTime1 = endTime1 - startTime1
+  const mockFunc = `
+    fn foo() -> &'static str {
+  "foo"
+}
+    `;
 
-      console.log(stderr);
-      console.log(stdout);
+  const modifySource = (wrapperPath: string) => {
+    const libPath = path.join(wrapperPath, "src", "lib.rs");
+    const libFile = fse.readFileSync(libPath, "utf-8");
 
-      console.log(`1st Rust post: ${msTime1.toFixed(2)}ms`);
+    cacheFiles.set(libPath, libFile);
 
-      const startTime2 = performance.now()
-      
-      await runCLI({
-        args: ["build", "-v"],
-        cwd: `${getTestCaseDir(0)}/post`,
-        cli: polywrapCli,
-      });
-      const endTime2 = performance.now()
-      const msTime2 = endTime2 - startTime2
+    const modifiedFile = `${libFile}\n${mockFunc}`;
 
-      console.log(`2nd Rust post: ${msTime2.toFixed(2)}ms`);
+    fse.writeFileSync(libPath, modifiedFile);
+  };
 
-      fse.removeSync(`${getTestCaseDir(0)}/pre/build`);
-      fse.removeSync(`${getTestCaseDir(0)}/pre/.polywrap`);
-      const startTime3 = performance.now()
-      
-      await runCLI({
-        args: ["build", "-v"],
-        cwd: `${getTestCaseDir(0)}/pre`,
-        cli: polywrapCli,
-      });
-      const endTime3 = performance.now()
-      const msTime3 = endTime3 - startTime3
+  const buildImage = async (wrapperPath: string, name: string, msg: string) => {
+    const startTime = performance.now();
 
-      console.log(`1st Rust pre: ${msTime3.toFixed(2)}ms`);
+    await runCLI({
+      args: ["codegen"],
+      cwd: wrapperPath,
+      cli: polywrapCli,
+    });
 
-      const startTime4 = performance.now()
-      
-      await runCLI({
-        args: ["build", "-v"],
-        cwd: `${getTestCaseDir(0)}/pre`,
-        cli: polywrapCli,
-      });
-      const endTime4 = performance.now()
-      const msTime4 = endTime4 - startTime4
+    execSync(`docker build ${wrapperPath} -t ${name}`);
+    const endTime = performance.now();
+    const msTime = endTime - startTime;
 
-      console.log(`2nd Rust pre: ${msTime4.toFixed(2)}ms`);
-    })
+    console.log(`${msg} (${name}): ${msTime.toFixed(2)}ms`);
+  };
+
+  beforeAll(() => {
+    fse.removeSync(`${getTestCaseDir(0)}/optimized/build`);
+    fse.removeSync(`${getTestCaseDir(0)}/optimized/.polywrap`);
+
+    fse.removeSync(`${getTestCaseDir(0)}/current/build`);
+    fse.removeSync(`${getTestCaseDir(0)}/current/.polywrap`);
+  });
+
+  afterAll(() => {
+    for (const [key, value] of cacheFiles) {
+      fse.writeFileSync(key, value);
+    }
+  });
+
+  it("Rust images", async () => {
+    execSync(`docker system prune -a -f`);
+
+    await buildImage(
+      `${getTestCaseDir(0)}/current`,
+      "rust-current",
+      "1st build - no cache"
+    );
+    await buildImage(
+      `${getTestCaseDir(0)}/current`,
+      "rust-current",
+      "2nd build - with cache"
+    );
+
+    modifySource(`${getTestCaseDir(0)}/current`);
+
+    await buildImage(
+      `${getTestCaseDir(0)}/current`,
+      "rust-current",
+      "3rd build - modified source"
+    );
+
+    execSync(`docker system prune -a -f`);
+
+    await buildImage(
+      `${getTestCaseDir(0)}/optimized`,
+      "rust-optimized",
+      "1st build - no cache"
+    );
+    await buildImage(
+      `${getTestCaseDir(0)}/optimized`,
+      "rust-optimized",
+      "2nd build - with cache"
+    );
+
+    modifySource(`${getTestCaseDir(0)}/optimized`);
+
+    await buildImage(
+      `${getTestCaseDir(0)}/optimized`,
+      "rust-optimized",
+      "3rd build - modified source"
+    );
+  });
 });
--- packages/test-cases/cases/cli/wasm/build-benchmarks/001-rust/pre/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub mod wrap;
-pub use wrap::*;
-
-pub fn i8_method(args: ArgsI8Method) -> i8 {
-    args.first + args.second
-}
