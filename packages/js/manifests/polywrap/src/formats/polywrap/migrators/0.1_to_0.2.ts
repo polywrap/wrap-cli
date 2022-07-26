f72a5ec4e468c856557a86b480de9b3b1d07052f
Pileks <jure@amber-it.co>
2022-07-26 11:22:26 +0200

    Add polywrap manifest schema 0.2

---
+++ packages/js/manifests/polywrap/src/formats/polywrap/migrators/0.1_to_0.2.ts
@@ -0,0 +1,15 @@
+export const migrate = undefined;
+
+// import { AnyPolywrapManifest, PolywrapManifest, PolywrapManifest_0_1 } from "..";
+
+// export const migrate = (manifest: PolywrapManifest_0_1): PolywrapManifest => {
+//   console.log("Cannot migrate from 0.1 to 0.2. A manual migration is needed!");
+//   return {
+//     format: "0.2",
+//     project: {
+//       name: "test",
+//       type: "asdf"
+//     },
+//     __type: "PolywrapManifest"
+//   };
+// };
