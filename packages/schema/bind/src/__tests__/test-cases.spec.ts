e58ad8439d3f0c621ee0e491d5ec9531b91ded26
ramil <ramilexe@gmail.com>
2022-08-03 21:40:10 +0300

    for debug only

--- packages/schema/bind/src/__tests__/test-cases.spec.ts
+++ packages/schema/bind/src/__tests__/test-cases.spec.ts
@@ -12,7 +12,7 @@ import {
   OutputEntry
 } from "@polywrap/os-js";
 
-import fs from "fs";
+import fs, {existsSync, mkdirSync} from "fs";
 import path from "path";
 
 describe("Polywrap Binding Test Suite", () => {
@@ -73,6 +73,42 @@ describe("Polywrap Binding Test Suite", () => {
           ),
           JSON.stringify(expectedOutput, null, 2),
         );
+
+        const paths: string[] = [];
+
+        
+        const outputDirectoryEntry = (root: string, entry: OutputEntry) => {
+          const entryPath = path.join(root, entry.name);
+          paths.push(entryPath);
+
+          switch (entry.type) {
+            case "File": {
+              writeFileSync(entryPath, entry.data);
+              break;
+            }
+            case "Directory": {
+              for (const subEntry of entry.data) {
+                if (!existsSync(entryPath)) {
+                  mkdirSync(entryPath, { recursive: true });
+                }
+                outputDirectoryEntry(entryPath, subEntry);
+              }
+              break;
+            }
+            default: {
+              throw Error(
+                  `outputDirectoryEntry: Unknown entry type. Entry: ${JSON.stringify(
+                      entry
+                  )}`
+              );
+            }
+          }
+        };
+
+        for (const entry of output.output.entries) {
+          outputDirectoryEntry(testResultDir, entry);
+        }
+        
 
         expect(output).toMatchObject(expectedOutput);
       }
