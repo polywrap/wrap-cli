2b48a7e36818d10f0a93b93959c96b8fd3360e5e
namesty <nestor09amesty@gmail.com>
2022-08-03 06:08:44 +0200

    (feat): refactored to Sequence and Step classes

--- packages/cli/src/commands/deploy.ts
+++ packages/cli/src/commands/deploy.ts
@@ -2,12 +2,13 @@
 import { Command, Program } from "./types";
 import {
   defaultPolywrapManifest,
-  DeployerHandler,
   DeployPackage,
   intlMsg,
   parseWasmManifestFileOption,
   PolywrapProject,
-  ResultList,
+  Sequence,
+  SequenceResult,
+  Step,
 } from "../lib";
 
 import { DeployManifest } from "@polywrap/polywrap-manifest-types-js";
@@ -15,7 +16,6 @@ import fs from "fs";
 import nodePath from "path";
 import { print } from "gluegun";
 import yaml from "js-yaml";
-import { Uri } from "@polywrap/core-js";
 import { validate } from "jsonschema";
 
 const defaultManifestStr = defaultPolywrapManifest.join(" | ");
@@ -69,8 +69,20 @@ async function run(options: DeployCommandOptions): Promise<void> {
     throw new Error("No deploy manifest found.");
   }
 
+  const allStepsFromAllSequences = Object.entries(
+    deployManifest.sequences
+  ).flatMap(([sequenceName, sequence]) => {
+    return Object.entries(sequence.steps).map(([stepName, step]) => {
+      return {
+        sequenceName,
+        name: stepName,
+        ...step,
+      };
+    });
+  });
+
   const packageNames = [
-    ...new Set(Object.values(deployManifest.stages).map((d) => d.package)),
+    ...new Set(allStepsFromAllSequences.map((step) => step.package)),
   ];
 
   sanitizePackages(packageNames);
@@ -78,95 +90,64 @@ async function run(options: DeployCommandOptions): Promise<void> {
   await project.cacheDeployModules(packageNames);
 
   const packageMap: Record<string, DeployPackage> = {};
-  const stageToPackageMap: Record<string, DeployPackage> = {};
 
   for await (const packageName of packageNames) {
     packageMap[packageName] = await project.getDeployModule(packageName);
   }
 
-  Object.entries(deployManifest.stages).forEach(([stageName, stageValue]) => {
-    stageToPackageMap[stageName] = packageMap[stageValue.package];
-  });
+  const stepToPackageMap: Record<
+    string,
+    DeployPackage & { sequenceName: string }
+  > = {};
 
-  validateManifestWithExts(deployManifest, stageToPackageMap);
-
-  const handlers: Record<string, DeployerHandler> = {};
-  const roots: { handler: DeployerHandler; uri: Uri }[] = [];
-
-  // Create all handlers
-  Object.entries(deployManifest.stages).forEach(([stageName, stageValue]) => {
-    const publisher = stageToPackageMap[stageName].deployer;
-    handlers[stageName] = new DeployerHandler(
-      stageName,
-      publisher,
-      stageValue.config,
-      print
-    );
-  });
-
-  // Establish dependency chains
-  Object.entries(deployManifest.stages).forEach(([key, value]) => {
-    const thisHandler = handlers[key];
-
-    if (value.depends_on) {
-      // Depends on another stage
-      handlers[value.depends_on].addNext(thisHandler);
-    } else if (value.uri) {
-      // It is a root node
-      roots.push({ uri: new Uri(value.uri), handler: thisHandler });
-    } else {
-      throw new Error(
-        `Stage '${key}' needs either previous (depends_on) stage or URI`
-      );
-    }
-  });
-
-  // Execute roots
-
-  const resultLists: ResultList[] = [];
-
-  for await (const root of roots) {
-    print.info(`\nExecuting deployment chain: \n`);
-    root.handler.getDependencyTree().printTree();
-    print.info("");
-    await root.handler.handle(root.uri);
-    resultLists.push(root.handler.getResultsList());
+  for (const step of allStepsFromAllSequences) {
+    stepToPackageMap[step.name] = {
+      ...packageMap[step.package],
+      sequenceName: step.sequenceName,
+    };
   }
 
-  const getResults = (
-    resultList: ResultList,
-    prefix?: string
-  ): {
-    name: string;
-    input: unknown;
-    result: string;
-    id: string;
-  }[] => {
-    const id = prefix ? `${prefix}.${resultList.name}` : resultList.name;
+  validateManifestWithExts(deployManifest, stepToPackageMap);
 
-    return [
-      {
-        id,
-        name: resultList.name,
-        input: resultList.input,
-        result: resultList.result,
-      },
-      ...resultList.children.flatMap((r) => getResults(r, id)),
-    ];
-  };
+  const sequences = Object.entries(deployManifest.sequences).map(
+    ([sequenceName, sequenceValue]) => {
+      const steps = Object.entries(sequenceValue.steps).map(
+        ([stepName, stepValue]) => {
+          return new Step({
+            name: stepName,
+            uriOrStepResult: stepValue.uri,
+            deployer: stepToPackageMap[stepName].deployer,
+            config: stepValue.config ?? {},
+          });
+        }
+      );
+
+      return new Sequence({
+        name: sequenceName,
+        steps,
+        config: sequenceValue.config ?? {},
+        printer: print,
+      });
+    }
+  );
+
+  const sequenceResults: SequenceResult[] = [];
+
+  for await (const sequence of sequences) {
+    const sequenceResult = await sequence.run();
+    sequenceResults.push(sequenceResult);
+  }
 
   if (outputFile) {
-    const resultOutput = resultLists.flatMap((r) => getResults(r));
-
     const outputFileExt = nodePath.extname(outputFile).substring(1);
     if (!outputFileExt) throw new Error("Require output file extension");
     switch (outputFileExt) {
       case "yaml":
       case "yml":
-        fs.writeFileSync(outputFile, yaml.dump(resultOutput));
+        fs.writeFileSync(outputFile, yaml.dump(sequenceResults));
         break;
       case "json":
-        fs.writeFileSync(outputFile, JSON.stringify(resultOutput, null, 2));
+        fs.writeFileSync(outputFile, JSON.stringify(sequenceResults, null, 2));
         break;
       default:
         throw new Error(
@@ -199,15 +180,13 @@ function sanitizePackages(packages: string[]) {
 
 function validateManifestWithExts(
   deployManifest: DeployManifest,
-  stageToPackageMap: Record<string, DeployPackage>
+  stepToPackageMap: Record<string, DeployPackage & { sequenceName: string }>
 ) {
-  const errors = Object.entries(
-    stageToPackageMap
-  ).flatMap(([stageName, deployPackage]) =>
-    deployPackage.manifestExt
+  const errors = Object.entries(stepToPackageMap).flatMap(([stepName, step]) =>
+    step.manifestExt
       ? validate(
-          deployManifest.stages[stageName].config,
-          deployPackage.manifestExt
+          deployManifest.sequences[step.sequenceName].steps[stepName].config,
+          step.manifestExt
         ).errors
       : []
   );
