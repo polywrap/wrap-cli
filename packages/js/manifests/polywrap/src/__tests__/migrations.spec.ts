680e8d6442d78a3677b268d2a19f4daf669f5065
Pileks <jure@amber-it.co>
2022-08-10 12:51:28 +0200

    Polywrap manifest migrations test

---
+++ packages/js/manifests/polywrap/src/__tests__/migrations.spec.ts
@@ -0,0 +1,15 @@
+import { deserializePolywrapManifest } from "../";
+
+import fs from "fs";
+
+describe("Polywrap Manifest Migrations", () => {
+  it("Should succesfully migrate from 0.1.0 to 0.2.0", async() => {
+    const manifestPath = __dirname + "/manifest/polywrap/migrate-0.1.0-to-0.2.0/polywrap-0.1.0.yaml";
+    const expectedManifestPath = __dirname + "/manifest/polywrap/migrate-0.1.0-to-0.2.0/polywrap-0.1.0.yaml";
+
+    const manifest = fs.readFileSync(manifestPath, "utf-8");
+    const expectedManifest = fs.readFileSync(expectedManifestPath, "utf-8");
+
+    expect(deserializePolywrapManifest(manifest)).toEqual(deserializePolywrapManifest(expectedManifest));
+  });
+});
