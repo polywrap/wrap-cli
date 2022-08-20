368f69c43c7bcd058572c1b0848098e1cdc881d3
dOrgJelli <jelli@dorg.tech>
2022-08-19 19:05:59 -0700

    remove all instances of 0.3.0

--- packages/js/manifests/polywrap/src/__tests__/migrations.spec.ts
+++ packages/js/manifests/polywrap/src/__tests__/migrations.spec.ts
@@ -3,9 +3,9 @@ import { deserializePolywrapManifest } from "../";
 import fs from "fs";
 
 describe("Polywrap Manifest Migrations", () => {
-  it("Should succesfully migrate from 0.1.0 to 0.3.0", async() => {
+  it("Should succesfully migrate from 0.1.0 to 0.2.0", async() => {
     const manifestPath = __dirname + "/manifest/polywrap/migrations/polywrap-0.1.0.yaml";
-    const expectedManifestPath = __dirname + "/manifest/polywrap/migrations/polywrap-0.3.0.yaml";
+    const expectedManifestPath = __dirname + "/manifest/polywrap/migrations/polywrap-0.2.0.yaml";
 
     const manifestFile = fs.readFileSync(manifestPath, "utf-8");
     const expectedManifestFile = fs.readFileSync(expectedManifestPath, "utf-8");
@@ -13,19 +13,6 @@ describe("Polywrap Manifest Migrations", () => {
     const manifest = deserializePolywrapManifest(manifestFile);
     const expectedManifest = deserializePolywrapManifest(expectedManifestFile);
 
-    expect(manifest).toEqual(expectedManifest);
-  });
-
-  it("Should succesfully migrate from 0.2 to 0.3.0", async() => {
-    const manifestPath = __dirname + "/manifest/polywrap/migrations/polywrap-0.2.yaml";
-    const expectedManifestPath = __dirname + "/manifest/polywrap/migrations/polywrap-0.3.0.yaml";
-
-    const manifestFile = fs.readFileSync(manifestPath, "utf-8");
-    const expectedManifestFile = fs.readFileSync(expectedManifestPath, "utf-8");
-
-    const manifest = deserializePolywrapManifest(manifestFile);
-    const expectedManifest = deserializePolywrapManifest(expectedManifestFile);
-    
     expect(manifest).toEqual(expectedManifest);
   });
 });
