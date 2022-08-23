8dc905b295287f7efe63eb7b752ef793776b6a8a
Pileks <jure@amber-it.co>
2022-08-23 23:18:46 +0200

    fix order of config application in client constructor

--- packages/js/client/src/PolywrapClient.ts
+++ packages/js/client/src/PolywrapClient.ts
@@ -77,6 +77,10 @@ export class PolywrapClient implements Client {
 
       const builder = new ClientConfigBuilder();
 
+      if (!options?.noDefaults) {
+        builder.addDefaults();
+      }
+      
       if (config) {
         builder.add(config);
 
@@ -87,10 +91,6 @@ export class PolywrapClient implements Client {
 
       if (!this._wrapperCache) {
         this._wrapperCache = new SimpleCache();
-      }
-
-      if (!options?.noDefaults) {
-        builder.addDefaults();
       }
 
       const sanitizedConfig = builder.build();
