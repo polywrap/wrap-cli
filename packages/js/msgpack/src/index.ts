f1db786f034f6732cf276a810c7623f8b5482c69
Niraj Kamdar <nirajkamdar27@pm.me>
2022-09-02 17:47:19 +0400

    refactor: remove unnecessary code

--- packages/js/msgpack/src/index.ts
+++ packages/js/msgpack/src/index.ts
@@ -1,13 +1,5 @@
 import { Encoder, Decoder, ExtensionCodec } from "@msgpack/msgpack";
 
-export function isBuffer(maybeBuf: unknown): maybeBuf is BufferSource {
-  if (maybeBuf instanceof ArrayBuffer || ArrayBuffer.isView(maybeBuf)) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
 enum ExtensionTypes {
   // must be in range 0-127
   GENERIC_MAP = 1,
