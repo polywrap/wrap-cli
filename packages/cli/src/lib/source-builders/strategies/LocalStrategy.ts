69ff95a15e5a31264f832a638bdbc21e71ab46e0
namesty <nestor09amesty@gmail.com>
2022-09-07 03:09:29 +0200

    (chore): updated local strategy

--- packages/cli/src/lib/source-builders/strategies/LocalStrategy.ts
+++ packages/cli/src/lib/source-builders/strategies/LocalStrategy.ts
@@ -1,13 +1,31 @@
-import { runCommand } from "../../system";
 import { SourceBuildStrategy } from "../SourceBuilder";
+
+import path from "path";
+import { exec } from "child_process";
 
 export class LocalBuildStrategy extends SourceBuildStrategy<void> {
   public async build(): Promise<void> {
-    const manifest = await this.project.getManifest();
-    const projectType = manifest.project.type; // e.g. "wasm/rust"
-    await runCommand(
-      `${__dirname}/../../defaults/build-scripts/${projectType}`,
-      true
-    );
+    const manifestDir = await this.project.getManifestDir();
+
+    const bindLanguage = await this.project.getManifestLanguage();
+    let abortPath = path.join(manifestDir, "src/wrap/entry/wrapAbort");
+    abortPath =
+      bindLanguage === "wasm/assemblyscript"
+        ? abortPath.replace("./", "")
+        : abortPath;
+    const scriptPath = `${__dirname}/../../defaults/build-scripts/${bindLanguage}.sh`;
+    const command = `chmod +x ${scriptPath} && ${scriptPath} ${manifestDir} ${this.outputDir} ${abortPath}`;
+
+    await new Promise<void>((res, rej) => {
+      exec(command, { cwd: manifestDir }, (err, stdout, stderr) => {
+        console.log(stdout);
+        console.error(stderr);
+        if (err) {
+          rej(err);
+        } else {
+          res();
+        }
+      });
+    });
   }
 }
