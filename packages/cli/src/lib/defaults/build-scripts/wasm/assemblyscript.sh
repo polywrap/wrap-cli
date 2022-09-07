7257f0a3a62345b80e97f5e5d8b5d34e8d3ca1df
namesty <nestor09amesty@gmail.com>
2022-09-08 00:41:26 +0200

    (fix): rust script env variables and AS shebang

--- packages/cli/src/lib/defaults/build-scripts/wasm/assemblyscript.sh
+++ packages/cli/src/lib/defaults/build-scripts/wasm/assemblyscript.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 asc "$1"/src/wrap/entry.ts \
   --path ./node_modules \
   --outFile "$2"/wrap.wasm \
--- packages/cli/src/lib/defaults/build-scripts/wasm/rust.sh
+++ packages/cli/src/lib/defaults/build-scripts/wasm/rust.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rustup target add wasm32-unknown-unknown
-RUSTFLAGS="-C link-arg=-z -C link-arg=stack-size=65536 -C link-arg=--import-memory"
+export RUSTFLAGS="-C link-arg=-z -C link-arg=stack-size=65536 -C link-arg=--import-memory"
 cargo install toml-cli
 # cargo install wasm-snip
 cargo install wasm-bindgen-cli
@@ -21,8 +21,9 @@ toml set "$1"/Cargo.toml package.name "module" > "$1"/Cargo-local.toml && \
 
 cargo build --manifest-path "$1"/Cargo.toml \
   --target wasm32-unknown-unknown --release
-WASM_INTERFACE_TYPES=1
-# wasm-bindgen "$1"/target/wasm32-unknown-unknown/release/module.wasm --out-dir "$2" --out-name bg_module.wasm
-# wasm-snip "$2"/bg_module.wasm -o "$2"/snipped_module.wasm && \
-#   rm -rf "$2"/bg_module.wasm
-npx wasm-opt --asyncify -Os "$1"/target/wasm32-unknown-unknown/release/module.wasm -o "$2"/wrap.wasm
+export WASM_INTERFACE_TYPES=1
+wasm-bindgen "$1"/target/wasm32-unknown-unknown/release/module.wasm --out-dir "$2" --out-name bg_module.wasm
+wasm-snip "$2"/bg_module.wasm -o "$2"/snipped_module.wasm && \
+  rm -rf "$2"/bg_module.wasm
+npx wasm-opt --asyncify -Os "$2"/snipped_module.wasm -o "$2"/wrap.wasm
+  rm -rf "$2"/snipped_module.wasm
