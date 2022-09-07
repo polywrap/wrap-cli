d326b0d3be4a03012594eabe7c9df7be7c4e42bd
namesty <nestor09amesty@gmail.com>
2022-09-07 03:09:05 +0200

    (chore): updated build scripts

--- packages/cli/src/lib/defaults/build-scripts/wasm/assemblyscript.sh
+++ packages/cli/src/lib/defaults/build-scripts/wasm/assemblyscript.sh
@@ -1,9 +1,7 @@
-#!/bin/sh
-yarn
-./node_modules/.bin/asc ./wrap/entry.ts \
+asc "$1"/src/wrap/entry.ts \
   --path ./node_modules \
-  --outFile ./build/wrap.wasm \
-  --use abort=./wrap/entry/wrapAbort \
+  --outFile "$2"/wrap.wasm \
+  --use abort="$3" \
   --optimize --importMemory \
   --runtime stub \
   --runPasses asyncify
--- packages/cli/src/lib/defaults/build-scripts/wasm/rust.sh
+++ packages/cli/src/lib/defaults/build-scripts/wasm/rust.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
+rustup target add wasm32-unknown-unknown
 RUSTFLAGS="-C link-arg=-z -C link-arg=stack-size=65536 -C link-arg=--import-memory"
-cargo build --manifest-path ./Cargo.toml \
+cargo install toml-cli
+# cargo install wasm-snip
+cargo install wasm-bindgen-cli
+
+toml set "$1"/Cargo.toml lib.crate-type ["cdylib"] > "$1"/Cargo-local.toml && \
+  rm -rf "$1"/Cargo.toml && \
+  mv "$1"/Cargo-local.toml "$1"/Cargo.toml && \
+  true
+
+# Clean up artifacts left by the toml CLI program ("["cdylib"]" -> ["cdylib"])
+sed -i 's/"\[cdylib\]"/\["cdylib"\]/g' "$1"/Cargo.toml
+
+# Ensure the package name = "module"
+toml set "$1"/Cargo.toml package.name "module" > "$1"/Cargo-local.toml && \
+  rm -rf "$1"/Cargo.toml && \
+  mv "$1"/Cargo-local.toml "$1"/Cargo.toml && \
+  true
+
+cargo build --manifest-path "$1"/Cargo.toml \
   --target wasm32-unknown-unknown --release
-rm -rf ./build
-mkdir ./build
 WASM_INTERFACE_TYPES=1
-wasm-bindgen ./target/wasm32-unknown-unknown/release/module.wasm --out-dir ./build --out-name bg_module.wasm
-wasm-snip ./build/bg_module.wasm -o ./build/snipped_module.wasm && \
-  rm -rf ./build/bg_module.wasm
-wasm-opt --asyncify -Os ./build/snipped_module.wasm -o ./build/wrap.wasm && \
-  rm -rf ./build/snipped_module.wasm
+# wasm-bindgen "$1"/target/wasm32-unknown-unknown/release/module.wasm --out-dir "$2" --out-name bg_module.wasm
+# wasm-snip "$2"/bg_module.wasm -o "$2"/snipped_module.wasm && \
+#   rm -rf "$2"/bg_module.wasm
+npx wasm-opt --asyncify -Os "$1"/target/wasm32-unknown-unknown/release/module.wasm -o "$2"/wrap.wasm
