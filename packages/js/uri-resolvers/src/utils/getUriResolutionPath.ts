873bc9c34e3259286e163a0941de4bcd9cad87e9
dOrgJelli <jelli@dorg.tech>
2022-09-13 22:10:41 +0200

    chore: add comments to getUriResolutionPath function

--- packages/js/uri-resolvers/src/utils/getUriResolutionPath.ts
+++ packages/js/uri-resolvers/src/utils/getUriResolutionPath.ts
@@ -3,8 +3,10 @@ import { IUriResolutionStep } from "@polywrap/core-js";
 export const getUriResolutionPath = (
   history: IUriResolutionStep<unknown>[]
 ): IUriResolutionStep<unknown>[] => {
+  // Get all non-empty items from the resolution history
   return history
     .filter((x) => {
+      // If a failure happened
       if (!x.result.ok) {
         return true;
       }
@@ -12,15 +14,19 @@ export const getUriResolutionPath = (
       const uriPackageOrWrapper = x.result.value;
 
       if (uriPackageOrWrapper.type === "uri") {
+        // If a redirect has occured
         return uriPackageOrWrapper.uri.uri !== x.sourceUri.uri;
       } else if (uriPackageOrWrapper.type === "package") {
+        // If a package was returned
         return true;
       } else if (uriPackageOrWrapper.type === "wrapper") {
+        // If a wrapper was returned
         return true;
       }
 
       return false;
     })
+    // Filter all sub-history
     .map((x) => {
       if (x.subHistory && x.subHistory.length) {
         return {
