a2c778a9c355e93030f35f898eec0708d194cd9e
nerfZael <bogunovij@gmail.com>
2022-09-15 12:01:51 +0200

    replaced toUri with Uri.from

--- packages/js/client-config-builder/src/utils/toUri.ts
+++
@@ -1,9 +0,0 @@
-import { Uri } from "@polywrap/core-js";
-
-export function toUri(uri: Uri | string): Uri {
-  if (typeof uri === "string") {
-    return new Uri(uri);
-  } else {
-    return uri;
-  }
-}
