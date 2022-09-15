0c3d0b52dfe1a4fe31b33c9f40bc127ce8c4e682
nerfZael <bogunovij@gmail.com>
2022-09-15 13:18:10 +0200

    exported package and wrapper registrations

--- packages/js/uri-resolvers/src/helpers/index.ts
+++ packages/js/uri-resolvers/src/helpers/index.ts
@@ -1,5 +1,7 @@
 export * from "./InfiniteLoopError";
+export * from "./PackageRegistration";
 export * from "./RecursiveResolver";
 export * from "./ResolverWithHistory";
 export * from "./ResolverWithLoopGuard";
 export * from "./UriResolverLike";
+export * from "./WrapperRegistration";
