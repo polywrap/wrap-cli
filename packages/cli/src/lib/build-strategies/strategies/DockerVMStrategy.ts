9320c4a5a67b29aa69241afaac0bd29f75fa52a2
namesty <nestor09amesty@gmail.com>
2022-09-15 07:19:19 +0200

    (chore): naive strategy for assemblyscript

--- packages/cli/src/lib/build-strategies/strategies/DockerVMStrategy.ts
+++ packages/cli/src/lib/build-strategies/strategies/DockerVMStrategy.ts
@@ -6,12 +6,39 @@ import {
 } from "../../system";
 import { BuildStrategyArgs, BuildStrategy } from "../BuildStrategy";
 import { intlMsg } from "../../intl";
+import { PolywrapProject } from "../../project";
 
 import fse from "fs-extra";
 import path from "path";
 
 type BuildImageId = string;
 const VOLUME_DIR_CACHE_SUBPATH = "build/volume";
+const CONTAINER_NAME = "vm-build-container";
+const VM_SCRIPTS_DIR = path.join(
+  __dirname,
+  "..",
+  "..",
+  "defaults",
+  "build-vm",
+  "scripts"
+);
+
+interface BuildManifestConfig {
+  [k: string]: unknown;
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  polywrap_module?: {
+    name: string;
+    dir: string;
+  };
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  polywrap_linked_packages?: {
+    dir: string;
+    name: string;
+  }[];
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  polywrap_manifests: string[];
+  include?: string[];
+}
 
 export class DockerVMBuildStrategy extends BuildStrategy<BuildImageId> {
   private _dockerLock: FileLock;
@@ -35,31 +62,98 @@ export class DockerVMBuildStrategy extends BuildStrategy<BuildImageId> {
     await this._dockerLock.request();
     try {
       await ensureDockerDaemonRunning();
-      const cacheDir = this.project.getCachePath(VOLUME_DIR_CACHE_SUBPATH);
+      const volumePaths = {
+        project: this.project.getCachePath(VOLUME_DIR_CACHE_SUBPATH),
+        linkedPackages: this.project.getCachePath(
+          PolywrapProject.cacheLayout.buildLinkedPackagesDir
+        ),
+        build: this.project.getCachePath("build/output"),
+      };
       const manifestDir = this.project.getManifestDir();
       const buildManifest = await this.project.getBuildManifest();
-      const includePaths = (buildManifest.config?.include as string[]) || [];
-      const scriptPath = path.join(__dirname, "..", "..", "defaults", "build-vm", "scripts");
+      const buildManifestConfig = buildManifest.config as BuildManifestConfig;
 
-      includePaths
-        .map((includePath) => path.join(manifestDir, includePath))
-        .forEach((includePath) => {
-          if (!fse.existsSync(includePath)) {
-            throw new Error(
-              `File to include with path ${includePath} does not exist`
-            );
-          }
+      // Copy manifests
+      buildManifestConfig.polywrap_manifests.forEach((manifestPath) => {
+        fse.copyFileSync(
+          path.join(manifestDir, manifestPath),
+          path.join(volumePaths.project, manifestPath)
+        );
+      });
 
-          fse.copySync(includePath, path.join(cacheDir, includePath));
-        });
-
-      await runCommand(
-        `docker run -v ${path.resolve(
-          cacheDir
-        )}:/project --name vm-build-container polywrap-vm-as /bin/bash ${}`
+      // Link Packages
+      let packageJson = fse.readJsonSync(
+        path.join(manifestDir, "package.json")
       );
 
-      await runCommand(`docker run -v ${cacheDir}:/project polywrap-vm-ts`);
+      if (buildManifestConfig.polywrap_linked_packages) {
+        await this.project.cacheBuildManifestLinkedPackages();
+
+        packageJson = {
+          ...packageJson,
+          dependencies: {
+            ...packageJson.dependencies,
+            ...buildManifestConfig.polywrap_linked_packages.reduce(
+              (acc, pkg) => {
+                acc[pkg.name] = `../linked-packages/${pkg.name}`;
+                return acc;
+              },
+              {} as Record<string, string>
+            ),
+          },
+        };
+      }
+
+      fse.writeJsonSync(
+        path.join(volumePaths.project, "package.json"),
+        packageJson
+      );
+
+      // Copy includes
+      if (buildManifestConfig.include) {
+        buildManifestConfig.include.forEach((includePath) => {
+          fse.copySync(
+            path.join(manifestDir, includePath),
+            path.join(volumePaths.project, includePath)
+          );
+        });
+      }
+
+      // Copy sources and build
+      if (buildManifestConfig.polywrap_module) {
+        fse.copySync(
+          path.join(manifestDir, buildManifestConfig.polywrap_module.dir),
+          path.join(
+            volumePaths.project,
+            buildManifestConfig.polywrap_module.dir
+          )
+        );
+
+        const buildCommand = `./node_modules/.bin/asc ${buildManifestConfig.polywrap_module.dir}/wrap/entry.ts \
+        --path ./node_modules \
+        --outFile ./build/wrap.wasm \
+        --use abort=${buildManifestConfig.polywrap_module.dir}/wrap/entry/wrapAbort \
+        --optimize --importMemory \
+        --runtime stub \
+        --runPasses asyncify`;
+
+        await runCommand(
+          `docker run -v ${path.resolve(
+            volumePaths.project
+          )}:/project -v ${path.resolve(
+            volumePaths.linkedPackages
+          )}:/linked-packages --name ${CONTAINER_NAME} -ti polywrap-vm-as /bin/bash -c "${buildCommand}"`
+        );
+
+        // Copy build output
+        await runCommand(
+          `docker cp ${CONTAINER_NAME}:/project/build/wrap.wasm ${this.outputDir}`,
+          this.project.quiet
+        );
+
+        // Remove container
+        await runCommand(`docker container rm -f ${CONTAINER_NAME}`);
+      }
 
       await this._dockerLock.release();
 
