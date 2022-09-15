98cbaa1ab3274a3883fda7116129845b18b01713
nerfZael <bogunovij@gmail.com>
2022-09-15 13:13:30 +0200

    removed plugins from uri-resolvers-js

--- packages/js/uri-resolvers/src/index.ts
+++ packages/js/uri-resolvers/src/index.ts
@@ -1,7 +1,6 @@
 export * from "./aggregator";
 export * from "./cache";
 export * from "./legacy";
-export * from "./plugins";
 export * from "./redirects";
 export * from "./packages";
 export * from "./helpers";
