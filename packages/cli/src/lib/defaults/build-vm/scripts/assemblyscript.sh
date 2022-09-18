e30817874d985cdfad1d562c07dcc8803adcd02a
namesty <nestor09amesty@gmail.com>
2022-09-18 16:05:04 +0200

    (chore): removed scriptHelpers in favor of script files

--- packages/cli/src/lib/defaults/build-vm/scripts/assemblyscript.sh
+++
@@ -1,7 +0,0 @@
-asc src/wrap/entry.ts \
-    --path ./node_modules \
-    --outFile ./build/wrap.wasm \
-    --use abort=src/wrap/entry/wrapAbort \
-    --optimize --importMemory \
-    --runtime stub \
-    --runPasses asyncify
