af30757c476553b16476d7cc05a81356619088f1
namesty <nestor09amesty@gmail.com>
2022-09-20 01:57:05 +0200

    (chore): project being built and artifacts being copied

--- packages/cli/src/lib/build-strategies/strategies/DockerVMStrategy.ts
+++ packages/cli/src/lib/build-strategies/strategies/DockerVMStrategy.ts
@@ -7,7 +7,6 @@ import {
 import { BuildStrategyArgs, BuildStrategy } from "../BuildStrategy";
 import { intlMsg } from "../../intl";
 import { PolywrapManifestLanguage, PolywrapProject } from "../../project";
-import { withSpinner } from "../../helpers";
 
 import fse from "fs-extra";
 import path from "path";
@@ -15,19 +14,17 @@ import Mustache from "mustache";
 
 type BuildImageId = string;
 const VOLUME_DIR_CACHE_SUBPATH = "build/volume";
-const CONTAINER_NAME = "vm-build-container";
 const VM_SCRIPTS_DIR = path.join(
   __dirname,
   "..",
   "..",
   "defaults",
-  "build-vm",
-  "scripts"
+  "build-vm-scripts"
 );
-const SOURCE_MANIFEST_MAP: Record<PolywrapManifestLanguage, string> = {
-  "wasm/assemblyscript": "package.json",
-  "wasm/rust": "Cargo.toml",
-  interface: "package.json",
+const ADDITIONAL_INCLUDES: Record<PolywrapManifestLanguage, string[]> = {
+  "wasm/assemblyscript": ["package.json", "node_modules"],
+  "wasm/rust": ["Cargo.toml", "Cargo.lock", "target"],
+  interface: ["package.json"],
 };
 
 interface BuildManifestConfig {
@@ -67,99 +64,103 @@ export class DockerVMBuildStrategy extends BuildStrategy<BuildImageId> {
 
   public async build(): Promise<string> {
     await this._dockerLock.request();
-    const run = async () => {
-      try {
-        await ensureDockerDaemonRunning();
-        const volumePaths = {
-          project: this.project.getCachePath(VOLUME_DIR_CACHE_SUBPATH),
-          linkedPackages: this.project.getCachePath(
-            PolywrapProject.cacheLayout.buildLinkedPackagesDir
-          ),
-          build: this.project.getCachePath("build/output"),
-        };
-        const manifestDir = this.project.getManifestDir();
-        const buildManifest = await this.project.getBuildManifest();
-        const buildManifestConfig = buildManifest.config as BuildManifestConfig;
+    try {
+      await ensureDockerDaemonRunning();
+      const volumePaths = {
+        project: this.project.getCachePath(VOLUME_DIR_CACHE_SUBPATH),
+        linkedPackages: this.project.getCachePath(
+          PolywrapProject.cacheLayout.buildLinkedPackagesDir
+        ),
+      };
 
-        // Copy manifests
-        buildManifestConfig.polywrap_manifests.forEach((manifestPath) => {
-          fse.copyFileSync(
-            path.join(manifestDir, manifestPath),
-            path.join(volumePaths.project, manifestPath)
+      if (fse.existsSync(volumePaths.project)) {
+        fse.removeSync(volumePaths.project);
+      }
+
+      const manifestDir = this.project.getManifestDir();
+      const buildManifest = await this.project.getBuildManifest();
+      const buildManifestConfig = buildManifest.config as BuildManifestConfig;
+
+      // Copy manifests
+      buildManifestConfig.polywrap_manifests.forEach((manifestPath) => {
+        fse.copySync(
+          path.join(manifestDir, manifestPath),
+          path.join(volumePaths.project, manifestPath)
+        );
+      });
+
+      const language = await this.project.getManifestLanguage();
+
+      // Copy additional includes
+
+      ADDITIONAL_INCLUDES[language].forEach((include) => {
+        if (fse.existsSync(path.join(manifestDir, include))) {
+          fse.copySync(
+            path.join(manifestDir, include),
+            path.join(volumePaths.project, include),
+            {
+              overwrite: false,
+            }
+          );
+        }
+      });
+
+      // Copy includes
+      if (buildManifestConfig.include) {
+        buildManifestConfig.include.forEach((includePath) => {
+          fse.copySync(
+            path.join(manifestDir, includePath),
+            path.join(volumePaths.project, includePath),
+            {
+              overwrite: false,
+            }
           );
         });
+      }
 
-        const language = await this.project.getManifestLanguage();
-
-        fse.copyFileSync(
-          path.join(manifestDir, SOURCE_MANIFEST_MAP[language]),
-          path.join(volumePaths.project, SOURCE_MANIFEST_MAP[language])
+      // Copy sources and build
+      if (buildManifestConfig.polywrap_module) {
+        fse.copySync(
+          path.join(manifestDir, buildManifestConfig.polywrap_module.dir),
+          path.join(
+            volumePaths.project,
+            buildManifestConfig.polywrap_module.dir
+          )
         );
 
-        // Link Packages
-        await this.project.cacheBuildManifestLinkedPackages();
+        const scriptTemplate = fse.readFileSync(
+          path.join(VM_SCRIPTS_DIR, `${language}.mustache`),
+          "utf8"
+        );
 
-        // Copy includes
-        if (buildManifestConfig.include) {
-          buildManifestConfig.include.forEach((includePath) => {
-            fse.copySync(
-              path.join(manifestDir, includePath),
-              path.join(volumePaths.project, includePath)
-            );
-          });
-        }
+        const scriptContent = Mustache.render(
+          scriptTemplate,
+          buildManifestConfig
+        );
+        const buildScriptPath = path.join(
+          volumePaths.project,
+          "polywrap-build.sh"
+        );
+        fse.writeFileSync(buildScriptPath, scriptContent);
 
-        // Copy sources and build
-        if (buildManifestConfig.polywrap_module) {
-          fse.copySync(
-            path.join(manifestDir, buildManifestConfig.polywrap_module.dir),
-            path.join(
-              volumePaths.project,
-              buildManifestConfig.polywrap_module.dir
-            )
-          );
+        await runCommand(
+          `docker run --rm -v ${path.resolve(
+            volumePaths.project
+          )}:/project -v ${path.resolve(
+            volumePaths.linkedPackages
+          )}:/linked-packages namesty/base-assemblyscript:latest /bin/bash -c "${scriptContent}"`
+        );
 
-          const scriptContent = Mustache.render(
-            path.join(VM_SCRIPTS_DIR, `${language}.mustache`),
-            buildManifestConfig
-          );
-          const buildScriptPath = path.join(volumePaths.project, "build.sh");
-          fse.writeFileSync(buildScriptPath, scriptContent);
-
-          await runCommand(
-            `docker run -v ${path.resolve(
-              volumePaths.project
-            )}:/project -v ${path.resolve(
-              volumePaths.linkedPackages
-            )}:/linked-packages --name ${CONTAINER_NAME} -ti polywrap-vm-as /bin/bash ${buildScriptPath}`
-          );
-
-          // Copy build output
-          await runCommand(
-            `docker cp ${CONTAINER_NAME}:/project/build/ ${this.outputDir}`,
-            this.project.quiet
-          );
-
-          // Remove container
-          await runCommand(`docker container rm -f ${CONTAINER_NAME}`);
-        }
-
-        await this._dockerLock.release();
-
-        return "";
-      } catch (e) {
-        await this._dockerLock.release();
-        throw e;
+        // Copy build output
+        fse.copySync(path.join(volumePaths.project, "build"), this.outputDir);
       }
-    };
 
-    return await withSpinner(
-      "Building VM Sources",
-      "Error Building VM Sources",
-      "Warning Building VM Sources",
-      async () => {
-        return await run();
-      }
-    );
+      await this._dockerLock.release();
+
+      return "";
+    } catch (e) {
+      await this._dockerLock.release();
+      throw e;
+    }
   }
 }
