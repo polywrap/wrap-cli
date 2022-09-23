85a82948b306744919ef3fe91a019d2631e4a8af
Jure Bogunovic <bogunovij@gmail.com>
2022-09-23 12:49:27 +0200

    fixed resolverLike still named resolvable

--- packages/js/uri-resolvers/src/utils/buildUriResolver.ts
+++ packages/js/uri-resolvers/src/utils/buildUriResolver.ts
@@ -16,51 +16,51 @@ import { PackageRegistration } from "../helpers/PackageRegistration";
 import { WrapperRegistration } from "../helpers/WrapperRegistration";
 
 export const buildUriResolver = <TError = undefined>(
-  resolvable: UriResolverLike,
+  resolverLike: UriResolverLike,
   resolverName?: string
 ): IUriResolver<TError> => {
-  if (Array.isArray(resolvable)) {
+  if (Array.isArray(resolverLike)) {
     return new UriResolverAggregator(
-      (resolvable as UriResolverLike[]).map((x) =>
+      (resolverLike as UriResolverLike[]).map((x) =>
         buildUriResolver(x, resolverName)
       ),
       resolverName
     ) as IUriResolver<TError>;
-  } else if (typeof resolvable === "function") {
+  } else if (typeof resolverLike === "function") {
     return new UriResolverAggregator(
-      resolvable as (
+      resolverLike as (
         uri: Uri,
         client: Client
       ) => Promise<Result<IUriResolver[], unknown>>,
       resolverName
     ) as IUriResolver<TError>;
-  } else if ((resolvable as Partial<IWrapPackage>).createWrapper) {
-    const uriPackage = resolvable as IUriPackage;
+  } else if ((resolverLike as Partial<IWrapPackage>).createWrapper) {
+    const uriPackage = resolverLike as IUriPackage;
     return (new PackageResolver(
       uriPackage.uri,
       uriPackage.package
     ) as unknown) as IUriResolver<TError>;
-  } else if ((resolvable as IUriResolver).tryResolveUri !== undefined) {
-    return resolvable as IUriResolver<TError>;
+  } else if ((resolverLike as IUriResolver).tryResolveUri !== undefined) {
+    return resolverLike as IUriResolver<TError>;
   } else if (
-    (resolvable as PackageRegistration).uri !== undefined &&
-    (resolvable as PackageRegistration).package !== undefined
+    (resolverLike as PackageRegistration).uri !== undefined &&
+    (resolverLike as PackageRegistration).package !== undefined
   ) {
-    const uriPackage = resolvable as PackageRegistration;
+    const uriPackage = resolverLike as PackageRegistration;
     return (new PackageResolver(
       toUri(uriPackage.uri),
       uriPackage.package
     ) as unknown) as IUriResolver<TError>;
   } else if (
-    (resolvable as WrapperRegistration).uri !== undefined &&
-    (resolvable as WrapperRegistration).wrapper !== undefined
+    (resolverLike as WrapperRegistration).uri !== undefined &&
+    (resolverLike as WrapperRegistration).wrapper !== undefined
   ) {
-    const uriWrapper = resolvable as WrapperRegistration;
+    const uriWrapper = resolverLike as WrapperRegistration;
     return (new WrapperResolver(
       toUri(uriWrapper.uri),
       uriWrapper.wrapper
     ) as unknown) as IUriResolver<TError>;
   } else {
-    throw new Error("Unknown resolvable type");
+    throw new Error("Unknown resolver-like type");
   }
 };
