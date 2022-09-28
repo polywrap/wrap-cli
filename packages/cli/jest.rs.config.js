7f53b4280e616c5467291d57f0047c5ddfb95f68
namesty <nestor09amesty@gmail.com>
2022-09-28 02:03:55 +0200

    (fix): jest rs config for cli fix

--- packages/cli/jest.rs.config.js
+++ packages/cli/jest.rs.config.js
@@ -1,5 +1,24 @@
 module.exports = {
-  ...require("./jest.config"),
+  collectCoverage: true,
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)"],
+  globals: {
+    "ts-jest": {
+      diagnostics: false
+    },
+  },
+  modulePathIgnorePatterns: [
+    "<rootDir>/src/__tests__/project/.polywrap",
+    "<rootDir>/src/__tests__/e2e/build-rs.spec.ts"
+  ],
+  testPathIgnorePatterns: [
+    "<rootDir>/src/__tests__/project/.polywrap"
+  ],
+  transformIgnorePatterns: [
+    "<rootDir>/src/__tests__/project/.polywrap"
+  ],
+  setupFilesAfterEnv: ["./jest.setup.js"],
   testMatch: ["**/build-rs.spec.ts"],
   modulePathIgnorePatterns: [],
 };
