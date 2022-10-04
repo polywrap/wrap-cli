303cb68dbc23610f044a9d4aed668fa50febcfd0
namesty <nestor09amesty@gmail.com>
2022-10-04 16:39:58 +0200

    (chore): docker lock test fix

--- packages/cli/src/__tests__/unit/docker.spec.ts
+++ packages/cli/src/__tests__/unit/docker.spec.ts
@@ -19,7 +19,7 @@ describe("e2e tests for docker", () => {
       promises.push(
         runCLI({
           args: ["build", "-v", "-s", "image"],
-          cwd: path.join(GetPathToCliTestFiles(), "wasm/build-cmd/001-sanity-assemblyscript"),
+          cwd: path.join(GetPathToCliTestFiles(), "wasm/build-cmd/assemblyscript/001-sanity"),
           cli: polywrapCli
         }).then((result: { exitCode: number; stdout: string; stderr: string }) => {
           const { exitCode, stderr } = result;
