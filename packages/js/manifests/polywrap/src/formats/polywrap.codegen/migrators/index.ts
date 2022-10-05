19e60ee388fbdfbe16cdea32f53153cbc3702648
krisbitney <kristoferbitney@hotmail.com>
2022-10-05 12:48:48 +0500

    removed unused placeholder codegen manifest migrator

--- packages/js/manifests/polywrap/src/formats/polywrap.codegen/migrators/index.ts
+++ packages/js/manifests/polywrap/src/formats/polywrap.codegen/migrators/index.ts
@@ -1,10 +1,3 @@
 import { Migrator } from "../../../migrations";
-import { migrate as migrate_0_1_0_to_0_1_0 } from "./0.1.0_to_0.1.0";
 
-export const migrators: Migrator[] = [
-  {
-    from: "0.1.0",
-    to: "0.1.0",
-    migrate: migrate_0_1_0_to_0_1_0,
-  },
-];
+export const migrators: Migrator[] = [];
