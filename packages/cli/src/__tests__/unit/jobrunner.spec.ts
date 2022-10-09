fbb567f06b148fdf9a6b41801393d495d337d5e8
Jure Bogunovic <bogunovij@gmail.com>
2022-10-09 15:36:53 +0200

    fixed issues after merge with remote

--- packages/cli/src/__tests__/unit/jobrunner.spec.ts
+++ packages/cli/src/__tests__/unit/jobrunner.spec.ts
@@ -3,23 +3,26 @@ import { buildWrapper } from "@polywrap/test-env-js";
 import { testCases } from "./jobrunner-test-cases";
 import { JobRunner } from "../../lib";
 import path from "path";
-import { ClientConfigBuilder } from "@polywrap/client-config-builder-js";
+import { ClientConfigBuilder, CustomClientConfig } from "@polywrap/client-config-builder-js";
+import { Uri } from "@polywrap/core-js";
 
 jest.setTimeout(200000);
 
 describe("workflow JobRunner", () => {
+  let defaultConfig: CustomClientConfig<Uri | string>;
+
   beforeAll(async () => {
     await buildWrapper(
       path.join(GetPathToTestWrappers(), "wasm-as", "simple-calculator")
     );
+    defaultConfig = new ClientConfigBuilder().addDefaults().build();
   });
 
   for (const testCase of testCases) {
     it(testCase.name, async () => {
-      expect(client).toBeTruthy();
       const ids = Object.keys(testCase.workflow.jobs);
       const jobRunner = new JobRunner(
-        new ClientConfigBuilder().addDefaults().build(),
+        defaultConfig,
         testCase.onExecution
       );
       await jobRunner.run(testCase.workflow.jobs, ids);
