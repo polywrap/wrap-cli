4c317f0774a81085ccacde6b52a27beffd7a544c
Jure Bogunovic <bogunovij@gmail.com>
2022-10-10 19:32:53 +0200

    reordered props

--- packages/js/client-config-builder/src/CustomClientConfig.ts
+++ packages/js/client-config-builder/src/CustomClientConfig.ts
@@ -9,9 +9,9 @@ import {
 import { UriResolverLike } from "@polywrap/uri-resolvers-js";
 
 export type CustomClientConfig<TUri extends Uri | string> = {
-  readonly redirects: IUriRedirect<TUri>[];
-  readonly interfaces: InterfaceImplementations<TUri>[];
   readonly envs: Env<TUri>[];
+  readonly interfaces: InterfaceImplementations<TUri>[];
+  readonly redirects: IUriRedirect<TUri>[];
   readonly wrappers: IUriWrapper<TUri>[];
   readonly packages: IUriPackage<TUri>[];
   readonly resolvers: UriResolverLike[];
