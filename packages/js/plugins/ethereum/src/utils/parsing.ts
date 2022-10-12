1a5fc732cdf7d1248b8fe5c99a9c2005f37e5a32
dOrgJelli <jelli@dorg.tech>
2022-10-12 18:00:21 +0200

    fix: proper view method result parsing

--- packages/js/plugins/ethereum/src/utils/parsing.ts
+++ packages/js/plugins/ethereum/src/utils/parsing.ts
@@ -26,41 +26,23 @@ export function constructAbi(method: string): any[] {
   return abi;
 }
 
-export function parseResult(abi: any[], result: any[] | any): string {
-  if (!(result instanceof Array)) {
-    // if not array, return single value
-    return result.toString();
-  }
-  const isRawAbi = abi[0] instanceof Object;
-  if (!isRawAbi) {
-    return stringifySimpleArray(result);
-  }
-  const outputs = abi[0].outputs;
-  const returnIsStruct = outputs.length > 0 && "components" in outputs[0];
-  if (returnIsStruct) {
-    return stringifyStruct(abi, result);
-  } else {
-    return stringifySimpleArray(result);
-  }
+export function parseResult(result: any): string {
+  return JSON.stringify(prepForStringify(result));
 }
 
-function stringifyStruct(abi: any[], result: any): string {
-  const objects: Record<string, string>[] = [];
-  for (const element of result) {
-    const object: Record<string, string> = {};
-    const output = abi[0].outputs[0];
-    for (const component of output.components) {
-      object[component.name] = element[component.name].toString();
+export function prepForStringify(value: any): any {
+  if (Array.isArray(value)) {
+    let prepared = [];
+    for (const item of value) {
+      prepared.push(prepForStringify(item));
     }
-    objects.push(object);
+    return prepared;
+  } else if (
+    typeof value === "object" &&
+    typeof value.toString === "function"
+  ) {
+    return value.toString();
+  } else {
+    return value;
   }
-  return JSON.stringify(objects);
-}
-
-function stringifySimpleArray(result: any): string {
-  const objects: string[] = [];
-  for (const element of result) {
-    objects.push(element.toString());
-  }
-  return JSON.stringify(objects);
 }
