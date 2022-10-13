6e65f20d8cc867dc19df944649a9ae5907522fa5
dOrgJelli <jelli@dorg.tech>
2022-10-13 19:21:34 +0200

    chore: update reserved-words test-case wrapper

--- packages/test-cases/cases/wrappers/wasm-as/reserved-words/src/index.ts
+++ packages/test-cases/cases/wrappers/wasm-as/reserved-words/src/index.ts
@@ -1,19 +1,19 @@
 import {
-  Args__if,
-  Args__for,
+  Args_if,
+  Args_for,
   _else,
   _while,
   _Box,
   getwhileKey
 } from "./wrap";
 
-export function _if(args: Args__if): _else {
+export function _if(args: Args_if): _else {
   return {
     _else: args._if._else
   };
 }
 
-export function _for(args: Args__for): _Box {
+export function _for(args: Args_for): _Box {
   const value: _while = args._in;
   return {
     box: getwhileKey(value)
