adaa15b212b72062a9ff2a28a93fddc8afc1c81b
DaoAdvocate <12145726+rihp@users.noreply.github.com>
2022-10-13 15:41:27 +0200

    wip: BigNumber tests

--- packages/py/polywrap-client/tests/test_client.py
+++ packages/py/polywrap-client/tests/test_client.py
@@ -86,3 +86,41 @@ async def test_invoke_bignumber_with_2args_and_2props():
 
 
 
+async def test_invoke_bignumber_with_2args_and_2props_floats():
+    client = PolywrapClient()
+    # BigNumber wrapper schema - https://wrappers.io/v/ipfs/Qme2YXThmsqtfpiUPHJUEzZSBiqX3woQxxdXbDJZvXrvAD
+    uri = Uri(f'fs/{Path(__file__).parent.joinpath("cases", "wrapperBigNumber.wasm").absolute()}')
+    print(Uri)
+    args = {
+        "arg1": "123.123",
+        "arg2": "55.5",
+        "obj": {
+            "prop1": "10.001",
+           "prop2": "4"
+        }
+    }
+    options = InvokerOptions(uri=uri, method="method", args=args, encode_result=False)
+    result = await client.invoke(options)
+    print(result)
+    assert result.result == str(123.123*55.5*10.001*4)
+
+
+async def test_invoke_bignumber_with_2args_and_2props_complex_numbers():
+    client = PolywrapClient()
+    # BigNumber wrapper schema - https://wrappers.io/v/ipfs/Qme2YXThmsqtfpiUPHJUEzZSBiqX3woQxxdXbDJZvXrvAD
+    uri = Uri(f'fs/{Path(__file__).parent.joinpath("cases", "wrapperBigNumber.wasm").absolute()}')
+    print(Uri)
+    args = {
+        # Question: Should bignumber work with complex numbers? 
+        "arg1": "3j",
+        "arg2": "5",
+        "obj": {
+            "prop1": "2",
+           "prop2": "4"
+        }
+    }
+    options = InvokerOptions(uri=uri, method="method", args=args, encode_result=False)
+    result = await client.invoke(options)
+    print(result)
+    # Question: Should bignumber work with complex numbers? 
+    assert result.result == str(3j*55*2*4)
