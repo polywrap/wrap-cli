6b06c056bf890e19a60b62c4feb85dfc31530d03
DaoAdvocate <media@polywrap.io>
2022-10-13 15:29:29 +0200

    simple tests for bignumber :)

--- packages/py/polywrap-client/tests/test_client.py
+++ packages/py/polywrap-client/tests/test_client.py
@@ -16,28 +16,39 @@ async def test_invoke():
     assert result.result == args["arg"]
 
 
-async def test_invoke_bignumber():
+async def test_invoke_bignumber_1arg_and_1prop():
     client = PolywrapClient()
-
-    # BigNumber wrapper schema
-    # https://wrappers.io/v/ipfs/Qme2YXThmsqtfpiUPHJUEzZSBiqX3woQxxdXbDJZvXrvAD
+    # BigNumber wrapper schema - https://wrappers.io/v/ipfs/Qme2YXThmsqtfpiUPHJUEzZSBiqX3woQxxdXbDJZvXrvAD
     uri = Uri(f'fs/{Path(__file__).parent.joinpath("cases", "wrapperBigNumber.wasm").absolute()}')
     print(Uri)
-
     args = {
-        "arg1": '98.7654321987654321',
-        "arg2": '333333',
+        "arg1": "123", # The base number
         "obj": {
-            "prop1": "98.7654321987654321"
+            "prop1": "1000", # multiply the base number by this factor
         }
     }
     options = InvokerOptions(uri=uri, method="method", args=args, encode_result=False)
-
-    print(options)
-
     result = await client.invoke(options)
     print(result)
-    assert result.result == args["arg1"]
+    assert result.result == "123000"
 
+
+async def test_invoke_bignumber_with_2args_and_2props():
+    client = PolywrapClient()
+    # BigNumber wrapper schema - https://wrappers.io/v/ipfs/Qme2YXThmsqtfpiUPHJUEzZSBiqX3woQxxdXbDJZvXrvAD
+    uri = Uri(f'fs/{Path(__file__).parent.joinpath("cases", "wrapperBigNumber.wasm").absolute()}')
+    print(Uri)
+    args = {
+        "arg1": "123123",
+        "arg2": "4",
+        "obj": {
+            "prop1": "1000",
+           "prop2": "4"
+        }
+    }
+    options = InvokerOptions(uri=uri, method="method", args=args, encode_result=False)
+    result = await client.invoke(options)
+    print(result)
+    assert result.result == str(123123*4*1000*4)
 
 
