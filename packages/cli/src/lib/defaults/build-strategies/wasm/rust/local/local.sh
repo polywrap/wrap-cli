94d9c43abe520c7af51f27dfac0b902721b903f2
Niraj Kamdar <nirajkamdar27@pm.me>
2022-11-06 18:30:51 +0530

    feat: remove wasm-bindgen from the rust wrapper build step

--- packages/cli/src/lib/defaults/build-strategies/wasm/rust/local/local.sh
+++ packages/cli/src/lib/defaults/build-strategies/wasm/rust/local/local.sh
@@ -3,7 +3,6 @@ rustup target add wasm32-unknown-unknown
 export RUSTFLAGS="-C link-arg=-z -C link-arg=stack-size=65536 -C link-arg=--import-memory"
 cargo install toml-cli
 cargo install wasm-snip
-cargo install wasm-bindgen-cli
 
 toml set "$1"/Cargo.toml lib.crate-type ["cdylib"] > "$1"/Cargo-local.toml && \
   rm -rf "$1"/Cargo.toml && \
@@ -19,9 +18,6 @@ toml set "$1"/Cargo.toml package.name "module" > "$1"/Cargo-local.toml && \
 
 cargo build --manifest-path "$1"/Cargo.toml \
   --target wasm32-unknown-unknown --release
-export WASM_INTERFACE_TYPES=1
-wasm-bindgen "$1"/target/wasm32-unknown-unknown/release/module.wasm --out-dir "$2" --out-name bg_module.wasm
-wasm-snip "$2"/bg_module.wasm -o "$2"/snipped_module.wasm && \
-  rm -rf "$2"/bg_module.wasm
+wasm-snip "$1"/target/wasm32-unknown-unknown/release/module.wasm -o "$2"/snipped_module.wasm
 npx wasm-opt --asyncify -Os "$2"/snipped_module.wasm -o "$2"/wrap.wasm
   rm -rf "$2"/snipped_module.wasm
