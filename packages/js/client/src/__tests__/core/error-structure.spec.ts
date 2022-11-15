3dd3d17d2691533774e8a497f7cda0d1bd35e510
krisbitney <kristoferbitney@hotmail.com>
2022-11-15 19:39:37 +0530

    uncommented some lines in error-structure.spec.ts

--- packages/js/client/src/__tests__/core/error-structure.spec.ts
+++ packages/js/client/src/__tests__/core/error-structure.spec.ts
@@ -24,9 +24,9 @@ describe("error structure", () => {
 
   beforeAll(async () => {
     await buildWrapper(simpleWrapperPath);
-    // await buildWrapper(badUtilWrapperPath);
-    // await buildWrapper(badMathWrapperPath);
-    // await buildWrapper(subinvokeErrorWrapperPath);
+    await buildWrapper(badUtilWrapperPath);
+    await buildWrapper(badMathWrapperPath);
+    await buildWrapper(subinvokeErrorWrapperPath);
 
     client = new PolywrapClient({
       redirects: [
