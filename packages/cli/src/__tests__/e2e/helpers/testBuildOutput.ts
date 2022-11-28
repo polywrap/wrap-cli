af6e7f08cc1bb88384070bc819e079e6536d6140
Pileks <jure@amber-it.co>
2022-11-28 17:35:58 +0100

    chore: refactor testBuildOutput

--- packages/cli/src/__tests__/e2e/helpers/testBuildOutput.ts
+++ packages/cli/src/__tests__/e2e/helpers/testBuildOutput.ts
@@ -2,11 +2,7 @@ import path from "path";
 import fs from "fs";
 
 export const testBuildOutput = (testCaseDir: string, buildDir: string) => {
-  const expectedOutputFile = path.join(
-    testCaseDir,
-    "expected",
-    "output.json"
-  );
+  const expectedOutputFile = path.join(testCaseDir, "expected", "output.json");
   if (fs.existsSync(expectedOutputFile)) {
     const expectedFiles = JSON.parse(
       fs.readFileSync(expectedOutputFile, { encoding: "utf8" })
@@ -14,9 +10,8 @@ export const testBuildOutput = (testCaseDir: string, buildDir: string) => {
 
     for (const file of expectedFiles) {
       if (!fs.existsSync(path.join(buildDir, file))) {
-        expect(path.join(buildDir, file)).toBe("debug")
+        fail(`Did not find expected file: ${path.join(buildDir, file)}`);
       }
-      expect(fs.existsSync(path.join(buildDir, file))).toBeTruthy();
     }
   }
 };
