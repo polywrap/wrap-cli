b8015ac743fd959f00405e17da673d4faa249bf3
Cesar <cesarbrazon10@gmail.com>
2023-01-16 22:15:14 +0100

    chore(polywrap/manifest): improve migrator warn message

--- packages/js/manifests/polywrap/src/formats/polywrap/migrators/0.3.0_to_0.4.0.ts
+++ packages/js/manifests/polywrap/src/formats/polywrap/migrators/0.3.0_to_0.4.0.ts
@@ -5,7 +5,7 @@ import { PolywrapManifest as NewManifest } from "../0.4.0";
 export function migrate(migrate: OldManifest, logger?: ILogger): NewManifest {
   if (migrate.extensions?.deploy) {
     logger?.warn(
-      `The Polywrap Deploy Manifest does not needs to be defined in the Polywrap Manifest, feel free to remove it.`
+      `The Polywrap Deploy Manifest does not needs to be defined in the Polywrap Manifest, please remove it`
     );
   }
   return {
