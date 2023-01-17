34f82b7442d979499f7c9226cb348fe8b3efe967
Cesar <cesarbrazon10@gmail.com>
2023-01-17 15:14:23 +0100

    js/manifest: remove deploy attribute in polywrap migration 0.4

--- packages/js/manifests/polywrap/src/formats/polywrap/migrators/0.3.0_to_0.4.0.ts
+++ packages/js/manifests/polywrap/src/formats/polywrap/migrators/0.3.0_to_0.4.0.ts
@@ -5,8 +5,9 @@ import { PolywrapManifest as NewManifest } from "../0.4.0";
 export function migrate(migrate: OldManifest, logger?: ILogger): NewManifest {
   if (migrate.extensions?.deploy) {
     logger?.warn(
-      `The Polywrap Deploy Manifest does not needs to be defined in the Polywrap Manifest, please remove it`
+      `The Polywrap Deploy Manifest ('extensions.deploy') is no longer used within the Polywrap Manifest and has been removed`
     );
+    delete migrate.extensions.deploy;
   }
   return {
     ...migrate,
