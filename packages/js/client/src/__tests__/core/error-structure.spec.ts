1ce344c8b15ac2fa894350cbf850cd3e20cac38f
Cesar <cesarbrazon10@gmail.com>
2023-02-04 11:53:13 +0100

    client/tests: invoke wrapper of compatible version almost works

--- packages/js/client/src/__tests__/core/error-structure.spec.ts
+++ packages/js/client/src/__tests__/core/error-structure.spec.ts
@@ -1,8 +1,15 @@
+import fs from "fs";
+import path from "path";
+import { WasmWrapper } from "@polywrap/wasm-js";
+import { Uri, Wrapper } from "@polywrap/core-js";
+
 import { GetPathToTestWrappers } from "@polywrap/test-cases";
-import { Uri, PolywrapClient } from "../..";
+import { PolywrapClient } from "../..";
 import { WrapError, WrapErrorCode } from "@polywrap/core-js";
 import { mockPluginRegistration } from "../helpers";
 import { defaultPackages } from "@polywrap/client-config-builder-js";
+import { deserializeWrapManifest, serializeWrapManifest } from "@polywrap/wrap-manifest-types-js";
+import { msgpackDecode, msgpackEncode } from "@polywrap/msgpack-js";
 
 jest.setTimeout(660000);
 
@@ -14,7 +21,6 @@ const asInvokeWrapperUri = new Uri(`fs/${asInvokeWrapperPath}`);
 
 const asConsumerWrapperPath = `${GetPathToTestWrappers()}/subinvoke/02-consumer/implementations/as`;
 const asConsumerWrapperUri = new Uri(`fs/${asConsumerWrapperPath}`);
-
 
 const rsSubinvokeWrapperPath = `${GetPathToTestWrappers()}/subinvoke/00-subinvoke/implementations/rs`;
 const rsSubinvokeWrapperUri = new Uri(`fs/${rsSubinvokeWrapperPath}`);
@@ -195,25 +201,40 @@ describe("Error structure", () => {
       expect(prevOfPrev.source).toEqual({ file: "src/index.ts", row: 8, col: 5 });
     });
 
-    // test("Invoke a wrapper of incompatible version", async () => {
-    //   const result = await client.invoke<string>({
-    //     uri: incompatibleWrapperUri.uri,
-    //     method: "simpleMethod",
-    //     args: {
-    //       arg: "test",
-    //     },
-    //   });
-    //
-    //   expect(result.ok).toBeFalsy();
-    //   if (result.ok) throw Error("should never happen");
-    //
-    //   expect(result.error?.name).toEqual("WrapError");
-    //   expect(result.error?.code).toEqual(WrapErrorCode.URI_RESOLVER_ERROR);
-    //   expect(result.error?.uri.endsWith("packages/test-cases/cases/wrappers/wasm-as/simple-deprecated")).toBeTruthy();
-    //   expect(result.error?.resolutionStack).toBeDefined();
-    //   expect(`${result.error?.cause}`).toContain(`Unrecognized WrapManifest schema version "0.0.1"`);
-    // });
-  });
+  //   test("Invoke a wrapper of incompatible version", async () => {
+  //     const wrapperPath = `${GetPathToTestWrappers()}/subinvoke/00-subinvoke/implementations/as`;
+  //     const manifestBuffer = fs.readFileSync(path.join(wrapperPath, "wrap.info"))
+  //     const wasmModuleBuffer = fs.readFileSync(path.join(wrapperPath, "wrap.wasm"))
+
+  //     const manifest: Record<string, unknown> = await msgpackDecode(manifestBuffer) as Record<string, unknown>;
+  //     manifest.version = "0.0.0.5" as any;
+  //     const modifiedManifestBuffer = msgpackEncode(manifest, false);
+  
+  //     let wrapper: Wrapper = await WasmWrapper.from(modifiedManifestBuffer, wasmModuleBuffer);
+  
+  //     const client = new PolywrapClient({
+  //       wrappers: [
+  //         {
+  //           uri: "wrap://ens/incompatible-wrapper.eth",
+  //           wrapper
+  //         }
+  //       ]
+  //     });
+  //     const result = await client.invoke<string>({
+  //       uri: "wrap://ens/incompatible-wrapper.eth",
+  //       method: "simpleMethod"
+  //     });
+    
+  //     expect(result.ok).toBeFalsy();
+  //     if (result.ok) throw Error("should never happen");
+    
+  //     expect(result.error?.name).toEqual("WrapError");
+  //     expect(result.error?.code).toEqual(WrapErrorCode.URI_RESOLVER_ERROR);
+  //     expect(result.error?.uri.endsWith("packages/test-cases/cases/wrappers/wasm-as/simple-deprecated")).toBeTruthy();
+  //     expect(result.error?.resolutionStack).toBeDefined();
+  //     expect(`${result.error?.cause}`).toContain(`Unrecognized WrapManifest schema version "0.0.1"`);
+  //   });
+  // });
 
   describe("Plugin wrapper", () => {
     const client = new PolywrapClient({
