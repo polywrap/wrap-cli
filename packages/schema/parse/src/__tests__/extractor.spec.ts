8d926d25b01e45c5a3bd1b80509c44732d1e28be
Nestor Amesty <nestor09amesty@gmail.com>
2023-02-09 02:37:40 +0100

    (chore): added multiple test cases for object defs

--- packages/schema/parse/src/__tests__/extractor.spec.ts
+++ packages/schema/parse/src/__tests__/extractor.spec.ts
@@ -4,7 +4,7 @@ import { EnumVisitorBuilder, FunctionsVisitorBuilder, ObjectVisitorBuilder } fro
 
 describe("Extractors", () => {
   describe("Enums", () => {
-    it("Extracts enum defs", () => {
+    it("Extracts simple enum defs", () => {
       const schema = `
         enum Color {
           RED
@@ -35,7 +35,7 @@ describe("Extractors", () => {
           kind: "Enum",
           constants: ["SMALL", "MEDIUM"]
         }
-    ]
+      ]
 
       visit(astNode, visitInParallel([enumExtractor]))
 
@@ -43,15 +43,134 @@ describe("Extractors", () => {
     })
   })
   describe("Objects", () => {
-    it("Extracts object defs", () => {
+    it("Extracts simple object defs", () => {
+      const schema = `
+        type Nested {
+          prop: UInt8
+        }
+      `
+
+      const astNode = parse(schema);
+      const abi: Abi = {
+        version: "0.2"
+      }
+
+      const objectExtractor = new ObjectVisitorBuilder(new Map()).build(abi);
+
+      const expected: ObjectDef[] = [
+        {
+          kind: "Object",
+          name: "Nested",
+          props: [
+            {
+              kind: "Property",
+              name: "prop",
+              type: {
+                kind: "Scalar",
+                scalar: "UInt8"
+              },
+              required: false
+            },
+          ]
+        }
+      ]
+
+      visit(astNode, visitInParallel([objectExtractor]))
+
+      expect(abi.objects).toMatchObject(expected)
+    })
+
+    it("Extracts objects with ref types", () => {
       const schema = `
         type Custom {
           prop1: String!
           nested: Nested
+          bar: Bar
         }
         
         type Nested {
           prop: UInt8
+        }
+      `
+
+      const astNode = parse(schema);
+      const abi: Abi = {
+        version: "0.2"
+      }
+      const uniqueDefs = new Map([
+        ["Custom", "Object" as const],
+        ["Nested", "Object" as const],
+        ["Bar", "Enum" as const]
+      ])
+      const objectExtractor = new ObjectVisitorBuilder(uniqueDefs).build(abi);
+
+      const expected: ObjectDef[] = [
+        {
+          kind: "Object",
+          name: "Custom",
+          props: [
+            {
+              kind: "Property",
+              name: "prop1",
+              type: {
+                kind: "Scalar",
+                scalar: "String"
+              },
+              required: true
+            },
+            {
+              kind: "Property",
+              name: "nested",
+              type: {
+                kind: "Ref",
+                ref_name: "Nested",
+                ref_kind: "Object"
+              },
+              required: false
+            },
+            {
+              kind: "Property",
+              name: "bar",
+              type: {
+                kind: "Ref",
+                ref_name: "Bar",
+                ref_kind: "Enum"
+              },
+              required: false
+            }
+          ]
+        },
+        {
+          kind: "Object",
+          name: "Nested",
+          props: [
+            {
+              kind: "Property",
+              name: "prop",
+              type: {
+                kind: "Scalar",
+                scalar: "UInt8"
+              },
+              required: false
+            },
+          ]
+        }
+      ]
+
+      visit(astNode, visitInParallel([objectExtractor]))
+
+      expect(abi.objects).toMatchObject(expected)
+    })
+
+    it("Extracts objects with array types", () => {
+      const schema = `
+        type Custom {
+          prop1: String!
+          nested: [Nested!]!
+        }
+        
+        type Nested {
+          prop: [UInt8]
         }
       `
 
@@ -83,11 +202,17 @@ describe("Extractors", () => {
               kind: "Property",
               name: "nested",
               type: {
-                kind: "Ref",
-                ref_name: "Nested",
-                ref_kind: "Object"
+                kind: "Array",
+                item: {
+                  required: true,
+                  type: {
+                    kind: "Ref",
+                    ref_name: "Nested",
+                    ref_kind: "Object"
+                  }
+                }
               },
-              required: false
+              required: true
             }
           ]
         },
@@ -99,11 +224,104 @@ describe("Extractors", () => {
               kind: "Property",
               name: "prop",
               type: {
-                kind: "Scalar",
-                scalar: "UInt8"
+                kind: "Array",
+                item: {
+                  required: false,
+                  type: {
+                    kind: "Scalar",
+                    scalar: "UInt8"
+                  }
+
+                }
               },
               required: false
-            },
+            }
+          ]
+        }
+      ]
+
+      visit(astNode, visitInParallel([objectExtractor]))
+
+      expect(abi.objects).toMatchObject(expected)
+    })
+    
+    it("Extracts objects with map types", () => {
+      const schema = `
+        type Custom {
+          nested: Map! @annotate(type: "Map<String, [Nested!]!>")
+        }
+        
+        type Nested {
+          prop: Map @annotate(type: "Map<UInt8, UInt8>")
+        }
+      `
+
+      const astNode = parse(schema);
+      const abi: Abi = {
+        version: "0.2"
+      }
+      const uniqueDefs = new Map([
+        ["Custom", "Object" as const],
+        ["Nested", "Object" as const]
+      ])
+      const objectExtractor = new ObjectVisitorBuilder(uniqueDefs).build(abi);
+
+      const expected: ObjectDef[] = [
+        {
+          kind: "Object",
+          name: "Custom",
+          props: [
+            {
+              kind: "Property",
+              name: "nested",
+              type: {
+                kind: "Map",
+                key: {
+                  kind: "Scalar",
+                  scalar: "String"
+                },
+                value: {
+                  required: true,
+                  type: {
+                    kind: "Array",
+                    item: {
+                      required: true,
+                      type: {
+                        kind: "Ref",
+                        ref_name: "Nested",
+                        ref_kind: "Object"
+                      }
+                    }
+                  }
+                }
+              },
+              required: true
+            }
+          ]
+        },
+        {
+          kind: "Object",
+          name: "Nested",
+          props: [
+            {
+              kind: "Property",
+              name: "prop",
+              type: {
+                kind: "Map",
+                key: {
+                  kind: "Scalar",
+                  scalar: "UInt8"
+                },
+                value: {
+                  required: false,
+                  type: {
+                    kind: "Scalar",
+                    scalar: "UInt8"
+                  }
+                }
+              },
+              required: false
+            }
           ]
         }
       ]
@@ -114,7 +332,7 @@ describe("Extractors", () => {
     })
   })
   describe("Functions", () => {
-    it("Extract functions defs", () => {
+    it("Extract simple functions defs", () => {
       const schema = `
         type Module {
           fooFunc(
