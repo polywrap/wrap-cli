771b549501141d100aa2943b475475b176915a3d
Pileks <jure@amber-it.co>
2023-02-10 01:15:00 +0100

    chore: fix ipfs-resolver tests

--- packages/js/plugins/uri-resolvers/ipfs-resolver/src/__tests__/helpers/getClient.ts
+++ packages/js/plugins/uri-resolvers/ipfs-resolver/src/__tests__/helpers/getClient.ts
@@ -10,43 +10,40 @@ import { providers } from "@polywrap/test-env-js";
 import { ipfsResolverPlugin } from "../..";
 
 export const getClient = (env: Record<string, unknown>) => {
-  return new PolywrapClient<string>(
-    {
-      envs: [
-        {
-          uri: "wrap://ens/ipfs.polywrap.eth",
-          env: {
-            provider: providers.ipfs,
+  return new PolywrapClient({
+    envs: [
+      {
+        uri: Uri.from("wrap://ens/ipfs.polywrap.eth"),
+        env: {
+          provider: providers.ipfs,
+        },
+      },
+      {
+        uri: Uri.from("wrap://ens/ipfs-resolver.polywrap.eth"),
+        env: env,
+      },
+    ],
+    interfaces: [
+      {
+        interface: ExtendableUriResolver.extInterfaceUri,
+        implementations: [Uri.from("wrap://ens/ipfs-resolver.polywrap.eth")],
+      },
+    ],
+    resolver: RecursiveResolver.from(
+      PackageToWrapperCacheResolver.from(
+        [
+          {
+            uri: Uri.from("wrap://ens/ipfs.polywrap.eth"),
+            package: ipfsPlugin({}),
           },
-        },
-        {
-          uri: "wrap://ens/ipfs-resolver.polywrap.eth",
-          env: env,
-        },
-      ],
-      interfaces: [
-        {
-          interface: ExtendableUriResolver.extInterfaceUri.uri,
-          implementations: ["wrap://ens/ipfs-resolver.polywrap.eth"],
-        },
-      ],
-      resolver: RecursiveResolver.from(
-        PackageToWrapperCacheResolver.from(
-          [
-            {
-              uri: Uri.from("wrap://ens/ipfs.polywrap.eth"),
-              package: ipfsPlugin({}),
-            },
-            {
-              uri: Uri.from("wrap://ens/ipfs-resolver.polywrap.eth"),
-              package: ipfsResolverPlugin({}),
-            },
-            new ExtendableUriResolver(),
-          ],
-          new WrapperCache()
-        )
-      ),
-    },
-    { noDefaults: true }
-  );
+          {
+            uri: Uri.from("wrap://ens/ipfs-resolver.polywrap.eth"),
+            package: ipfsResolverPlugin({}),
+          },
+          new ExtendableUriResolver(),
+        ],
+        new WrapperCache()
+      )
+    ),
+  });
 };
