aae23213572260781efcf45c28de1453e7d3b3d5
Pileks <jure@amber-it.co>
2023-02-10 01:11:41 +0100

    chore: fix fs-resolver tests

--- packages/js/plugins/uri-resolvers/file-system-resolver/src/__tests__/helpers/getClient.ts
+++ packages/js/plugins/uri-resolvers/file-system-resolver/src/__tests__/helpers/getClient.ts
@@ -9,31 +9,28 @@ import { fileSystemPlugin } from "@polywrap/fs-plugin-js";
 import { fileSystemResolverPlugin } from "../..";
 
 export const getClient = () => {
-  return new PolywrapClient<string>(
-    {
-      interfaces: [
-        {
-          interface: ExtendableUriResolver.extInterfaceUri.uri,
-          implementations: ["wrap://ens/fs-resolver.polywrap.eth"],
-        },
-      ],
-      resolver: RecursiveResolver.from(
-        PackageToWrapperCacheResolver.from(
-          [
-            {
-              uri: Uri.from("wrap://ens/fs-resolver.polywrap.eth"),
-              package: fileSystemResolverPlugin({}),
-            },
-            {
-              uri: Uri.from(defaultInterfaces.fileSystem),
-              package: fileSystemPlugin({}),
-            },
-            new ExtendableUriResolver(),
-          ],
-          new WrapperCache()
-        )
-      ),
-    },
-    { noDefaults: true }
-  );
+  return new PolywrapClient({
+    interfaces: [
+      {
+        interface: ExtendableUriResolver.extInterfaceUri,
+        implementations: [Uri.from("wrap://ens/fs-resolver.polywrap.eth")],
+      },
+    ],
+    resolver: RecursiveResolver.from(
+      PackageToWrapperCacheResolver.from(
+        [
+          {
+            uri: Uri.from("wrap://ens/fs-resolver.polywrap.eth"),
+            package: fileSystemResolverPlugin({}),
+          },
+          {
+            uri: Uri.from(defaultInterfaces.fileSystem),
+            package: fileSystemPlugin({}),
+          },
+          new ExtendableUriResolver(),
+        ],
+        new WrapperCache()
+      )
+    ),
+  });
 };
