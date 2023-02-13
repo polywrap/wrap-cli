5151f53200c4cbb76cf172887ceaa529b1260c0e
Nestor Amesty <nestor09amesty@gmail.com>
2023-02-13 17:42:22 +0100

    (chore): made visitor support enter and leave

--- packages/schema/linking/src/visitor.ts
+++ packages/schema/linking/src/visitor.ts
@@ -17,12 +17,17 @@ interface IAbiVisitor {
   AnyType?: (node: AnyType) => void;
 }
 
+interface IAbiVisitorEnterAndLeave {
+  enter?: IAbiVisitor;
+  leave?: IAbiVisitor;
+}
+
 export class AbiVisitor {
-  constructor(private readonly visitor: IAbiVisitor) {}
+  constructor(private readonly visitor: IAbiVisitorEnterAndLeave) {}
 
   private _Abi(node: Abi) {
-    if (this.visitor.Abi) {
-      this.visitor.Abi(node);
+    if (this.visitor.enter?.Abi) {
+      this.visitor.enter.Abi(node);
     }
 
     if (node.imports) {
@@ -48,11 +53,15 @@ export class AbiVisitor {
         this._EnumDef(enumNode);
       }
     }
+
+    if (this.visitor.leave?.Abi) {
+      this.visitor.leave.Abi(node);
+    }
   }
 
   private _ImportedAbi(node: ImportedAbi) {
-    if (this.visitor.ImportedAbi) {
-      this.visitor.ImportedAbi(node);
+    if (this.visitor.enter?.ImportedAbi) {
+      this.visitor.enter.ImportedAbi(node);
     }
 
     if (node.functions) {
@@ -72,11 +81,15 @@ export class AbiVisitor {
         this._EnumDef(enumNode);
       }
     }
+
+    if (this.visitor.leave?.ImportedAbi) {
+      this.visitor.leave.ImportedAbi(node);
+    }
   }
 
   private _FunctionDef(node: FunctionDef) {
-    if (this.visitor.FunctionDef) {
-      this.visitor.FunctionDef(node);
+    if (this.visitor.enter?.FunctionDef) {
+      this.visitor.enter.FunctionDef(node);
     }
 
     if (node.args) {
@@ -86,49 +99,71 @@ export class AbiVisitor {
     }
 
     this._ResultDef(node.result);
+
+    if (this.visitor.leave?.FunctionDef) {
+      this.visitor.leave.FunctionDef(node);
+    }
   }
 
   private _ArgumentDef(node: ArgumentDef) {
-    if (this.visitor.ArgumentDef) {
-      this.visitor.ArgumentDef(node);
+    if (this.visitor.enter?.ArgumentDef) {
+      this.visitor.enter.ArgumentDef(node);
     }
     this._AnyType(node.type);
+
+    if (this.visitor.leave?.ArgumentDef) {
+      this.visitor.leave.ArgumentDef(node);
+    }
   }
 
   private _ResultDef(node: ResultDef) {
-    if (this.visitor.ResultDef) {
-      this.visitor.ResultDef(node);
+    if (this.visitor.enter?.ResultDef) {
+      this.visitor.enter.ResultDef(node);
     }
     this._AnyType(node.type);
+
+    if (this.visitor.leave?.ResultDef) {
+      this.visitor.leave.ResultDef(node);
+    }
   }
 
   private _ObjectDef(node: ObjectDef) {
-    if (this.visitor.ObjectDef) {
-      this.visitor.ObjectDef(node);
+    if (this.visitor.enter?.ObjectDef) {
+      this.visitor.enter.ObjectDef(node);
     }
     if (node.props) {
       for (const propertyNode of node.props) {
         this._PropertyDef(propertyNode);
       }
     }
+    if (this.visitor.leave?.ObjectDef) {
+      this.visitor.leave.ObjectDef(node);
+    }
   }
 
   private _PropertyDef(node: PropertyDef) {
-    if (this.visitor.PropertyDef) {
-      this.visitor.PropertyDef(node);
+    if (this.visitor.enter?.PropertyDef) {
+      this.visitor.enter.PropertyDef(node);
     }
     this._AnyType(node.type);
+    if (this.visitor.leave?.PropertyDef) {
+      this.visitor.leave?.PropertyDef(node);
+    }
   }
 
   private _EnumDef(node: EnumDef) {
-    if (this.visitor.EnumDef) {
-      this.visitor.EnumDef(node);
+    if (this.visitor.enter?.EnumDef) {
+      this.visitor.enter.EnumDef(node);
+    }
+
+    if (this.visitor.leave?.EnumDef) {
+      this.visitor.leave.EnumDef(node);
     }
   }
 
   private _AnyType(node: AnyType) {
-    if (this.visitor.AnyType) {
-      this.visitor.AnyType(node);
+    if (this.visitor.enter?.AnyType) {
+      this.visitor.enter.AnyType(node);
     }
 
     switch (node.kind) {
@@ -148,38 +183,62 @@ export class AbiVisitor {
         this._ImportRefType(node);
         break;
     }
+
+    if (this.visitor.leave?.AnyType) {
+      this.visitor.leave.AnyType(node);
+    }
   }
 
   private _ScalarType(node: ScalarType) {
-    if (this.visitor.ScalarType) {
-      this.visitor.ScalarType(node);
+    if (this.visitor.enter?.ScalarType) {
+      this.visitor.enter.ScalarType(node);
+    }
+
+    if (this.visitor.leave?.ScalarType) {
+      this.visitor.leave.ScalarType(node);
     }
   }
 
   private _RefType(node: RefType) {
-    if (this.visitor.RefType) {
-      this.visitor.RefType(node);
+    if (this.visitor.enter?.RefType) {
+      this.visitor.enter.RefType(node);
+    }
+
+    if (this.visitor.leave?.RefType) {
+      this.visitor.leave.RefType(node);
     }
   }
 
   private _ImportRefType(node: ImportRefType) {
-    if (this.visitor.ImportRefType) {
-      this.visitor.ImportRefType(node);
+    if (this.visitor.enter?.ImportRefType) {
+      this.visitor.enter.ImportRefType(node);
+    }
+
+    if (this.visitor.leave?.ImportRefType) {
+      this.visitor.leave.ImportRefType(node);
     }
   }
 
   private _ArrayType(node: ArrayType) {
-    if (this.visitor.ArrayType) {
-      this.visitor.ArrayType(node);
+    if (this.visitor.enter?.ArrayType) {
+      this.visitor.enter.ArrayType(node);
     }
     this._AnyType(node.item.type);
+
+    if (this.visitor.leave?.ArrayType) {
+      this.visitor.leave.ArrayType(node);
+    }
   }
 
   private _MapType(node: MapType) {
-    if (this.visitor.MapType) {
-      this.visitor.MapType(node);
+    if (this.visitor.enter?.MapType) {
+      this.visitor.enter.MapType(node);
     }
     this._AnyType(node.value.type);
+
+    if (this.visitor.leave?.MapType) {
+      this.visitor.leave.MapType(node);
+    }
   }
 
   visit(node: Abi) {
