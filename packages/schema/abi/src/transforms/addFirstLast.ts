714f73d7b56ebfc8325963392563a2b7455d0f92
Nestor Amesty <nestor09amesty@gmail.com>
2023-03-02 00:20:48 +0100

    (feat): add first last transform

---
+++ packages/schema/abi/src/transforms/addFirstLast.ts
@@ -0,0 +1,77 @@
+import { Abi, EnumDef, FunctionDef, ImportedAbi, ObjectDef } from "@polywrap/abi-types";
+import { IAbiVisitor } from "../AbiVisitor";
+
+const addFirstLastProps = <T>(items: T[]): T[] => {
+  return items.map((item, index) => {
+    const isFirst = index === 0;
+    const isLast = index === items.length - 1;
+
+    return {
+      ...item,
+      isFirst,
+      isLast,
+    };
+  });
+}
+
+export const addFirstLast: IAbiVisitor = {
+  Abi: (abi: Abi): Abi => {
+    if (abi.functions) {
+      abi.functions = addFirstLastProps(abi.functions);
+    }
+
+    if (abi.objects) {
+      abi.objects = addFirstLastProps(abi.objects);
+    }
+
+    if (abi.enums) {
+      abi.enums = addFirstLastProps(abi.enums);
+    }
+
+    if (abi.imports) {
+      abi.imports = addFirstLastProps(abi.imports);
+    }
+
+    return abi;
+  },
+  Import: (abi: ImportedAbi): ImportedAbi => {
+    if (abi.functions) {
+      abi.functions = addFirstLastProps(abi.functions);
+    }
+
+    if (abi.objects) {
+      abi.objects = addFirstLastProps(abi.objects);
+    }
+
+    if (abi.enums) {
+      abi.enums = addFirstLastProps(abi.enums);
+    }
+
+    if (abi.imports) {
+      abi.imports = addFirstLastProps(abi.imports);
+    }
+
+    return abi;
+  },
+  ObjectDef: (objectDef: ObjectDef): ObjectDef => {
+    if (objectDef.props) {
+      objectDef.props = addFirstLastProps(objectDef.props);
+    }
+
+    return objectDef;
+  },
+  EnumDef: (enumDef: EnumDef): EnumDef => {
+    if (enumDef.constants) {
+      enumDef.constants = addFirstLastProps(enumDef.constants);
+    }
+
+    return enumDef;
+  },
+  FunctionDef: (functionDef: FunctionDef): FunctionDef => {
+    if (functionDef.args) {
+      functionDef.args = addFirstLastProps(functionDef.args);
+    }
+
+    return functionDef;
+  }
+}
