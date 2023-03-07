ad08c4fa0700265fb6d96ebe3da55ec6ad08be40
dOrgJelli <jelli@dorg.tech>
2023-03-06 21:49:11 -0500

    chore: use wrap-test-harness branch

--- packages/test-cases/cases/wrappers/wasm-as/simple-env-types/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { Env, Args_getEnv, ModuleBase } from "./wrap";
-
-export class Module extends ModuleBase {
-  getEnv(_: Args_getEnv, env: Env): Env | null {
-    return env;
-  }
-}
--- packages/test-cases/cases/wrappers/wasm-as/simple/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { ModuleBase, Args_simpleMethod } from "./wrap";
-
-export class Module extends ModuleBase {
-  simpleMethod(args: Args_simpleMethod): string {
-    return args.arg;
-  }
-}
