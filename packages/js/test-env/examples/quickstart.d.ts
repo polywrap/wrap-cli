b5f48f904e585f882e3151e3d1c302d6bea6c438
Jure Bogunovic <bogunovij@gmail.com>
2023-03-07 18:10:13 +0100

    removed merge artifacts

--- packages/js/test-env/examples/quickstart.d.ts
+++
@@ -1,8 +0,0 @@
-export declare function init(): Promise<void>;
-export declare function stop(): Promise<void>;
-export declare function build(): Promise<string>;
-export declare function cli(): Promise<{
-    exitCode: number;
-    stdout: string;
-    stderr: string;
-}>;
