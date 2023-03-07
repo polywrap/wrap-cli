4dfd847b0316bbfdf7bb7f82ddf1ad081c4c96f4
Pileks <jure@amber-it.co>
2023-03-07 22:38:19 +0100

    ClientConfigBuilder - sanitize URIs

--- packages/js/client-config-builder/src/BaseClientConfigBuilder.ts
+++ packages/js/client-config-builder/src/BaseClientConfigBuilder.ts
@@ -1,6 +1,11 @@
 import { BuilderConfig, IClientConfigBuilder } from "./types";
 
-import { CoreClientConfig, Wrapper, IWrapPackage } from "@polywrap/core-js";
+import {
+  CoreClientConfig,
+  Wrapper,
+  IWrapPackage,
+  Uri,
+} from "@polywrap/core-js";
 import { UriResolverLike } from "@polywrap/uri-resolvers-js";
 
 export abstract class BaseClientConfigBuilder implements IClientConfigBuilder {
@@ -22,22 +27,19 @@ export abstract class BaseClientConfigBuilder implements IClientConfigBuilder {
 
   add(config: Partial<BuilderConfig>): IClientConfigBuilder {
     if (config.envs) {
-      this._config.envs = { ...this._config.envs, ...config.envs };
+      this.addEnvs(config.envs);
     }
 
     if (config.redirects) {
-      this._config.redirects = {
-        ...this._config.redirects,
-        ...config.redirects,
-      };
+      this.addRedirects(config.redirects);
     }
 
     if (config.wrappers) {
-      this._config.wrappers = { ...this._config.wrappers, ...config.wrappers };
+      this.addWrappers(config.wrappers);
     }
 
     if (config.packages) {
-      this._config.packages = { ...this._config.packages, ...config.packages };
+      this.addPackages(config.packages);
     }
 
     if (config.interfaces) {
@@ -56,63 +58,70 @@ export abstract class BaseClientConfigBuilder implements IClientConfigBuilder {
   }
 
   addWrapper(uri: string, wrapper: Wrapper): IClientConfigBuilder {
-    this._config.wrappers[uri] = wrapper;
+    this._config.wrappers[this.sanitizeUri(uri)] = wrapper;
 
     return this;
   }
 
   addWrappers(uriWrappers: Record<string, Wrapper>): IClientConfigBuilder {
-    this._config.wrappers = { ...this._config.wrappers, ...uriWrappers };
+    for (const uri in uriWrappers) {
+      this.addWrapper(this.sanitizeUri(uri), uriWrappers[uri]);
+    }
 
     return this;
   }
 
   removeWrapper(uri: string): IClientConfigBuilder {
-    delete this._config.wrappers[uri];
+    delete this._config.wrappers[this.sanitizeUri(uri)];
 
     return this;
   }
 
   addPackage(uri: string, wrapPackage: IWrapPackage): IClientConfigBuilder {
-    this._config.packages[uri] = wrapPackage;
+    this._config.packages[this.sanitizeUri(uri)] = wrapPackage;
 
     return this;
   }
 
   addPackages(uriPackages: Record<string, IWrapPackage>): IClientConfigBuilder {
-    this._config.packages = { ...this._config.packages, ...uriPackages };
+    for (const uri in uriPackages) {
+      this.addPackage(this.sanitizeUri(uri), uriPackages[uri]);
+    }
 
     return this;
   }
 
   removePackage(uri: string): IClientConfigBuilder {
-    delete this._config.packages[uri];
+    delete this._config.packages[this.sanitizeUri(uri)];
 
     return this;
   }
 
   addEnv(uri: string, env: Record<string, unknown>): IClientConfigBuilder {
-    this._config.envs[uri] = { ...this._config.envs[uri], ...env };
+    this._config.envs[this.sanitizeUri(uri)] = {
+      ...this._config.envs[this.sanitizeUri(uri)],
+      ...env,
+    };
 
     return this;
   }
 
   addEnvs(envs: Record<string, Record<string, unknown>>): IClientConfigBuilder {
     for (const [uri, env] of Object.entries(envs)) {
-      this.addEnv(uri, env);
+      this.addEnv(this.sanitizeUri(uri), env);
     }
 
     return this;
   }
 
   removeEnv(uri: string): IClientConfigBuilder {
-    delete this._config.envs[uri];
+    delete this._config.envs[this.sanitizeUri(uri)];
 
     return this;
   }
 
   setEnv(uri: string, env: Record<string, unknown>): IClientConfigBuilder {
-    this._config.envs[uri] = env;
+    this._config.envs[this.sanitizeUri(uri)] = env;
 
     return this;
   }
@@ -121,12 +130,16 @@ export abstract class BaseClientConfigBuilder implements IClientConfigBuilder {
     interfaceUri: string,
     implementationUri: string
   ): IClientConfigBuilder {
-    const existingInterface = this._config.interfaces[interfaceUri];
+    const existingInterface = this._config.interfaces[
+      this.sanitizeUri(interfaceUri)
+    ];
 
     if (existingInterface) {
-      existingInterface.add(implementationUri);
+      existingInterface.add(this.sanitizeUri(implementationUri));
     } else {
-      this._config.interfaces[interfaceUri] = new Set([implementationUri]);
+      this._config.interfaces[this.sanitizeUri(interfaceUri)] = new Set([
+        this.sanitizeUri(implementationUri),
+      ]);
     }
 
     return this;
@@ -136,14 +149,21 @@ export abstract class BaseClientConfigBuilder implements IClientConfigBuilder {
     interfaceUri: string,
     implementationUris: Array<string> | Set<string>
   ): IClientConfigBuilder {
-    const existingInterface = this._config.interfaces[interfaceUri];
+    const existingInterface = this._config.interfaces[
+      this.sanitizeUri(interfaceUri)
+    ];
 
     if (existingInterface) {
       for (const implementationUri of implementationUris) {
-        existingInterface.add(implementationUri);
+        existingInterface.add(this.sanitizeUri(implementationUri));
       }
     } else {
-      this._config.interfaces[interfaceUri] = new Set(implementationUris);
+      const sanitizedImplUris = [...implementationUris].map((x) =>
+        this.sanitizeUri(x)
+      );
+      this._config.interfaces[this.sanitizeUri(interfaceUri)] = new Set(
+        sanitizedImplUris
+      );
     }
 
     return this;
@@ -153,29 +173,37 @@ export abstract class BaseClientConfigBuilder implements IClientConfigBuilder {
     interfaceUri: string,
     implementationUri: string
   ): IClientConfigBuilder {
-    const existingInterface = this._config.interfaces[interfaceUri];
+    const existingInterface = this._config.interfaces[
+      this.sanitizeUri(interfaceUri)
+    ];
 
     if (!existingInterface) return this;
 
-    existingInterface.delete(implementationUri);
+    existingInterface.delete(this.sanitizeUri(implementationUri));
+
+    if (existingInterface.size == 0) {
+      delete this.config.interfaces[this.sanitizeUri(interfaceUri)];
+    }
 
     return this;
   }
 
   addRedirect(from: string, to: string): IClientConfigBuilder {
-    this._config.redirects[from] = to;
+    this._config.redirects[this.sanitizeUri(from)] = this.sanitizeUri(to);
 
     return this;
   }
 
   addRedirects(redirects: Record<string, string>): IClientConfigBuilder {
-    this._config.redirects = { ...this._config.redirects, ...redirects };
+    for (const uri in redirects) {
+      this.addRedirect(this.sanitizeUri(uri), this.sanitizeUri(redirects[uri]));
+    }
 
     return this;
   }
 
   removeRedirect(from: string): IClientConfigBuilder {
-    delete this._config.redirects[from];
+    delete this._config.redirects[this.sanitizeUri(from)];
 
     return this;
   }
@@ -192,5 +220,9 @@ export abstract class BaseClientConfigBuilder implements IClientConfigBuilder {
     }
 
     return this;
+  }
+
+  private sanitizeUri(uri: string): string {
+    return Uri.from(uri).uri;
   }
 }
