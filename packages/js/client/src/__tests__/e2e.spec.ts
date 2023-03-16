b461dc4f0f8c35e4eb0f8d37815c071506e42ec9
dOrgJelli <jelli@dorg.tech>
2023-03-15 21:01:49 -0400

    chore: uncomment test cases

--- packages/js/client/src/__tests__/e2e.spec.ts
+++ packages/js/client/src/__tests__/e2e.spec.ts
@@ -1,5 +1,5 @@
-// import { typeTestCases } from "./core/type-test-cases";
-// import { subinvokeCase } from "./core/wrap-features/subinvoke-case";
+import { typeTestCases } from "./core/type-test-cases";
+import { subinvokeCase } from "./core/wrap-features/subinvoke-case";
 import { envTestCases } from "./core/wrap-features/env-case";
 import { interfaceInvokeCase } from "./core/wrap-features/interface-implementation-case";
 
@@ -7,8 +7,8 @@ export const supportedImplementations = ["as", "rs"];
 describe.each(supportedImplementations)(
   "client <-> wrappers end to end",
   (i) => {
-    // typeTestCases(i);
-    // subinvokeCase(i);
+    typeTestCases(i);
+    subinvokeCase(i);
     envTestCases(i);
     interfaceInvokeCase(i);
   }
