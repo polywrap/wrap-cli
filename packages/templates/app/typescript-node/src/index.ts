db65129dac1383e0b3a537a0348a118c82743201
Pileks <jure@amber-it.co>
2023-03-20 19:52:03 +0100

    Fix ts-node template

--- packages/templates/app/typescript-node/src/index.ts
+++ packages/templates/app/typescript-node/src/index.ts
@@ -24,7 +24,12 @@ async function main() {
     values: ["0xB1B7586656116D546033e3bAFF69BFcD6592225E", "500"]
   }, client);
 
-  console.log(`Ethereum_Module.encodeParams:\n${result.data}`);
+  if (!result.ok) {
+    console.log(`Error:\n${result.error}`);
+    return;
+  }
+
+  console.log(`Ethereum_Module.encodeParams:\n${result.value}`);
 }
 
 main()
