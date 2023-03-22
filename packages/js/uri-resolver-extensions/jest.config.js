ccb20a779efbff3fbaf3a8ad4b7663a642aba5da
Jure Bogunovic <bogunovij@gmail.com>
2023-03-22 13:18:51 +0100

    added support for embedded test wrappers

--- packages/js/uri-resolver-extensions/jest.config.js
+++ packages/js/uri-resolver-extensions/jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   collectCoverage: true,
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
   testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
-  modulePathIgnorePatterns: ['./src/__tests__/apis'],
+  modulePathIgnorePatterns: ["./src/__tests__/wrappers"],
   globals: {
-    'ts-jest': {
-      diagnostics: false
-    }
-  }
+    "ts-jest": {
+      diagnostics: false,
+    },
+  },
 };
