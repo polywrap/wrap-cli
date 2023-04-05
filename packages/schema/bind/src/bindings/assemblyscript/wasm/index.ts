e49c34e915f4a2aabfc44b7d2eab6d1b4e3340a5
namesty <nestor09amesty@gmail.com>
2023-04-05 20:52:48 +0200

    (chore): updated top level AS schema bindings

--- packages/schema/bind/src/bindings/assemblyscript/wasm/index.ts
+++ packages/schema/bind/src/bindings/assemblyscript/wasm/index.ts
@@ -9,15 +9,65 @@ import {
   toPrefixedGraphQLType,
 } from "@polywrap/schema-parse";
 import { AbiVisitor, IAbiVisitorEnterAndLeave } from "@polywrap/schema-abi";
-import { Abi } from "@polywrap/abi-types";
+import { Abi, EnumDef, FunctionDef, ObjectDef } from "@polywrap/abi-types";
 import { OutputEntry, readDirectorySync } from "@polywrap/os-js";
 import path from "path";
-import { WrapAbi } from "@polywrap/wrap-manifest-types-js/src";
 
 const templatesDir = readDirectorySync(path.join(__dirname, "./templates"));
 const subTemplates = loadSubTemplates(templatesDir.entries);
 const templatePath = (subpath: string) =>
   path.join(__dirname, "./templates", subpath);
+
+const getImportedObjects = (abi: Abi) => {
+  const results: ObjectDef[] = []
+  
+  abi.imports?.forEach(importedAbi => {
+    importedAbi.objects?.forEach(obj => {
+      results.push(obj)
+    })
+
+    getImportedObjects({
+      ...importedAbi,
+      version: "0.2"
+    })
+  })
+
+  return results
+}
+
+const getImportedEnums = (abi: Abi) => {
+  const results: EnumDef[] = []
+  
+  abi.imports?.forEach(importedAbi => {
+    importedAbi.enums?.forEach(obj => {
+      results.push(obj)
+    })
+
+    getImportedEnums({
+      ...importedAbi,
+      version: "0.2"
+    })
+  })
+
+  return results
+}
+
+const getImportedFuncs = (abi: Abi) => {
+  const results: FunctionDef[] = []
+  
+  abi.imports?.forEach(importedAbi => {
+    importedAbi.functions?.forEach(obj => {
+      results.push(obj)
+    })
+
+    getImportedFuncs({
+      ...importedAbi,
+      version: "0.2"
+    })
+  })
+
+  return results
+}
 
 export const generateBinding: GenerateBindingFn = (
   options: BindOptions
@@ -32,11 +82,11 @@ export const generateBinding: GenerateBindingFn = (
   const abi = applyTransforms(options.abi);
 
   // Generate object type folders
-  if (abi.objectTypes) {
-    for (const objectType of abi.objectTypes) {
+  if (abi.objects) {
+    for (const objectType of abi.objects) {
       output.entries.push({
         type: "Directory",
-        name: objectType.type,
+        name: objectType.name,
         data: renderTemplates(
           templatePath("object-type"),
           objectType,
@@ -48,50 +98,47 @@ export const generateBinding: GenerateBindingFn = (
 
   // Generate imported folder
   const importEntries: OutputEntry[] = [];
+  const importedObjects = getImportedObjects(abi)
+  const importedEnums = getImportedEnums(abi)
+  const importedFuncs = getImportedFuncs(abi)
 
   // Generate imported module type folders
-  if (abi.importedModuleTypes) {
-    for (const importedModuleType of abi.importedModuleTypes) {
-      importEntries.push({
-        type: "Directory",
-        name: importedModuleType.type,
-        data: renderTemplates(
-          templatePath("imported/module-type"),
-          importedModuleType,
-          subTemplates
-        ),
-      });
-    }
+  for (const importedModuleType of importedFuncs) {
+    importEntries.push({
+      type: "Directory",
+      name: importedModuleType.name,
+      data: renderTemplates(
+        templatePath("imported/module-type"),
+        importedModuleType,
+        subTemplates
+      ),
+    });
   }
 
   // Generate imported enum type folders
-  if (abi.importedEnumTypes) {
-    for (const importedEnumType of abi.importedEnumTypes) {
-      importEntries.push({
-        type: "Directory",
-        name: importedEnumType.type,
-        data: renderTemplates(
-          templatePath("imported/enum-type"),
-          importedEnumType,
-          subTemplates
-        ),
-      });
-    }
+  for (const importedEnumType of importedEnums) {
+    importEntries.push({
+      type: "Directory",
+      name: importedEnumType.name,
+      data: renderTemplates(
+        templatePath("imported/enum-type"),
+        importedEnumType,
+        subTemplates
+      ),
+    });
   }
 
   // Generate imported object type folders
-  if (abi.importedObjectTypes) {
-    for (const importedObectType of abi.importedObjectTypes) {
-      importEntries.push({
-        type: "Directory",
-        name: importedObectType.type,
-        data: renderTemplates(
-          templatePath("imported/object-type"),
-          importedObectType,
-          subTemplates
-        ),
-      });
-    }
+  for (const importedObectType of importedObjects) {
+    importEntries.push({
+      type: "Directory",
+      name: importedObectType.name,
+      data: renderTemplates(
+        templatePath("imported/object-type"),
+        importedObectType,
+        subTemplates
+      ),
+    });
   }
 
   if (importEntries.length) {
@@ -105,40 +152,27 @@ export const generateBinding: GenerateBindingFn = (
     });
   }
 
-  // Generate interface type folders
-  if (abi.interfaceTypes) {
-    for (const interfaceType of abi.interfaceTypes) {
+  // Generate module type folders
+  if (abi.functions) {
+    for (const functionType of abi.functions) {
       output.entries.push({
         type: "Directory",
-        name: interfaceType.type,
+        name: functionType.name,
         data: renderTemplates(
-          templatePath("interface-type"),
-          interfaceType,
+          templatePath("module-type"),
+          functionType,
           subTemplates
         ),
       });
     }
   }
 
-  // Generate module type folders
-  if (abi.moduleType) {
-    output.entries.push({
-      type: "Directory",
-      name: abi.moduleType.type,
-      data: renderTemplates(
-        templatePath("module-type"),
-        abi.moduleType,
-        subTemplates
-      ),
-    });
-  }
-
   // Generate enum type folders
-  if (abi.enumTypes) {
-    for (const enumType of abi.enumTypes) {
+  if (abi.enums) {
+    for (const enumType of abi.enums) {
       output.entries.push({
         type: "Directory",
-        name: enumType.type,
+        name: enumType.name,
         data: renderTemplates(
           templatePath("enum-type"),
           enumType,
