a0784148cc4dcae653e5eee670f23abaa202756c
Niraj Kamdar <nirajkamdar27@pm.me>
2023-04-10 18:45:41 +0400

    fix: python plugin example

--- packages/templates/plugin/python/sample/__init__.py
+++ packages/templates/plugin/python/sample/__init__.py
@@ -1,3 +1,14 @@
-# NOTE: This is an auto-generated file. All modifications will be overwritten.
+from polywrap_plugin import PluginPackage
+from .wrap import *
 
 
+class SampleModule(Module):
+    def sample_method(self, args: ArgsSampleMethod, client: object, env: None):
+        return f"{args['data']} from sample_method"
+
+
+def sample_plugin() -> PluginPackage:
+    return PluginPackage(
+        module=SampleModule(None),
+        manifest=manifest,
+    )
