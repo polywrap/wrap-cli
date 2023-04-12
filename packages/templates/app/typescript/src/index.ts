9b64b122c4c41f5b47446c30763cb88c40931a40
Pileks <jure@amber-it.co>
2023-04-12 19:47:27 +0200

    import newer logger in typescript template

--- packages/templates/app/typescript/src/index.ts
+++ packages/templates/app/typescript/src/index.ts
@@ -1,4 +1,8 @@
-import { HelloWorld_Module, Ethereum_Module } from "./wrap";
+import {
+  Logger_Module,
+  Ethereum_Module,
+  Logger_Logger_LogLevelEnum,
+} from "./wrap";
 
 import { PolywrapClient } from "@polywrap/client-js";
 
@@ -7,22 +11,37 @@ const client = new PolywrapClient();
 async function main() {
   console.log("Invoking: logMessage");
 
-  await HelloWorld_Module.logMessage({
-    message: "Hello there"
-  }, client);
+  await Logger_Module.log(
+    {
+      message: "Hello there",
+      level: Logger_Logger_LogLevelEnum.INFO,
+    },
+    client
+  );
 
-  await HelloWorld_Module.logMessage({
-    message: "Hello again"
-  }, client);
+  await Logger_Module.log(
+    {
+      message: "Hello again",
+      level: Logger_Logger_LogLevelEnum.INFO,
+    },
+    client
+  );
 
-  await HelloWorld_Module.logMessage({
-    message: "One last time..."
-  }, client);
+  await Logger_Module.log(
+    {
+      message: "One last time...",
+      level: Logger_Logger_LogLevelEnum.INFO,
+    },
+    client
+  );
 
-  const result = await Ethereum_Module.encodeParams({
-    types: ["address", "uint256"],
-    values: ["0xB1B7586656116D546033e3bAFF69BFcD6592225E", "500"]
-  }, client);
+  const result = await Ethereum_Module.encodeParams(
+    {
+      types: ["address", "uint256"],
+      values: ["0xB1B7586656116D546033e3bAFF69BFcD6592225E", "500"],
+    },
+    client
+  );
 
   if (result.ok) {
     console.log(`Ethereum_Module.encodeParams:\n${result.value}`);
