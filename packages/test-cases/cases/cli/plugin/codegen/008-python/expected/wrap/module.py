0fcd658bc655bd2ad5e82be8570a638c60fb093e
Cesar <cesarbrazon10@gmail.com>
2023-04-12 21:14:28 +0200

    chore: fix python codegen tests

--- packages/test-cases/cases/cli/plugin/codegen/008-python/expected/wrap/module.py
+++ packages/test-cases/cases/cli/plugin/codegen/008-python/expected/wrap/module.py
@@ -8,17 +8,26 @@ from .types import *
 
 from polywrap_core import InvokerClient, UriPackageOrWrapper
 from polywrap_plugin import PluginModule
+from polywrap_msgpack import GenericMap
 
 TConfig = TypeVar("TConfig")
 
 
+ArgsSampleMethod = TypedDict("ArgsSampleMethod", {
+    "data": str
+})
 
-class ArgsSampleMethod(TypedDict):
-    data: str
 
 class Module(Generic[TConfig], PluginModule[TConfig]):
+    def __new__(cls, *args, **kwargs):
+        # NOTE: This is used to dynamically add WRAP ABI compatible methods to the class
+        instance = super().__new__(cls)
+        setattr(instance, "sampleMethod", instance.sample_method)
+        return instance
+
     @abstractmethod
     def sample_method(
+        self,
         args: ArgsSampleMethod,
         client: InvokerClient[UriPackageOrWrapper],
         env: None
