0d4147bd6ec20bff6793546a0a19c76c6facb3a1
krisbitney <kristoferbitney@hotmail.com>
2023-04-13 13:45:36 +0000

    added test for typeHandler

---
+++ packages/js/core/src/__tests__/typesHandler.spec.ts
@@ -0,0 +1,60 @@
+import { typesHandler } from "../utils";
+
+describe('typesHandler', () => {
+  it('should return the original value if it is not a Map', () => {
+    const value = { a: 1 };
+    expect(typesHandler(undefined, value)).toEqual(value);
+  });
+
+  it('should return an empty object if the Map keys are not of type string', () => {
+    const value = new Map([[1, 'one'], [2, 'two']]);
+    expect(typesHandler(undefined, value)).toEqual({});
+  });
+
+  it('should convert empty Map into empty object', () => {
+    const value = new Map();
+    const expected = {};
+    expect(typesHandler(undefined, value)).toEqual(expected);
+  });
+
+  it('should convert Map object with string keys into object with key-value pairs', () => {
+    const value = new Map([['a', 1], ['b', 2]]);
+    const expected = { a: 1, b: 2 };
+    expect(typesHandler(undefined, value)).toEqual(expected);
+  });
+
+  it('should correctly stringify a Map object with string keys', () => {
+    const myObject = {
+      myMap: new Map([['a', 1], ['b', 2]])
+    };
+    const expected = '{\n  "myMap": {\n    "a": 1,\n    "b": 2\n  }\n}';
+
+    const stringified = JSON.stringify(myObject, typesHandler, 2);
+    expect(stringified).toEqual(expected);
+  });
+
+  it('should correctly stringify a Map object with non-string values', () => {
+    const myObject = {
+      myMap: new Map([['a', { foo: 'bar' }]])
+    };
+    const expected = '{\n  "myMap": {\n    "a": {\n      "foo": "bar"\n    }\n  }\n}';
+
+    const stringified = JSON.stringify(myObject, typesHandler, 2);
+
+    console.log(expected)
+    console.log(stringified)
+
+    expect(stringified).toEqual(expected);
+  });
+
+  it('should correctly stringify a non-Map object', () => {
+    const myObject = {
+      foo: 'bar',
+      baz: 42
+    };
+    const expected = '{\n  "foo": "bar",\n  "baz": 42\n}';
+
+    const stringified = JSON.stringify(myObject, typesHandler, 2);
+    expect(stringified).toEqual(expected);
+  });
+});
--- packages/js/core/src/utils/typesHandler.ts
+++ packages/js/core/src/utils/typesHandler.ts
@@ -1,12 +1,23 @@
+/**
+ * Converts a Map object with string keys into an object with key-value pairs
+ * that can be stringified using JSON.stringify. Returns an empty object if the
+ * keys are not of type string. Returns the original value if it is not a Map.
+ *
+ * @param _ Unused.
+ * @param value The value from the object to be stringified.
+ * @returns The converted object or the original value if it is not a Map
+ * or if the Map's keys are not of type string.
+ */
 export const typesHandler = (_: unknown, value: unknown): unknown => {
   if (value instanceof Map) {
-    return Array.from(value).reduce(
-      (obj: Record<string, unknown>, [key, value]) => {
-        obj[key] = value;
-        return obj;
-      },
-      {}
-    );
+    const obj: Record<string, unknown> = {};
+    const firstKey = value.keys().next().value;
+    if (typeof firstKey === "string") {
+      for (const [k, v] of value.entries()) {
+        obj[k] = v;
+      }
+    }
+    return obj;
   }
 
   return value;
