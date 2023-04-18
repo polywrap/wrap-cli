7f9aba8c94262316f451465c008596ae0b1b76d0
namesty <nestor09amesty@gmail.com>
2023-04-18 23:32:12 +0200

    (fix): bind sanity tests updated with serde renames

--- packages/test-cases/cases/bind/sanity/output/plugin-rs/module.rs
+++ packages/test-cases/cases/bind/sanity/output/plugin-rs/module.rs
@@ -11,31 +11,45 @@ use super::types::*;
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct ArgsModuleMethod {
     pub str: String,
+    #[serde(rename = "optStr")]
     pub opt_str: Option<String>,
     pub en: CustomEnum,
+    #[serde(rename = "optEnum")]
     pub opt_enum: Option<CustomEnum>,
+    #[serde(rename = "enumArray")]
     pub enum_array: Vec<CustomEnum>,
+    #[serde(rename = "optEnumArray")]
     pub opt_enum_array: Option<Vec<Option<CustomEnum>>>,
     pub map: Map<String, i32>,
+    #[serde(rename = "mapOfArr")]
     pub map_of_arr: Map<String, Vec<i32>>,
+    #[serde(rename = "mapOfMap")]
     pub map_of_map: Map<String, Map<String, i32>>,
+    #[serde(rename = "mapOfObj")]
     pub map_of_obj: Map<String, AnotherType>,
+    #[serde(rename = "mapOfArrOfObj")]
     pub map_of_arr_of_obj: Map<String, Vec<AnotherType>>,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct ArgsObjectMethod {
     pub object: AnotherType,
+    #[serde(rename = "optObject")]
     pub opt_object: Option<AnotherType>,
+    #[serde(rename = "objectArray")]
     pub object_array: Vec<AnotherType>,
+    #[serde(rename = "optObjectArray")]
     pub opt_object_array: Option<Vec<Option<AnotherType>>>,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct ArgsOptionalEnvMethod {
     pub object: AnotherType,
+    #[serde(rename = "optObject")]
     pub opt_object: Option<AnotherType>,
+    #[serde(rename = "objectArray")]
     pub object_array: Vec<AnotherType>,
+    #[serde(rename = "optObjectArray")]
     pub opt_object_array: Option<Vec<Option<AnotherType>>>,
 }
 
