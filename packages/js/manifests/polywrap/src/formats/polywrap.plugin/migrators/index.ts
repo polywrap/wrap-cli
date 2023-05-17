09d4b6dba49ec7abe0da9c638431d5d8dade3514
krisbitney <kristoferbitney@hotmail.com>
2023-05-17 17:30:02 +0100

    updated polywrap.plugin.yaml and polywrap.app.yaml versions to 0.4.0

--- packages/js/manifests/polywrap/src/formats/polywrap.plugin/migrators/index.ts
+++ packages/js/manifests/polywrap/src/formats/polywrap.plugin/migrators/index.ts
@@ -1,6 +1,7 @@
 import { Migrator } from "../../../migrations";
 import { migrate as migrate_0_1_0_to_0_2_0 } from "./0.1.0_to_0.2.0";
 import { migrate as migrate_0_2_0_to_0_3_0 } from "./0.2.0_to_0.3.0";
+import { migrate as migrate_0_3_0_to_0_4_0 } from "./0.3.0_to_0.4.0";
 
 export const migrators: Migrator[] = [
   {
@@ -21,6 +22,6 @@ export const migrators: Migrator[] = [
   {
     from: "0.3.0",
     to: "0.4.0",
-    migrate: migrate_0_2_0_to_0_3_0,
+    migrate: migrate_0_3_0_to_0_4_0,
   },
 ];
