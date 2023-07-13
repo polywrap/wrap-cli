420114d999bbb1600413a099c522480341a200d2
dOrgJelli <jelli@dorg.tech>
2023-07-13 05:28:14 +0200

    chore: fix lint

--- packages/schema/bind/src/bindings/golang/transforms/moduleNeedsTypes.ts
+++ packages/schema/bind/src/bindings/golang/transforms/moduleNeedsTypes.ts
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { isBaseType, isBuiltInType } from "../types";
+
 import {
   MethodDefinition,
   ModuleDefinition,
 } from "@polywrap/wrap-manifest-types-js";
 import { AbiTransforms } from "@polywrap/schema-parse";
-
-import { isBaseType, isBuiltInType } from "../types";
 
 interface ModuleNeedsTypesState {
   moduleDefinition?: ModuleDefinition;
@@ -17,7 +18,7 @@ export function moduleNeedsTypes(): AbiTransforms {
   return {
     enter: {
       ModuleDefinition: (def: ModuleDefinition) => {
-        console.log("ENTER", def.type)
+        console.log("ENTER", def.type);
         state.moduleDefinition = def;
         state.needsTypes = false;
         return def;
@@ -25,31 +26,31 @@ export function moduleNeedsTypes(): AbiTransforms {
       MethodDefinition: (def: MethodDefinition) => {
         console.log("METHOD", def.name);
         if (def.arguments && def.arguments.length > 0) {
-          console.log("NEEDS cause args")
+          console.log("NEEDS cause args");
           state.needsTypes = true;
         }
 
         if (def.return) {
           const returnType = def.return.type;
           if (!isBaseType(returnType) && !isBuiltInType(returnType)) {
-            console.log("NEEDS cause ret")
+            console.log("NEEDS cause ret");
             state.needsTypes = true;
           }
         }
         return def;
-      }
+      },
     },
     leave: {
       ModuleDefinition: (def: ModuleDefinition) => {
-        console.log("LEAVE", def.name)
-        const needsTypes = state.needsTypes
+        console.log("LEAVE", def.name);
+        const needsTypes = state.needsTypes;
         state.moduleDefinition = undefined;
         state.needsTypes = undefined;
         return {
           ...def,
           needsTypes,
         };
-      }
+      },
     },
   };
 }
