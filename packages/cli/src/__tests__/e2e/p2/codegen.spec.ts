8efe03b6b3e6de4e3c5506916c5d35c2183405dd
krisbitney <kristoferbitney@hotmail.com>
2023-07-17 13:36:55 -0500

    updated codegen help test

--- packages/cli/src/__tests__/e2e/p2/codegen.spec.ts
+++ packages/cli/src/__tests__/e2e/p2/codegen.spec.ts
@@ -16,6 +16,9 @@ Options:
                                      (default: polywrap.yaml | polywrap.yml)
   -g, --codegen-dir <path>           Output directory for the generated code
                                      (default: ./src/wrap)
+  -b, --bindgen <URI>                Uri for custom bindgen wrap (must
+                                     implement wrap-abi-bindgen interface; see
+                                     https://github.com/polywrap/wrap-abi-bindgen)
   -s, --script <path>                Path to a custom generation script
                                      (JavaScript | TypeScript)
   -c, --client-config <config-path>  Add custom configuration to the
