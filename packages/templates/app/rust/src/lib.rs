e8a49155ee3ec9bf06559d73fbd3a17c855a60c2
Niraj Kamdar <nirajkamdar27@pm.me>
2023-08-28 17:40:13 +0530

    chore: use ipfs.io in template

--- packages/templates/app/rust/src/lib.rs
+++ packages/templates/app/rust/src/lib.rs
@@ -4,29 +4,18 @@ use polywrap::ByteBuf;
 use wrap::types::*;
 
 pub fn main() {
-    let ipfs_provider = "http://localhost:5001";
-    let data = "hello world";
-    let ipfs = IpfsModule::new(None, None, None);
-    let ipfs_add_result = ipfs.add_file(&IpfsModuleArgsAddFile{
-      data: IpfsFileEntry {
-          name: "hello.txt".to_string(),
-          data: ByteBuf::from(data.as_bytes().to_vec()),
-      },
-      ipfs_provider: ipfs_provider.to_string(),
-      timeout: None,
-      add_options: None
-    }, None, None, None).unwrap();
+  let ipfs_provider = "https://ipfs.io";
+  let cid = "Qmc5gCcjYypU7y28oCALwfSvxCBskLuPKWpK4qpterKC7z";
+  let ipfs = IpfsModule::new(None, None, None);
 
-    let cid = ipfs_add_result.hash;
+  let data = ipfs.cat(&IpfsModuleArgsCat{
+    cid: cid.to_string(),
+    ipfs_provider: ipfs_provider.to_string(),
+    timeout: None,
+    cat_options: None
+  }, None, None, None).unwrap();
 
-    let data = ipfs.cat(&IpfsModuleArgsCat{
-      cid: cid.clone(),
-      ipfs_provider: ipfs_provider.to_string(),
-      timeout: None,
-      cat_options: None
-    }, None, None, None).unwrap();
-
-    assert_eq!(data, ByteBuf::from("hello world".as_bytes().to_vec()));
+  assert_eq!(data, ByteBuf::from("Hello World!\r\n".as_bytes().to_vec()));
 }
 
 #[cfg(test)]
