2e774b8559932d7371a28cf4b923bc2cd182da12
krisbitney <kris@dorg.tech>
2023-08-30 18:48:39 +0300

    using "Ethers" instead of "Ethereum"

--- packages/templates/app/ios/Template/PolywrapDemo.swift
+++ packages/templates/app/ios/Template/PolywrapDemo.swift
@@ -8,19 +8,19 @@ func polywrapDemo() {
     let logArgs = LoggingArgsLog(level: .INFO, message: "Hello there")
     try? logger.log(args: logArgs)
 
-    // Ethereum.encodeParams
-    print("Invoking: Ethereum.encodeParams(...)")
+    // Ethers.encodeParams
+    print("Invoking: Ethers.encodeParams(...)")
 
-    let eth = Ethereum()
-    let encodeArgs = EthereumArgsEncodeParams(
+    let eth = Ethers()
+    let encodeArgs = EthersArgsEncodeParams(
             types: ["address", "uint256"],
             values: ["0xB1B7586656116D546033e3bAFF69BFcD6592225E", "500"]
     )
     do {
         let encoded = try eth.encodeParams(args: encodeArgs)
-        print("Ethereum.encodeParams:\n\(encoded)")
+        print("Ethers.encodeParams:\n\(encoded)")
     } catch {
-        let logArgs = LoggingArgsLog(level: .ERROR, message: "Error - Ethereum.encodeParams: \(error)")
+        let logArgs = LoggingArgsLog(level: .ERROR, message: "Error - Ethers.encodeParams: \(error)")
         try? logger.log(args: logArgs)
         return
     }
