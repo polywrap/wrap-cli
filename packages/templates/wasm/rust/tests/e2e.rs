2c32efac348db413e8fa885ea0d84127cd3d3546
krisbitney <kris@dorg.tech>
2023-08-31 22:34:08 +0300

    passing rust tests in wrap-rust template

--- packages/templates/wasm/rust/tests/e2e.rs
+++ packages/templates/wasm/rust/tests/e2e.rs
@@ -1,39 +1,15 @@
-use template_wasm_rs::wrap::module::ArgsSampleMethod;
-use polywrap::{
-    PolywrapClient,
-    PolywrapClientConfig,
-    polywrap_core::uri::Uri,
+mod types;
+use crate::types::wrap::types::{
+    TemplateModule,
+    TemplateModuleArgsSampleMethod
 };
-use polywrap_msgpack_serde::to_vec;
-use std::path::Path;
-
-fn get_client() -> PolywrapClient {
-    let mut config = PolywrapClientConfig::new();
-    PolywrapClient::new(config.into())
-}
-
-pub fn get_wrap_uri() -> Result<Uri, String> {
-    let path = Path::new("../build")
-        .canonicalize()
-        .map_err(|e| e.to_string())?
-        .into_os_string()
-        .into_string()
-        .map_err(|e| format!("{e:?}"))?;
-    let uri = Uri::try_from(path).map_err(|e| e.to_string())?;
-    Ok(uri)
-}
 
 #[test]
 fn sample_method() {
-    let uri = get_wrap_uri().unwrap();
-    let args = ArgsSampleMethod {
+    let args = TemplateModuleArgsSampleMethod {
         arg: "input data".to_string(),
     };
-    let encoded_args = to_vec(&args).unwrap();
-
-    let response = get_client()
-        .invoke::<String>(&uri, "sampleMethod", Some(&encoded_args), None, None)
-        .unwrap();
-
-    assert_eq!(response, "input data from sample_method");
+    let template: TemplateModule = TemplateModule::new(None, None, None);
+    let response = template.sample_method(&args, None, None, None).unwrap();
+    assert_eq!(response.result, "input data from sample_method");
 }
