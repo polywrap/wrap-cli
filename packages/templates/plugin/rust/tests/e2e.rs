49b89cac10dbefbb88a81e04851f6b2a440e1403
krisbitney <kris@dorg.tech>
2023-10-27 12:26:46 +0500

    fixed rust plugin template test

--- packages/templates/plugin/rust/tests/e2e.rs
+++ packages/templates/plugin/rust/tests/e2e.rs
@@ -1,30 +1,25 @@
 use template_plugin_rs::SamplePlugin;
 use template_plugin_rs::wrap::module::ArgsSampleMethod;
-use polywrap_core::{
-    client::ClientConfig,
-    uri::Uri,
-};
+use polywrap_core::uri::Uri;
 use polywrap_plugin::{package::PluginPackage};
 use polywrap_client::{
-    client::PolywrapClient,
-    builder::{PolywrapClientConfig, PolywrapClientConfigBuilder},
+    client::Client,
+    builder::{ClientConfig, ClientConfigBuilder},
 };
 use polywrap_msgpack_serde::to_vec;
-use std::{
-    sync::{Arc, Mutex},
-};
+use std::sync::Arc;
 
-fn get_client() -> PolywrapClient {
+fn get_client() -> Client {
     let sample_plugin = SamplePlugin {};
     let plugin_pkg = PluginPackage::<SamplePlugin>::from(sample_plugin);
 
-    let mut config = PolywrapClientConfig::new();
+    let mut config = ClientConfig::new();
     config.add_package(
         Uri::try_from("plugin/sample").unwrap(),
         Arc::new(plugin_pkg)
     );
 
-    PolywrapClient::new(config.into())
+    Client::new(config.into())
 }
 
 #[test]
